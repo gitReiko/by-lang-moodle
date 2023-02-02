a8e9c8949d912f36357220b5054a31ca5e0b1af2
Denis <denis.makouski@bseu.by>
2023-02-02 16:52:25 +0300

    ++

---
+++ перакладзена 4.0/bulkusers.php
@@ -0,0 +1,47 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'bulkusers', language 'en', version '4.0'.
+ *
+ * @package     bulkusers
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addall'] = 'Дадаць усіх';
+$string['addsel'] = 'Дадаць да абрання';
+$string['allfilteredusers'] = 'Усе адфільтраваныя ({$a->count}/{$a->total})';
+$string['allselectedusers'] = 'Усе абраныя ({$a->count}/{$a->total})';
+$string['allusers'] = 'Усе карыстальнікі ({$a})';
+$string['available'] = 'Даступныя';
+$string['confirmmessage'] = 'Вы сапраўды жадаеце адправіць вышэйазначанае паведамленне ўсім гэтым карыстальнікам?<br />{$a}';
+$string['deleteduser'] = 'Выдаленыя карыстальнікі';
+$string['nofilteredusers'] = 'Карыстальнікі не знойдзены (0/{$a})';
+$string['noselectedusers'] = 'Карыстальнікі не абраны';
+$string['removeall'] = 'Выдаліць усіх';
+$string['removesel'] = 'Выдаліць з абрання';
+$string['selected'] = 'Абраныя';
+$string['selectedlist'] = 'Спіс абраных карыстальнікаў...';
+$string['selectedlist_help'] = 'Карыстальнікі могуць быць дададзены ці выдалены са спіса абраных карыстальнікаў з дапамогай націску па іх імі, а потым націску на адпаведную кнопку. З дапамогай утрымання кнопкі Apple ці Ctrl і націску па імёнам льга адначасова абраць некалькіх карыстальнікаў.';
+$string['users'] = 'Карыстальнікі';
+$string['users_help'] = 'У поле даступныя пералічаны ўсе карыстальнікі, якія адпавядаюць актыўным фільтрам. Калі фільтры не ўсталяваны, у спісе адлюстроўваюцца ўсе карыстальнікі сайта.';
+$string['usersfound'] = '{$a} карыстальнікаў знойдзена.';
+$string['usersinlist'] = 'Карыстальнікаў у спісе';
+$string['usersselected'] = '{$a} карыстальнікаў абрана.';
