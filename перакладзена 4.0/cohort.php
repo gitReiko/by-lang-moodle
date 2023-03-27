ca80fcf671bd2774c55d00faa212ba7f92427041
Denis <denis.makouski@bseu.by>
2023-03-27 17:12:42 +0300

    ++

---
+++ перакладзена 4.0/cohort.php
@@ -0,0 +1,99 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'cohort', language 'en', version '4.0'.
+ *
+ * @package     cohort
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addcohort'] = 'Дадаць новую кагорту';
+$string['allcohorts'] = 'Усе кагорты';
+$string['anycohort'] = 'Усякі';
+$string['assign'] = 'Прызначэнне';
+$string['assigncohorts'] = 'Прызначэнне чальцоў кагорты';
+$string['assignto'] = 'Чальцы кагорты \'{$a}\'';
+$string['backtocohorts'] = 'Назад да кагортаў';
+$string['bulkadd'] = 'Дадаць у кагорту';
+$string['bulknocohort'] = 'Даступныя кагорты не знойдзены';
+$string['categorynotfound'] = 'Катэгорыя <b>{$a}</b> не знойдзена ці вы не маеце дазволу на стварэнне кагорты. Будзе выкарыстаны кантэкст па змоўчванні.';
+$string['cohort'] = 'Кагорта';
+$string['cohortmember'] = 'Чальцы кагорты';
+$string['cohorts'] = 'Кагорты';
+$string['cohortsin'] = '{$a}: даступныя кагорты';
+$string['component'] = 'Крыніца';
+$string['contextnotfound'] = 'Кантэкст <b>{$a}</b> не знойдзена ці вы не маеце дазволу на стварэнне кагорты. Будзе выкарыстаны кантэкст па змоўчванні.';
+$string['csvcontainserrors'] = 'У дадзеных CSV былі выяўлены памылкі. Дэталі глядзіце ніжэй.';
+$string['csvcontainswarnings'] = 'У дадзеных CSV былі выяўлены папярэджанні. Дэталі глядзіце ніжэй.';
+$string['csvextracolumns'] = 'Калонка(і) <b>{$a}</b> будзе праігнаравана.';
+$string['currentusers'] = 'Карыстальнікі зараз';
+$string['currentusersmatching'] = 'Адпаведнасць цяперашніх карыстальнікаў';
+$string['defaultcontext'] = 'Кантэкст па змоўчванні';
+$string['delcohort'] = 'Выдаліць кагорту';
+$string['delconfirm'] = 'Вы сапраўды жадаеце выдаліць кагорту \'{$a}\'?';
+$string['description'] = 'Апісанне';
+$string['displayedrows'] = 'Адлюстравана {$a->displayed} радкоў з {$a->total}.';
+$string['duplicateidnumber'] = 'Кагорта з такім ID ужо існуе';
+$string['editcohort'] = 'Рэдагаваць кагорту';
+$string['editcohortidnumber'] = 'Рэдагаваць ID кагорты ';
+$string['editcohortname'] = 'Рэдагаваць назву кагорты';
+$string['eventcohortcreated'] = 'Кагорта створана';
+$string['eventcohortdeleted'] = 'Кагорта выдалена';
+$string['eventcohortmemberadded'] = 'Карыстальнік даданы ў кагорту';
+$string['eventcohortmemberremoved'] = 'Карыстальнік выдалены з кагорты';
+$string['eventcohortupdated'] = 'Кагорта абноўлена';
+$string['external'] = 'Вонкавая кагорта';
+$string['idnumber'] = 'ID кагорты';
+$string['invalidtheme'] = 'Тэма кагорты не існуе';
+$string['memberofcohort'] = 'Чалец кагорты';
+$string['memberscount'] = 'Памер кагорты';
+$string['name'] = 'Назва';
+$string['namecolumnmissing'] = 'Нешта не так з фарматам файла CSV. Калі ласка, праверце, што ў ім утрымліваюцца слушныя назвы слупкоў. Каб дадаць карыстальнікаў у кагорту, перайдзіце ў падзел \'Загрузіць карыстальнікаў\' у Адміністраванні сайта.';
+$string['namefieldempty'] = 'Назва поля не можа быць пустым';
+$string['newidnumberfor'] = 'Новы ID для кагорты {$a}';
+$string['newnamefor'] = 'Новая назва кагорты {$a}';
+$string['nocomponent'] = 'Створана ўручную';
+$string['potusers'] = 'Патэнцыйныя карыстальнікі';
+$string['potusersmatching'] = 'Патэнцыйныя прыдатныя карыстальнікі';
+$string['preview'] = 'Агляд';
+$string['privacy:metadata:cohort_members'] = 'Інфармацыя пра кагорту карыстальніка.';
+$string['privacy:metadata:cohort_members:cohortid'] = 'ID кагорты';
+$string['privacy:metadata:cohort_members:timeadded'] = 'Час (timestamp), калі карыстальнік быў даданы ў кагорту';
+$string['privacy:metadata:cohort_members:userid'] = 'ID карыстальніка, які злучаны з дадзенай кагортай';
+$string['removeuserwarning'] = 'Выдаленне карыстальнікаў з кагорты можа прывесці да адлічэння карыстальнікаў з курсаў. Гэта выдаліць іхнія налады, адзнакі, чалецтва ў групах і іншую інфармацыю, звязаную з курсамі.';
+$string['search'] = 'Пошук';
+$string['searchcohort'] = 'Пошук кагорты';
+$string['selectfromcohort'] = 'Абраць чальцоў з кагорты';
+$string['systemcohorts'] = 'Сістэмныя кагорты';
+$string['unknowncohort'] = 'Невядомая кагорта ({$a})!';
+$string['uploadcohorts'] = 'Загрузіць кагорты';
+$string['uploadcohorts_help'] = 'Кагорты могуць быць загружаны ў тэкставым файле. Фармат файла мусіць быць наступным:
+
+* Кожны радок файла ўтрымлівае адзін запіс
+* Кожны запіс уяўляе сабою серыю дадзеных, падзеленых абраным падзельнікам
+* Першы запіс ўтрымлівае спіс назваў палёў, якія вызначаюць фармат астатняй частцы файла
+* Патрабаваная назва поля - name
+* Дадатковыя назвы полёў - idnumber, description, descriptionformat, visible, context, category, category_id, category_idnumber, category_path';
+$string['uploadedcohorts'] = 'Загружана {$a} кагортаў';
+$string['useradded'] = 'Карыстальнік даданы ў кагорту "{$a}"';
+$string['visible'] = 'Бачная';
+$string['visible_help'] = 'Кожная кагорта можа быць прагледжана карыстальнікамі, якія маюць здольнасць \'moodle/cohort:view\' у кантэксце кагорты.<br/>
+Бачныя кагорты таксама могуць быць прагледжаны карыстальнікамі ў адпаведных курсах.';
