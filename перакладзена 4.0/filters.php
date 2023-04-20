2f8e80ef2fd6adb496d13f19cc3db5b27cd48ab7
Denis <denis.makouski@bseu.by>
2023-04-20 17:16:56 +0300

    ++

---
+++ перакладзена 4.0/filters.php
@@ -0,0 +1,92 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'filters', language 'en', version '4.0'.
+ *
+ * @package     filters
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['actfilterhdr'] = 'Актыўныя фільтры';
+$string['addfilter'] = 'Дадаць фільтар';
+$string['anycategory'] = 'усякая катэгорыя';
+$string['anycourse'] = 'усякі курс';
+$string['anycourses'] = 'Залічаны на ўсякі курс';
+$string['anyfield'] = 'усякае поле';
+$string['anyrole'] = 'усякая роля';
+$string['anyvalue'] = 'усякае значэньне';
+$string['applyto'] = 'Ужыць да';
+$string['categoryrole'] = 'Роля ў катэгорыі';
+$string['contains'] = 'зьмяшчае';
+$string['content'] = 'Зьмесьціва';
+$string['contentandheadings'] = 'Зьмесьціва ды загалоўкі';
+$string['coursecategory'] = 'катэгорыя курса';
+$string['courserole'] = 'Роля ў курсе';
+$string['courserolelabel'] = '{$a->label} - {$a->rolename} у {$a->coursename} з катэгорыі {$a->categoryname}';
+$string['courserolelabelerror'] = '{$a->label} памылка: курс {$a->coursename} не існуе';
+$string['coursevalue'] = 'значэньне курса';
+$string['datelabelisafter'] = '{$a->label} знаходзіцца пасьля {$a->after}';
+$string['datelabelisbefore'] = '{$a->label} знаходзіцца перад {$a->before}';
+$string['datelabelisbetween'] = '{$a->label} знаходзіцца між {$a->after} і {$a->before}';
+$string['defaultx'] = 'Па змаўчаньні ({$a})';
+$string['disabled'] = 'Адключана';
+$string['doesnotcontain'] = 'не зьмяшчае';
+$string['endswith'] = 'канчаецца на';
+$string['filterallwarning'] = 'Ужываньне фільтраў да загалоўкаў і зьмесціва можа моцна павялічыць нагрузку на вашы сэрвер. Калі ласка, выкарыстоўвайце наладу \'Ужыць да\' ашчадліва. Галоўным чынам для фільтра шматмоўе.';
+$string['filtersettings'] = 'Налады фільтраў';
+$string['filtersettings_help'] = 'Гэтая старонка дазволіць вам уключыць ці выключыць фільтры ў пэўнай часткі сайта.
+
+Частка фільтраў можа дазваляць вам усталёўваць лакальныя налады, у гэтым выпадку побач з іхняй назвай будзе спасылка на налады.';
+$string['filtersettingsforin'] = 'Налады фільтраў для {$a->filter} у {$a->context}';
+$string['filtersettingsin'] = 'Налады фільтраў у {$a}';
+$string['firstaccess'] = 'Першы доступ';
+$string['globalrolelabel'] = '{$a->label} - {$a->value}';
+$string['isactive'] = 'Актыўны?';
+$string['isafter'] = 'пасьля';
+$string['isanyvalue'] = 'усякае значэньне';
+$string['isbefore'] = 'перад';
+$string['isdefined'] = 'вызначана';
+$string['isempty'] = 'адсутнічае';
+$string['isequalto'] = 'роўна';
+$string['isnotdefined'] = 'ня вызначана';
+$string['isnotequalto'] = 'ня роўна';
+$string['limiterfor'] = '{$a} абмежавальнік поля';
+$string['neveraccessed'] = 'Ніколі ня меў доступу';
+$string['nevermodified'] = 'Ніколі не зьмяняўся';
+$string['newfilter'] = 'Новы фільтар';
+$string['nofiltersenabled'] = 'Фільтры плагінаў выключаны на гэтым сайце.';
+$string['off'] = 'Выключыць';
+$string['offbutavailable'] = 'Выключыць, але зрабіць даступным';
+$string['on'] = 'Уключыць';
+$string['privacy:reason'] = 'Падсістэма фільтраў не захоўвае ніякіх дадзеных.';
+$string['profilefilterfield'] = 'Назва поля профіля';
+$string['profilefilterlimiter'] = 'Аператар поля профіля';
+$string['profilelabel'] = '{$a->label}: {$a->profile} {$a->operator} {$a->value}';
+$string['profilelabelnovalue'] = '{$a->label}: {$a->profile} {$a->operator}';
+$string['removeall'] = 'Выдаліць усе фільтры';
+$string['removeselected'] = 'Выдаліць абранае';
+$string['replacefilters'] = 'Замяніць фільтры';
+$string['selectlabel'] = '{$a->label} {$a->operator} {$a->value}';
+$string['startswith'] = 'пачынаецца з';
+$string['tablenosave'] = 'Зьмены ў вышэйшай табліцы захоўваюцца аўтаматычна.';
+$string['textlabel'] = '{$a->label} {$a->operator} {$a->value}';
+$string['textlabelnovalue'] = '{$a->label} {$a->operator}';
+$string['valuefor'] = '{$a} значэньне';
