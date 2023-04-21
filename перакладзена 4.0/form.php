ed8f18598fedda777803213019b7b78620d17e0e
Denis <denis.makouski@bseu.by>
2023-04-21 17:01:09 +0300

    ++

---
+++ перакладзена 4.0/form.php
@@ -0,0 +1,93 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'form', language 'en', version '4.0'.
+ *
+ * @package     form
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addfields'] = 'Дадаць {$a} палі форме';
+$string['advancedelement'] = 'Прасунуты элемент';
+$string['close'] = 'Закрыць';
+$string['custom'] = 'Наладжвальны';
+$string['day'] = 'Дзень';
+$string['default'] = 'Па змаўчаньні';
+$string['display'] = 'Адлюстраваць';
+$string['err_alphanumeric'] = 'Тут льга друкаваць толькі літары ці лічбы.';
+$string['err_email'] = 'Тут льга друкаваць толькі слушны адрас імэйл.';
+$string['err_lettersonly'] = 'Тут льга друкаваць толькі літары.';
+$string['err_maxfiles'] = 'Тут льга прымацоўваць не болей {$a} файлаў.';
+$string['err_maxlength'] = 'Тут льга друкаваць не болей {$a->format} знакаў.';
+$string['err_minlength'] = 'Тут трэба друкаваць прынамсі {$a->format} знакаў.';
+$string['err_nonzero'] = 'Тут льга друкаваць лічбу, якая не пачынаецца з 0.';
+$string['err_nopunctuation'] = 'Тут нельга друкаваць знакі прыпынку.';
+$string['err_numeric'] = 'Тут трэба друкаваць лічбу.';
+$string['err_positiveint'] = 'Тут трэба друкаваць лічбу, большую за 0.';
+$string['err_rangelength'] = 'Тут льга друкаваць ад {$a->format[0]} да {$a->format[1]} знакаў.';
+$string['err_required'] = 'Тут мусіць быць значэньне.';
+$string['err_wrappingwhitespace'] = 'Значэньне не павінна пачынацца ці сканчацца прабеламі.';
+$string['err_wrongfileextension'] = 'Частку файлаў нельга запампаваць ({$a->wrongfiles}). Дазволены файлы толькі наступных тыпаў {$a->allowlist}.';
+$string['filesofthesetypes'] = 'Прымальныя тыпы файлаў:';
+$string['filetypesany'] = 'Усе тыпы файлаў';
+$string['filetypesnotall'] = 'Тут не дазволены для абраньня \'Усе тыпы файлаў\'';
+$string['filetypesnotallowed'] = 'Гэтыя тыпы файлаў не дазволены тут: {$a}';
+$string['filetypesnotwhitelisted'] = 'Гэтыя тыпы файлаў не дазволены тут: {$a}';
+$string['filetypesothers'] = 'Іншыя файлы';
+$string['filetypesunknown'] = 'Невядомыя тыпы файлаў: {$a}';
+$string['general'] = 'Агульныя';
+$string['hideadvanced'] = 'Схаваць прасунутыя';
+$string['hour'] = 'Гадзіна';
+$string['minute'] = 'Хвіліна';
+$string['miscellaneoussettings'] = 'Розныя налады';
+$string['modstandardels'] = 'Агульныя налады модуля';
+$string['month'] = 'Месяц';
+$string['mustbeoverriden'] = 'Абстрактны метад form_definition() мусіць быць вызначаны у класе {$a}, калі ласка, выпраўце гэтае у кодзе.';
+$string['newvaluefor'] = 'Новае значэньне для {$a}';
+$string['nomethodforaddinghelpbutton'] = 'Не існуе метаду для даданьня кнопкі дапамогі да формы элементу {$a->name} (клас {$a->classname})';
+$string['nonexistentformelements'] = 'Спроба даданьня кнопкі дапамогі да неіснуючага элемента формы : {$a}';
+$string['nopermissionform'] = 'Вы не маеце дазволу на доступ да гэтай формы.';
+$string['noselection'] = 'Без абраньня';
+$string['nosuggestions'] = 'Без прапановы';
+$string['novalue'] = 'Нічога не надрукавана';
+$string['novalueclicktoset'] = 'Націсніце для друку тэкста';
+$string['optional'] = 'Дадаткова';
+$string['othersettings'] = 'Іншыя налады';
+$string['passwordunmaskedithint'] = 'Рэдагаваць пароль';
+$string['passwordunmaskinstructions'] = 'Націсьніце enter, каб захаваць зьмены';
+$string['passwordunmaskrevealhint'] = 'Раскрыць';
+$string['privacy:metadata:preference:filemanager_recentviewmode'] = 'Нядаўна абраны рэжым агляду элемента выбарніку файлаў.';
+$string['privacy:preference:filemanager_recentviewmode'] = 'Пераважны шлях адлюстраваньня файлаў у выбарніку файлаў: {$a}';
+$string['requiredelement'] = 'Абавязковае поле';
+$string['security'] = 'Бясьпека';
+$string['selectallornone'] = 'Абраць усё/нічога';
+$string['selected'] = 'Абранае';
+$string['selecteditems'] = 'Абраныя прадметы:';
+$string['showadvanced'] = 'Паказаць прасунутае';
+$string['showless'] = 'Паказаць меней...';
+$string['showmore'] = 'Паказаць болей...';
+$string['somefieldsrequired'] = 'У гэтай форме адзначаны {$a} абавязковыя палі.';
+$string['suggestions'] = 'Прапановы';
+$string['time'] = 'Час';
+$string['timeunit'] = 'Адзінка часу';
+$string['timing'] = 'Тэрміны';
+$string['unmaskpassword'] = 'Выкрыць';
+$string['year'] = 'Год';
