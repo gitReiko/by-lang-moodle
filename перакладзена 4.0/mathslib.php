142695a7467f6ffd574c9a00aa758e6d5b00c03e
Denis <denis.makouski@bseu.by>
2023-06-14 16:34:57 +0300

    ++

---
+++ перакладзена 4.0/mathslib.php
@@ -0,0 +1,43 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'mathslib', language 'en', version '4.0'.
+ *
+ * @package     mathslib
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['anunexpectederroroccured'] = 'узьнікла непрадбачная памылка';
+$string['cannotassigntoconstant'] = 'канстанта \'{$a}\' ня можа быць прызначана';
+$string['cannotredefinebuiltinfunction'] = 'нельга перавызначыць убудаваную функцыю \'{$a}()\'';
+$string['divisionbyzero'] = 'дзяленьне на нуль';
+$string['expectingaclosingbracket'] = 'чакаецца закрыцьцё дужкі';
+$string['illegalcharactergeneral'] = 'хібны знак \'{$a}\'';
+$string['illegalcharacterunderscore'] = 'хібны знак \'_\'';
+$string['implicitmultiplicationnotallowed'] = 'чакаецца аператар, невідавочнае памнажэньне не дапускаецца.';
+$string['internalerror'] = 'унутраная памылка';
+$string['operatorlacksoperand'] = 'аператару \'{$a}\' нястае аперанда';
+$string['undefinedvariable'] = 'нявызначаная зьменная \'{$a}\'';
+$string['undefinedvariableinfunctiondefinition'] = 'нявызначаная зьменная \'{$a}\' у вызначэньні функцыі';
+$string['unexpectedclosingbracket'] = 'нечаканае закрыцьцё дужак';
+$string['unexpectedcomma'] = 'нечаканая коска';
+$string['unexpectedoperator'] = 'нечаканы аператар \'{$a}\'';
+$string['wrongnumberofarguments'] = 'хібная колькасьць аргументаў (атрымана: {$a->given}, чакана: {$a->expected})';
