3817086f8094077499658d2b77ff166cdf1b59a5
Denis <denis.makouski@bseu.by>
2023-06-14 16:56:43 +0300

    ++

---
+++ перакладзена 4.0/media.php
@@ -0,0 +1,41 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'media', language 'en', version '4.0'.
+ *
+ * @package     media
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['audioextensions'] = 'Аўдыё: {$a}';
+$string['defaultheight'] = 'Прадвызначаная вышыня';
+$string['defaultheightdesc'] = 'Вызначае вышыню прайгравальніка, калі вышыня ня вызначаная, а актуальная вышыня медыяфайла ня можа быць вызначана прайгравальнікам.';
+$string['defaultwidth'] = 'Прадвызначаная шырыня';
+$string['defaultwidthdesc'] = 'Вызначае шырыню прайгравальніка, калі шырыня ня вызначаная, а актуальная шырыня медыяфайла ня можа быць вызначана прайгравальнікам.';
+$string['extensions'] = 'Пашырэньні: {$a}';
+$string['managemediaplayers'] = 'Кіраваньне медыяпрайгравальнікамі';
+$string['mediaformats'] = 'Даступныя прайгравальнікі';
+$string['mediaformats_desc'] = 'Калі прайгравальнікі ўключаныя, медыяфайлы льга ўбудоўваць плагінамі мультымедыйныя фільтры (калі ўключаны) ці з дапамогай файла або спасылкай. Калі прайгравальнікі выключаны, гэтыя фарматы нельга будзе ўбудаваць, але карыстальнікі змогуць спампоўваць рэсурсы ці перахадзіць па спасылке.
+
+Калі два прайгравальніка падтрымліваюць адзін і той жа фармат, уключэньне абодвух павысіць сумяшчальнасьць розных прыладащ, напрыклад, мабільных тэлефонаў. Льга павысіць яшчэ болей сумяшчальнасьць, прапанаваўшы некалькі файлаў ў розных фарматах для аднаго аўдыё ці відэа кліпа.';
+$string['privacy:metadata'] = 'Убудова медыя не захоўвае аніякіх асабістых дадзеных.';
+$string['supports'] = 'Падтрымка';
+$string['videoextensions'] = 'Відэа: {$a}';
