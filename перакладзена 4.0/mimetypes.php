7c77e51b97d29985dc92939f647a1ea8c996744c
Denis <denis.makouski@bseu.by>
2023-06-15 17:22:38 +0300

    ++

---
+++ перакладзена 4.0/mimetypes.php
@@ -0,0 +1,84 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'mimetypes', language 'en', version '4.0'.
+ *
+ * @package     mimetypes
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['application/dash_xml'] = 'Dynamic Adaptive Streaming over HTTP (MPEG-DASH)';
+$string['application/epub_zip'] = 'кніга EPUB';
+$string['application/json'] = '{$a->MIMETYPE2} тэкст';
+$string['application/msword'] = 'Дакумент Word';
+$string['application/pdf'] = 'Дакумент PDF';
+$string['application/vnd.moodle.backup'] = 'Рэзервовая копія Moodle';
+$string['application/vnd.ms-excel'] = 'Табліца Excel';
+$string['application/vnd.ms-excel.sheet.macroEnabled.12'] = 'Працоўная кніга з уключанымі макрасамі Excel 2007';
+$string['application/vnd.ms-powerpoint'] = 'Прэзентацыя Powerpoint';
+$string['application/vnd.oasis.opendocument.spreadsheet'] = 'Табліца OpenDocument';
+$string['application/vnd.oasis.opendocument.spreadsheet-template'] = 'Шаблон табліцы OpenDocument';
+$string['application/vnd.oasis.opendocument.text'] = 'Тэкставы дакумент OpenDocument';
+$string['application/vnd.oasis.opendocument.text-template'] = 'Тэкставы шаблон OpenDocument';
+$string['application/vnd.oasis.opendocument.text-web'] = 'Шаблон ўэб старонкі OpenDocument';
+$string['application/vnd.openxmlformats-officedocument.presentationml.presentation'] = 'Прэзентацыя Powerpoint 2007';
+$string['application/vnd.openxmlformats-officedocument.presentationml.slideshow'] = 'Слайд-Шоў Powerpoint 2007';
+$string['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'] = 'Табліца Excel 2007';
+$string['application/vnd.openxmlformats-officedocument.spreadsheetml.template'] = 'Шаблон Excel 2007';
+$string['application/vnd.openxmlformats-officedocument.wordprocessingml.document'] = 'Дакумент Word 2007';
+$string['application/x-iwork-keynote-sffkey'] = 'Прэзентацыя iWork Keynote';
+$string['application/x-iwork-numbers-sffnumbers'] = 'Табліца iWork Numbers';
+$string['application/x-iwork-pages-sffpages'] = 'Дакумент iWork Pages';
+$string['application/x-javascript'] = 'Крыніца JavaScript';
+$string['application/x-mpegURL'] = 'HTTP Live Streaming (HLS)';
+$string['application/x-mspublisher'] = 'Дакумент выдаўца';
+$string['application/x-shockwave-flash'] = 'Flash анімацыя';
+$string['application/xhtml_xml'] = 'Дакумент XHTML';
+$string['archive'] = 'Архіў ({$a->EXT})';
+$string['audio'] = 'Аўдыё файл ({$a->EXT})';
+$string['default'] = '{$a->mimetype}';
+$string['document/unknown'] = 'Файл';
+$string['group:archive'] = 'Архіўныя файлы';
+$string['group:audio'] = 'Аўдыё файлы';
+$string['group:document'] = 'Файлы дакументаў';
+$string['group:html_audio'] = 'Аўдыё файлы, што адпачатку падтрымліваюць браўзары';
+$string['group:html_track'] = 'HTML файлы трэкаў';
+$string['group:html_video'] = 'Відэа файлы, што адпачатку падтрымліваюць браўзары';
+$string['group:image'] = 'Файлы выяваў';
+$string['group:media_source'] = 'Струменевыя медыя';
+$string['group:optimised_image'] = 'Файлы выяваў, што трэба аптымізаваць, напрыклад, дасягненьні';
+$string['group:presentation'] = 'Файлы прэзентацыяў';
+$string['group:sourcecode'] = 'Зыходны код';
+$string['group:spreadsheet'] = 'Файлы табліцаў';
+$string['group:video'] = 'Відэа файлы';
+$string['group:web_audio'] = 'Ўэб аўдыё файлы';
+$string['group:web_file'] = 'Ўэб файлы';
+$string['group:web_image'] = 'Ўэб файлы выяваў';
+$string['group:web_video'] = 'Ўэб відэа файлы';
+$string['image'] = 'Выява ({$a->MIMETYPE2})';
+$string['image/vnd.microsoft.icon'] = 'Абразок Windows';
+$string['text/css'] = 'Каскадная Табліца Стыляў (CSS)';
+$string['text/csv'] = 'Значэнmні, падзеленыя коскамі';
+$string['text/html'] = 'HTML дакумент';
+$string['text/plain'] = 'Тэкставы файл';
+$string['text/rtf'] = 'RTF дакумент';
+$string['text/vtt'] = 'Ўэб Тэкставая Дарожка Відэа';
+$string['video'] = 'Відэа файл ({$a->EXT})';
