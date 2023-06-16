cdf4111836f9b603e96f838021fc7dfaf13e0c55
Denis <denis.makouski@bseu.by>
2023-06-16 09:54:10 +0300

    ++

---
+++ перакладзена 4.0/my.php
@@ -0,0 +1,47 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'my', language 'en', version '4.0'.
+ *
+ * @package     my
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addpage'] = 'Дадаць старонку';
+$string['alldashboardswerereset'] = 'Усе старонкі Прыладавай былі скінуты да стандарта.';
+$string['allprofileswerereset'] = 'Усе старонкі профілю былі скінуты да стандарта.';
+$string['coursemanagementoptions'] = 'Опцыі кіраваньня курсам';
+$string['defaultpage'] = 'Старонка прыладавай па стандарту';
+$string['defaultprofilepage'] = 'Старонка профілю па стандарту';
+$string['delpage'] = 'Выдаліць старонку';
+$string['error:dashboardisdisabled'] = 'Прыладная была адключана адміністратарам.';
+$string['managepages'] = 'Кіраваньне старонкамі';
+$string['mymoodle'] = 'Прыладавая';
+$string['nocourses'] = 'Інфармацыя пра курс адсутнічае.';
+$string['noguest'] = 'Прыладавая не даступная для карыстальнікаў-гасьцей';
+$string['pinblocks'] = 'Наладжваньне прымацаваных блокаў для прыладавай';
+$string['pinblocksexplan'] = 'Усе налады блокаў, якія вы зробіце тут будуць бачныя (але не рэдагавальныя) для ўсіх карыстальнікаў Moodle на іхніх старонках агляду Прыладавая.';
+$string['privacy:metadata:core_my:preference:user_home_page_preference'] = 'Перавагі прыладавай карыстальніка.';
+$string['reseterror'] = 'Адбылася памылка падчас скідваньня вашай старонкі';
+$string['reseteveryonesdashboard'] = 'Скінуць Прыладавую ўсім карыстальнікам';
+$string['reseteveryonesprofile'] = 'Скінуць профіль усім карыстальнікам';
+$string['resetpage'] = 'Скінуць старонку да стандарта';
+$string['resettingdashboards'] = 'Скідваньне прыладавых карыстальнікаў да стандарта...';
