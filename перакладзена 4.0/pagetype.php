2abe7a9e866393a03c30e0914f57caeae509e668
Denis <denis.makouski@bseu.by>
2023-06-16 11:06:12 +0300

    ++

---
+++ перакладзена 4.0/pagetype.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'pagetype', language 'en', version '4.0'.
+ *
+ * @package     pagetype
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['page-admin-current'] = 'Цяперашняя старонка адміністраваньня сайта';
+$string['page-admin-x'] = 'Усякая старонка адміністраваньня сайта';
+$string['page-course-index'] = 'Верхняя старонка прагляду катэгорыяў';
+$string['page-course-index-category'] = 'Цяперашняя старонка прагляду катэгорыяў';
+$string['page-course-index-x'] = 'Усякая старонка прагляду катэгорыяў';
+$string['page-course-report-x'] = 'Усякая справаздача курса';
+$string['page-course-search'] = 'Старонка пошуку курсаў';
+$string['page-course-view-x'] = 'Усякі тып галоўнай старонкі курса';
+$string['page-course-x'] = 'Усякая старонка курса';
+$string['page-mod-x'] = 'Усякая старонка модуля актыўнасьці';
+$string['page-mod-x-view'] = 'Усякая галоўная старонка модуля актыўнасьці';
+$string['page-my-index'] = 'Старонка прыладавай';
+$string['page-report-x'] = 'Усякая старонка справаздачы';
+$string['page-site-index'] = 'Толькі старонка дому';
+$string['page-site-x'] = 'Усякая старонка верхняга роўня сайта';
+$string['page-user-profile'] = 'Толькі старонкі профілю карыстальніка';
+$string['page-user-view'] = 'Толькі старонкі профілю карыстальніка';
+$string['page-user-x'] = 'Усе старонкі карыстальнікаў';
+$string['page-x'] = 'Усякая старонка';
