55a5b46b3857f0c52dcc70a7da35198026ed8320
Denis <denis.makouski@bseu.by>
2023-06-16 10:23:18 +0300

    ++

---
+++ перакладзена 4.0/notes.php
@@ -0,0 +1,78 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'notes', language 'en', version '4.0'.
+ *
+ * @package     notes
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addbulknote'] = 'Дадаць новую нататку {$a} людзям';
+$string['addbulknotedone'] = 'Нататка дадана {$a} людзям';
+$string['addbulknotedonesingle'] = 'Нататка дадана 1 асобе';
+$string['addbulknotesingle'] = 'Дадаць новую нататку 1 асобе';
+$string['addnewnote'] = 'Дадаць новую нататку';
+$string['addnewnoteselect'] = 'Абярыце карыстальнікаў, якім жадаеце надрукаваць нататку';
+$string['bynameondate'] = 'ад {$a->name} - {$a->date}';
+$string['configenablenotes'] = 'Уключыць захаваньне нататак пра асобных карыстальнікаў.';
+$string['content'] = 'Зьмесьціва';
+$string['course'] = 'курс';
+$string['coursenotes'] = 'Нататкі курса';
+$string['created'] = 'створана';
+$string['deleteconfirm'] = 'Выдаліць гэтую нататку?';
+$string['deletenotes'] = 'Выдаліць усе нататкі';
+$string['editnote'] = 'Рэдагаваць нататку';
+$string['enablenotes'] = 'Уключыць нататкі';
+$string['eventnotecreated'] = 'Нататка створана';
+$string['eventnotedeleted'] = 'Нататка выдалена';
+$string['eventnotesviewed'] = 'Нататка прагледжана';
+$string['eventnoteupdated'] = 'Нататка абноўлена';
+$string['invalidid'] = 'Пададзены хібны ID нататкі';
+$string['invaliduserid'] = 'Хібны id карыстальніка: {$a}';
+$string['myprofileownnotes'] = 'Мае нататкі';
+$string['nocontent'] = 'Зьмесьціва нататкі можа быць пустым';
+$string['nonotes'] = 'Нататкі гэтага тыпу пакуль адсутнічаюць';
+$string['nopermissiontodelete'] = 'Вы ня можаце выдаліць гэтую нататку';
+$string['note'] = 'Нататка';
+$string['notes'] = 'Нататкі';
+$string['notesdisabled'] = 'Нататкі адключаны, даруйце.';
+$string['notesnotvisible'] = 'Вам забаронена праглядаць нататкі.';
+$string['nouser'] = 'Вы мусіце абраць карыстальніка';
+$string['page-notes-index'] = 'Галоўная старонка нататак';
+$string['page-notes-x'] = 'Усякія старонкі нататак';
+$string['personal'] = 'асабістая';
+$string['personalnotes'] = 'Асабістыя нататкі';
+$string['privacy:metadata:core_notes'] = 'Кампанент нататак захоўвае нататкі карыстальнікаў у ядры падсістэмы.';
+$string['privacy:metadata:core_notes:content'] = 'Зьмесьціва нататкі';
+$string['privacy:metadata:core_notes:courseid'] = 'ID курса, зьвязанага з нататкай';
+$string['privacy:metadata:core_notes:created'] = 'Дата/час стварэньня нататкі';
+$string['privacy:metadata:core_notes:lastmodified'] = 'Дата/час апошняй зьмены нататкі';
+$string['privacy:metadata:core_notes:publishstate'] = 'Стан публікацыі нататкі';
+$string['privacy:metadata:core_notes:userid'] = 'ID карыстальніка, зьвязанага з нататкай';
+$string['publishstate'] = 'Кантэкст';
+$string['publishstate_help'] = 'Кантэкст нататкі вызначае, хто можа бачыць нататку ў штодзённым карыстаньні. Карыстальнікі мусяць разумець, што ўсе нататкі, уключаючы асабістыя, будуць паказвацца згодна з законамі і юрысдыкцыямі.
+
+* Асабістыя - Нататка будзе бачна толькі вам
+* Курс - Нататка будзе бачна ў гэтым курсе
+* Сайт - Нататка будзе бачна ўва ўсіх курсах';
+$string['site'] = 'сайт';
+$string['sitenotes'] = 'Нататкі сайта';
+$string['unknown'] = 'невядома';
