a0e2348c71d794f2939b68e20cb81cdc9c9ba6d9
Denis <denis.makouski@bseu.by>
2023-06-16 12:00:30 +0300

    ++

---
+++ перакладзена 4.0/privacy.php
@@ -0,0 +1,47 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'privacy', language 'en', version '4.0'.
+ *
+ * @package     privacy
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['broughtbymoodle'] = 'Гэты экспарт забясьпечаны Moodle.';
+$string['exportfrom'] = 'Экспарт з {$a}';
+$string['exporttime'] = 'Экспарт у {$a}';
+$string['exportuser'] = 'Дадзеныя для {$a}';
+$string['navigation'] = 'Навігацыя';
+$string['privacy:metadata'] = 'Падсістэма прыватнасьці не захоўвае аніякіх асабістых дадзеных і прызначана ў якасьці канала між кампанентамі і інтэрфэйсам, што ўжываецца для апісаньня, экспарту і выдаленьня сваіх дадзеных.';
+$string['privacy:subsystem:empty'] = 'Гэтая падсістэма не захоўвае аніякіх дадзеных.';
+$string['trace:deletingapproved'] = 'Зьдзяйсьненьне выдаленьня з {$a->total} ухваленных кантэкстаў ({$a->datetime})';
+$string['trace:deletingapprovedusers'] = 'Зьдзяйсьненьне выдаленьня карыстальнікаў з {$a->total} ухваленных кампанентаў для кантэксту {$a->contextid} ({$a->datetime}).';
+$string['trace:deletingcontext'] = 'Зьдзяйсьненьне выдаленьня кантэксту з {$a->total} кампанентаў ({$a->datetime})';
+$string['trace:deletinguser'] = 'Зьдзяйсьненьне выдаленьня карыстальнікаў з {$a->total} кампанентаў ({$a->datetime})';
+$string['trace:done'] = 'Выканана';
+$string['trace:exportcomplete'] = 'Экспарт выкананы';
+$string['trace:exportingapproved'] = 'Зьдзяйсьненьне першаснага экспарту для {$a->total} кампанентаў ({$a->datetime})';
+$string['trace:exportingrelated'] = 'Зьдзяйсьненьне зьвязанага экспарту для {$a->total} кампанентаў ({$a->datetime})';
+$string['trace:fetchcomponents'] = 'Атрыманьне дадзеных з {$a->total} кампанентаў ({$a->datetime})';
+$string['trace:finalisingexport'] = 'Фіналізацыя экспарту';
+$string['trace:preprocessingcomponent'] = 'Папярэдняя апрацоўка {$a->component} ({$a->progress}/{$a->total}) ({$a->datetime})';
+$string['trace:processingcomponent'] = 'Апрацоўка {$a->component} ({$a->progress}/{$a->total}) ({$a->datetime})';
+$string['viewdata'] = 'Націсьніце на спасылку ў навігацыі для прагляду дадзеных.';
