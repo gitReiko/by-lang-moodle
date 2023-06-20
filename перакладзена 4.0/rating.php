16dd44b8cfa492fada5beb01dee2b4ecd63e9573
Denis <denis.makouski@bseu.by>
2023-06-20 10:47:23 +0300

    ++

---
+++ перакладзена 4.0/rating.php
@@ -0,0 +1,65 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'rating', language 'en', version '4.0'.
+ *
+ * @package     rating
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['aggregateavg'] = 'Сярэдняе рэйтынгаў';
+$string['aggregatecount'] = 'Колькасьць рэйтынгаў';
+$string['aggregatemax'] = 'Максімальны рэйтынг';
+$string['aggregatemin'] = 'Мінімальны рэйтынг';
+$string['aggregatenone'] = 'Без рэйтынгу';
+$string['aggregatesum'] = 'Сума рэйтынгаў';
+$string['aggregatetype'] = 'Тып агрэгацыі';
+$string['aggregatetype_help'] = 'Тып агрэгаці вызначае шлях кабінаваньня рэйтынгаў у выніковую адзнаку ў журнале адзнак.
+
+* Сярэдняе рэйтынгаў - Сярэдняе значэньне ўсіх рэйтынгаў.
+* Колькасьць рэйтынгаў - Колькасьць прадметаў з рэйтынгам ператвараецца ў выніковую адзнаку. Зьвярніце ўвагу, выніковая адзнака ня можа перасягаць максімальную адзнаку за актыўнасьць.
+* Максімальны - Найвысокі рэйтынг становіцца выніковай адзнакай.
+* Мінімальны - Найнізкі рэйтынг становіцца выніковай адзнакай.
+* Сума - Усе рэйтынгі сумуюцца. Зьвярніце ўвагу, выніковая адзнака ня можа перасягаць максімальную адзнаку для актыўнасьці.
+
+Калі абрана "Без рэйтынгу", тады актыўнасьць ня будзе адлюстроўвацца ў журнале адзнак.';
+$string['allowratings'] = 'Дазволіць рэйтынгаваньне прадметаў?';
+$string['allratingsforitem'] = 'Усе адпраўленыя рэйтынгі';
+$string['capabilitychecknotavailable'] = 'Здольнасьць будзе недаступнай пакуль актыўнасьць не захаваная';
+$string['couldnotdeleteratings'] = 'Прабачце, гэтае нельга выдаліць, бо людзі ўжо адрэйтынгавалі гэтае';
+$string['norate'] = 'Рэйтынг прадметаў не дазволены!';
+$string['noratings'] = 'Рэйтынгі не адпраўленыя';
+$string['noviewanyrate'] = 'Вы можаце праглядваць толькі вынікі прадметаў, якія адрэйтынгавалі';
+$string['noviewrate'] = 'Вы ня маеце здольнсьці праглядваць рэйтынгі прадметаў';
+$string['privacy:metadata:rating'] = 'Уведзены карыстальнікам рэйтынг захоўваецца разам з прадметам, які мае рэйтынг.';
+$string['privacy:metadata:rating:rating'] = 'Лічбавы рэйтынг, што ўвёў карыстальнік.';
+$string['privacy:metadata:rating:timecreated'] = 'Час першага рэйтынгаваньня.';
+$string['privacy:metadata:rating:timemodified'] = 'Час апошняга абнаўленьня рэйтынга.';
+$string['privacy:metadata:rating:userid'] = 'Карыстальнік, што адрэйтынгаваў.';
+$string['rate'] = 'Рэйтынгаваць';
+$string['ratepermissiondenied'] = 'Вы ня маеце дазволу рэйтынгаваць гэты прадмет';
+$string['rating'] = 'Рэйтынг';
+$string['ratinginvalid'] = 'Рэйтынг хібны';
+$string['ratings'] = 'Рэйтынгі';
+$string['ratingtime'] = 'Абмежаваць рэйтынгі прадметаў з датамі ў гэтым дыяпазоне:';
+$string['rolewarning'] = 'Ролі з дазволам рэйтынгаваць';
+$string['rolewarning_help'] = 'Ролі з дазволам рэйтынгаваць - гэта ролі са здольнасьцю moodle/rating:rate плюс з любымі здольнасьцямі рэйтыгаваньня актыўнасьцяў. Вы можаце даць іншым ролям дазвол рэйтынгаваньня праз старонку Дазволы.';
+$string['scaleselectionrequired'] = 'Падчас абраньня тыпу агрыгацыі рэйтынгаў вы мусіце таксама абраць шкалу ці ўсталяваць максімальны бал.';
