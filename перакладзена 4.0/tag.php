d8cc70cf20323f4ab0efebaf55a4d389cd46042c
Denis <denis.makouski@bseu.by>
2023-06-21 11:45:05 +0300

    ++

---
+++ перакладзена 4.0/tag.php
@@ -0,0 +1,165 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'tag', language 'en', version '4.0'.
+ *
+ * @package     tag
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['added'] = 'Даданы стандартыя тэгі';
+$string['addotags'] = 'Дадаць стандартныя тэгі';
+$string['addtagcoll'] = 'Дадаць калекцыю тэгаў';
+$string['addtagtomyinterests'] = 'Дадаць "{$a}" у сьпіс маіх зацікаўленасьцяў';
+$string['alltagpages'] = 'Усе старонкі тэгаў';
+$string['backtoallitems'] = 'Вярнуцца да ўсіх прадметаў з тэгамі "{$a}"';
+$string['changename'] = 'Зьмяніць назву тэга';
+$string['changeshowstandard'] = 'Зьмяніць адлюстраваньне стандартных тэгаў у вобласьці {$a}';
+$string['changessaved'] = 'Зьмены захаваныя';
+$string['changetagcoll'] = 'Зьмяніць калекцыю тэгаў вобласьці {$a}';
+$string['changetype'] = 'Зьмяніць тып тэга';
+$string['collnameexplained'] = 'Пакінуць поле пустым, каб выкарыстаць стандартнае значэньне: {$a}';
+$string['combined'] = 'Тэгі аб\'яднаныя';
+$string['combineselected'] = 'Аб\'яднаць абранае';
+$string['component'] = 'Кампанент';
+$string['confirmdeletetag'] = 'Вы ўпэўненыя, што жадаеце выдаліць гэты тэг?';
+$string['confirmdeletetags'] = 'Вы ўпэўненыя, што жадаеце выдаліць абраныя тэгі?';
+$string['count'] = 'Колькасьць';
+$string['coursetags'] = 'Тэгі курса';
+$string['defautltagcoll'] = 'Стандартная калекцыя';
+$string['delete'] = 'Выдаліць';
+$string['deleted'] = 'Тэгі выдаленыя';
+$string['deletedcoursetags'] = 'Выдалена - Тэгі курса';
+$string['deleteselected'] = 'Выдаліць абранае';
+$string['description'] = 'Апісаньне';
+$string['editcollname'] = 'Рэдагаваць назву калекцыі тэгаў';
+$string['editisstandard'] = 'Зьмяніць стандартнае ўжываньне тэгаў';
+$string['editname'] = 'Рэдагаваць назву тэга';
+$string['editsearchable'] = 'Зьмяніць шуканае';
+$string['edittag'] = 'Рэдагаваць гэты тэг';
+$string['edittagcoll'] = 'Рэдагаваць калекцыі тэгаў {$a}';
+$string['edittagcollection'] = 'Зьмяніць калекцыі тэгаў';
+$string['entertags'] = 'Надрукуйце тэгі...';
+$string['errortagfrontpage'] = 'Тэгі на галоўнай старонцы сайту не дазволены';
+$string['errorupdatingrecord'] = 'Памылка абнаўленьня запісу тэга';
+$string['eventtagadded'] = 'Тэг даданы да прадмета';
+$string['eventtagcollcreated'] = 'Калекцыі тэгаў створана';
+$string['eventtagcolldeleted'] = 'Калекцыі тэгаў выдалена';
+$string['eventtagcollupdated'] = 'Калекцыі тэгаў абноўлена';
+$string['eventtagcreated'] = 'Тэг створаны';
+$string['eventtagdeleted'] = 'Тэг выдалены';
+$string['eventtagflagged'] = 'Тэг пазначаны сьцягам';
+$string['eventtagremoved'] = 'Тэг выдалены з прадмета';
+$string['eventtagunflagged'] = 'Тэг болей не пазначаны сьцягам';
+$string['eventtagupdated'] = 'Тэг абноўлены';
+$string['exclusivemode'] = 'Паказваць толькі з тэгамі {$a->tagarea}';
+$string['flag'] = 'Адзначыць сьцягам';
+$string['flagasinappropriate'] = 'Адзначыць сьцягам як недарэчнае';
+$string['flagged'] = 'Тэг адзначаны сьцягам';
+$string['helprelatedtags'] = 'Зьвязаныя тэгі, падзеленыя коскамі';
+$string['id'] = 'id';
+$string['inalltagcoll'] = 'Паўсюль';
+$string['inputstandardtags'] = 'Надрукуйце сьпіс новых тэгаў падзеленых коскамі';
+$string['itemstaggedwith'] = '{$a->tagarea} з тэгам "{$a->tag}"';
+$string['lesstags'] = 'меней...';
+$string['managestandardtags'] = 'Кіраваньне стандартнымі тэгамі';
+$string['managetagcolls'] = 'Кіраваньне калекцыямі тэгаў';
+$string['managetags'] = 'Кіраваньне тэгамі';
+$string['moretags'] = 'болей...';
+$string['name'] = 'Назва тэга';
+$string['namesalreadybeeingused'] = 'Назвы тэгаў ужо выкарыстоўваюцца';
+$string['nameuseddocombine'] = 'Назва тэгаў ужо выкарыстоўваецца. Вы жадаеце аб\'яднаць гэтыя тэгі?';
+$string['newcollnamefor'] = 'Новая назва для калекцыі тэгаў {$a}';
+$string['newnamefor'] = 'Новая назва для тэга {$a}';
+$string['nextpage'] = 'Болей';
+$string['noresultsfor'] = 'Вынікі для "{$a}" адсутнічаюць';
+$string['notagsfound'] = 'Супадзеньне тэгаў "{$a}" ня знойдзена';
+$string['nothingtoupdate'] = 'Няма чаго абнаўляць';
+$string['owner'] = 'Уласьнік';
+$string['page-tag-index'] = 'Старонка аднога тэга';
+$string['page-tag-manage'] = 'Старонка кіраваньня тэгаў';
+$string['page-tag-search'] = 'Старонка пошуку тэгаў';
+$string['page-tag-x'] = 'Усе старонкі тэгаў';
+$string['prevpage'] = 'Назад';
+$string['privacy:metadata:tag'] = 'Дэталі пра кожны ўнікальны тэг захоўваюцца разам з апісаньнем і іншай зьвязанай інфармацыяй';
+$string['privacy:metadata:tag:description'] = 'Апісаньне тэга.';
+$string['privacy:metadata:tag:flag'] = 'Ці пазначаны тэг сьцягам недарэчнае.';
+$string['privacy:metadata:tag:name'] = 'Назва тэга - гэта нармалізваная версія назвы.';
+$string['privacy:metadata:tag:rawname'] = 'Назва тэга - гэта назва, што адлюстроўваецца.';
+$string['privacy:metadata:tag:timemodified'] = 'Час апошняй зьмены тэга.';
+$string['privacy:metadata:tag:userid'] = 'ID карыстальніка, што стварыў тэг.';
+$string['privacy:metadata:taginstance'] = 'Спасылка між кожны тэгам і месцам выкарыстаньня.';
+$string['privacy:metadata:taginstance:ordering'] = 'Адносная чарга гэтага тэга.';
+$string['privacy:metadata:taginstance:tagid'] = 'Спасылка на тэг.';
+$string['privacy:metadata:taginstance:timecreated'] = 'Час спасыланьня гэтага тэга на мэту.';
+$string['privacy:metadata:taginstance:timemodified'] = 'Час зьмены гэтага тэга на мэту.';
+$string['privacy:metadata:taginstance:tiuserid'] = 'Месца кантэнту з супольным выкарыстаньнем, якому карыстальнікі могуць даць асабістыя тэгі, уласьнік экзэмпляру тэга захоўваецца.';
+$string['ptags'] = 'Тэгі, вызначаныя карыстальнікам (Падзеленыя коскай)';
+$string['relatedblogs'] = 'Апошнія запісі блогу';
+$string['relatedtags'] = 'Зьвязаныя тэгі';
+$string['removetagfrommyinterests'] = 'Выдаліць "{$a}" з маіх зацікаўленасьцяў';
+$string['reset'] = 'Скінуць сьцяг тэгу';
+$string['resetfilter'] = 'Скінуць фільтар';
+$string['resetflag'] = 'Скінуць сьцяг';
+$string['responsiblewillbenotified'] = 'Адказная асоба будзе абвешчана';
+$string['rssdesc'] = 'Гэты RSS-стужка была аўтаматычна згенеравана Moodle і ўтрымлівае створаныя карыстальнікам тэгі для курсаў.';
+$string['rsstitle'] = 'RSS-стужка тэгаў курса для карыстальніка: {$a}';
+$string['search'] = 'Пошук';
+$string['searchable'] = 'Шуканы';
+$string['searchable_help'] = 'Калі сьцяжок усталяваны, тэгі гэтай калекцыі льга знайсьці праз старонку "Пошук тэгаў". Калі сьцяжок не ўсталяваны, тэгі па-ранейшаму будзе льга знайсьці праз іншыя старонкі курса.';
+$string['searchresultsfor'] = 'Шукаць вынікі для "{$a}"';
+$string['searchtags'] = 'Пошук тэгаў';
+$string['seeallblogs'] = 'Праглядзець запісы блога з тэгам "{$a}"';
+$string['select'] = 'Абраць';
+$string['selectcoll'] = 'Абраць калекцыю тэгаў';
+$string['selectmaintag'] = 'Абярыце тэг, які будзе выкарыстоўвацца пасьля аб\'яднаньня';
+$string['selectmultipletags'] = 'Калі ласка, абярыце болей аднога тэга';
+$string['selecttag'] = 'Абраць тэг {$a}';
+$string['settypedefault'] = 'Выдаліць са стандартных тэгаў';
+$string['settypestandard'] = 'Зрабіць стандартным';
+$string['showingfirsttags'] = 'Паказваць {$a} найпапулярных тэгаў';
+$string['showstandard'] = 'Выкарыстаньне стандартных тэгаў';
+$string['showstandard_help'] = 'Падчас друку тэгаў могуць быць прапанаваны ці прымушаны стандартныя тэгі, тое значыць, што новыя тэгі могуць быць не прынятымі. У якасьці альтэрнацівы, льга дазволіць ставараць новыя тэгі без прапановы стандартных.';
+$string['standardforce'] = 'Прымушаць';
+$string['standardhide'] = 'Не прапаноўваць';
+$string['standardsuggest'] = 'Прапаноўваць';
+$string['standardtag'] = 'Стандартнае';
+$string['suredeletecoll'] = 'Вы ўпэўненыя, што жадаеце выдаліць калекцыю тэгаў "{$a}"?';
+$string['tag'] = 'Тэг';
+$string['tagarea_blog_external'] = 'Пасты вонкавых блогаў';
+$string['tagarea_course'] = 'Курсы';
+$string['tagarea_course_modules'] = 'Актыўнасьці ды рэсурсы';
+$string['tagarea_post'] = 'Пасты блогу';
+$string['tagarea_user'] = 'Зацікаўленасьці карыстальніка';
+$string['tagareaenabled'] = 'Уключана';
+$string['tagareaname'] = 'Назва';
+$string['tagareas'] = 'Вобласьці тэгаў';
+$string['tagcollection'] = 'Калекцыя тэгаў';
+$string['tagcollection_help'] = 'Калекцыі тэгаў - гэта зборы тэгаў для розных вобласьцяў. Напрыклад, калекцыю стандартных тэгаў льга выкарыстоўваць для тэгаў курса з зацікаўленасьцямі карыстальнакаў, а пасты блогу льга захоўваць у асобных калекцыях. Калі карыстальнік націскае на тэг, на старонцы тэгаў адлюстроўваюцца толькі прадметы з тэгамі той жа калекцыі. Тэгі могуць аўтаматычна дадавацца да калекцыі ў адпаведнасьці з вобласьцю тэгаў ці могуць быць даданы ўручную як стандартныя тэгі.';
+$string['tagcollections'] = 'Калекцыі тэгаў';
+$string['tagdescription'] = 'Апісаньне тэгаў';
+$string['tags'] = 'Тэгі';
+$string['tagsaredisabled'] = 'Тэгі адключаныя';
+$string['thingstaggedwith'] = '"{$a->name}" выкарыстоўваецца {$a->count} разоў';
+$string['thingtaggedwith'] = '"{$a->name}" выкарыстоўваецца аднойчы';
+$string['timemodified'] = 'Зьменена';
+$string['typechanged'] = 'Тып тэгу зьменены';
+$string['updatetag'] = 'Абнавіць';
