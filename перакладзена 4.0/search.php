0a5336c212055c3302e3fdbafb4818fce73f964b
Denis <denis.makouski@bseu.by>
2023-06-21 10:10:45 +0300

    ++

---
+++ перакладзена 4.0/search.php
@@ -0,0 +1,148 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'search', language 'en', version '4.0'.
+ *
+ * @package     search
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['advancedsearch'] = 'Прасунуты пошук';
+$string['all'] = 'Усе';
+$string['allareas'] = 'Усе вобласьці';
+$string['allcourses'] = 'Усе курсы';
+$string['allusers'] = 'Усе карыстальнікі';
+$string['author'] = 'Аўтар';
+$string['authorname'] = 'Імя аўтара';
+$string['back'] = 'Назад';
+$string['beadmin'] = 'Вы мусіце быць адмінам, каб выкарыстоўваць гэтую старонку.';
+$string['checkdb'] = 'Праверыць базу дадзеных';
+$string['checkdbadvice'] = 'Праверыць вашую базу дадзеных на магчымыя праблемы.';
+$string['checkdir'] = 'Праверыць тэчку';
+$string['checkdiradvice'] = 'Пераканайцеся, што тэчка з дадзенымі існуе і ў яе льга запісваць.';
+$string['commenton'] = 'Каментаваньне ўключана';
+$string['confirm_delete'] = 'Вы ўпэўненыя, што жадаеце выдаліць індэкс {$a}? Пакуль вобласьць пошуку ня будзе праіндэксавана, карыстальнікі ня будуць атрымліваць вынікі пошуку з гэтай вобласьці.';
+$string['confirm_deleteall'] = 'Вы ўпэўненыя, што жадаеце выдаліць усё праіндэксавае зьмесьціва зараз? Пакуль сайт ня будзе наноў праіндэксаваны, карыстальнікі не атрымаюць вынікі пошуку.';
+$string['confirm_indexall'] = 'Вы ўпэўненыя, што жадаеце праіндэксаваць зьмесьціва зараз? Калі шмат зьмесьціва патрабуе індэксаваньня, тое можа заняць шмат часу. Для дзеючых сэрвераў звычайна варта пакінуць інэксаваньне праз запланаванае заданьне \'Глабальнае індэксаваньне пошуку\'.';
+$string['confirm_reindexall'] = 'Вы ўпэўненыя, што жадаеце пераіндэксаваць усё зьмесьціва сайту зараз? Калі вашы сайт утрымлівае вялікую колькасьць зьмесьціва, тое зойме шмат часу, і карыстальнікі ня змогуць атрымаць поўныя вынікі пошуку да завяршэньня працэсу.';
+$string['content:courserole'] = '{$a->role} у {$a->course}';
+$string['core-all'] = 'Усе';
+$string['core-course-content'] = 'Зьмесьціва курса';
+$string['core-courses'] = 'Курсы';
+$string['core-other'] = 'Іншае';
+$string['core-users'] = 'Карыстальнікі';
+$string['createanindex'] = 'стварыць індэкс';
+$string['createdon'] = 'Створана';
+$string['database'] = 'База дадзеных';
+$string['databasestate'] = 'Індэксаваньне статусу базы дадзеных';
+$string['datadirectory'] = 'Тэчка дадзеных';
+$string['deleteindex'] = 'Выдаліць індэкс {$a}';
+$string['deletionsinindex'] = 'Выдаленьне ў індэксе';
+$string['docmodifiedon'] = 'Апошняя зьмена {$a}';
+$string['doctype'] = 'Тып Дакуманта';
+$string['doctypenotsupported'] = 'Азначаны тып дакуманта пакуль не падтрымліваецца';
+$string['documents'] = 'дакуманты';
+$string['documentsfor'] = 'Дакуманты для';
+$string['documentsindatabase'] = 'Дакуманты ў базе дадзеных';
+$string['documentsinindex'] = 'Дакуманты ў індэксе';
+$string['duration'] = 'Працягласьць';
+$string['emptydatabaseerror'] = 'Табліца базы дадзеных адсутнічае ці не ўтрымлівае запісаў індэксу.';
+$string['enginenotfound'] = 'Рухавік {$a} ня знойдзены.';
+$string['enginenotinstalled'] = 'Рухавік {$a} не ўсталяваны.';
+$string['enginenotselected'] = 'Вы не абралі анкіякага рухавіка пошуку.';
+$string['engineserverstatus'] = 'Рухавік пошуку не даступны. Калі ласка, зьвяжыцеся з вашым адміністратарам.';
+$string['enteryoursearchquery'] = 'Надрукуйце вашы пошукавы запыт';
+$string['error_indexing'] = 'Адбылася памылка падчас індэксаваньня';
+$string['errorareanotavailable'] = 'вобласьць пошуку {$a} не даступная.';
+$string['errors'] = 'Памылкі';
+$string['everywhere'] = 'Паўсюль дзе вы маеце доступ';
+$string['filesinindexdirectory'] = 'Файлы ў тэчцы індэксаваньня';
+$string['filterheader'] = 'Фільтар';
+$string['fromtime'] = 'Зьмена пасьля';
+$string['globalsearch'] = 'Глабальны пошук';
+$string['globalsearchdisabled'] = 'Глабальны пошук не даступны.';
+$string['gradualreindex'] = 'Паступовае пераіндэксаваньне {$a}';
+$string['gradualreindex_confirm'] = 'Вы ўпэўненыя, што жадаеце пераіндэксаваць {$a}? Тое зойме нейкі час, але існыя дадзеныя застануцца даступнымі падчас пераіндэксаваньня.';
+$string['gradualreindex_queued'] = 'Запытана пераіндэксаваньне для {$a->name} ({$a->count} кантэкстаў). Гэтае індэксаваньне будзе выканана запланаваным заданьнем "Глабальнае індэксаваньне пошуку".';
+$string['incourse'] = 'у курсе {$a}';
+$string['index'] = 'Індэкс';
+$string['indexwhendisabledfullnotice'] = 'У гэты час індэксаваньне не дазволена, бо пошук адключаны. Каб уключыць пошук, знайдзіце наладу <a href="{$a->url}">searchindexwhendisabled</a>.';
+$string['indexwhendisabledshortnotice'] = 'Індэксаваньне не даступнае.';
+$string['invalidindexerror'] = 'Тэчка індэксаваньня ліба ўтрымлівае хібны індэкс, ліба навогул нічога не ўтрымлівае.';
+$string['ittook'] = 'Патрабуецца';
+$string['matchingfile'] = 'Супастаўлена з файлам <span class="filename">{$a}</span>';
+$string['matchingfiles'] = 'Супастаўлена з файламі:';
+$string['mycoursesonly'] = 'Толькі мае курсы';
+$string['next'] = 'Уперад';
+$string['noindexmessage'] = 'Адмін: Здаецца пошукавы індэкс адсутнічае. Калі ласка,';
+$string['noresults'] = 'Вынікі адсутнічаюць';
+$string['normalsearch'] = 'Нармальны пошук';
+$string['notitle'] = 'Назва адсутнічае';
+$string['openedon'] = 'адкрыта на';
+$string['optimize'] = 'Аптымізаваць';
+$string['order'] = 'Чарга вынікаў';
+$string['order_location'] = 'Прыярытэт аддаёцца вынікам, зьвязаным з {$a}';
+$string['order_relevance'] = 'Спачатку больш рэлевантныя вынікі';
+$string['priority'] = 'Прыярытэт';
+$string['priority_normal'] = 'Нармальны';
+$string['priority_reindexing'] = 'Пераіндэксаваньне';
+$string['privacy:metadata'] = 'Падсістэма пошуку не захоўвае аніякіх асабістых дадзеных.';
+$string['progress'] = 'Прагрэс';
+$string['queryerror'] = 'Уведзены вамі запыт ня можа быць апрацаваны рухавіком пошуку: {$a}';
+$string['queueheading'] = 'Дадатковая чарга індэксаваньня ({$a} прадметаў)';
+$string['resultsreturnedfor'] = 'вынікі вернутыя для';
+$string['runindexer'] = 'Запусьціць індэксатар (рэальна)';
+$string['runindexertest'] = 'Запусьціць тэст індэксатару';
+$string['schemanotupdated'] = 'Схема пошуку састарэла.';
+$string['schemaversionunknown'] = 'Пошукавы рухавік ня ведае пра цяперашнюю версію схемы.';
+$string['score'] = 'Вынік';
+$string['search'] = 'Пошук';
+$string['search:course'] = 'Курсы';
+$string['search:course_teacher'] = 'Настаўнік курса';
+$string['search:customfield'] = 'Уласныя палі курса';
+$string['search:message_received'] = 'Паведамленьні - атрыманы';
+$string['search:message_sent'] = 'Паведамленьні - адпраўлены';
+$string['search:mycourse'] = 'Мае курсы';
+$string['search:section'] = 'Падзелы курса';
+$string['search:user'] = 'Карыстальнікі';
+$string['searcharea'] = 'Вобласьць пошуку';
+$string['searchareacategories'] = 'Катэгорыі вобласьці пошуку';
+$string['searching'] = 'Пошук у ...';
+$string['searchnotpermitted'] = 'Вам не дазволена ажыцяўляць пошук';
+$string['searchsetupdescription'] = 'Наступныя крокі дапамогуць вам наладзіць глабальны пошук у Moodle.';
+$string['searchwithin'] = 'Шукаць у межах';
+$string['seconds'] = 'сякундаў';
+$string['solutions'] = 'Вынахады';
+$string['statistics'] = 'Статыстыка';
+$string['step'] = 'Крок';
+$string['thesewordshelpimproverank'] = 'Наступныя словы дапамагаюць палепшаць рэйтынг';
+$string['thesewordsmustappear'] = 'Наступныя словы мусяць быць';
+$string['thesewordsmustnotappear'] = 'Наступныя словы ня мусяць быць';
+$string['title'] = 'Назва';
+$string['tofetchtheseresults'] = 'каб атрымаць гэтыя вынікі';
+$string['topresults'] = 'Лепшыя вынікі';
+$string['totalsize'] = 'Агульны памер';
+$string['totime'] = 'Зьмена да';
+$string['type'] = 'Тып';
+$string['uncompleteindexingerror'] = 'Індэксаваньне не было пасьпяхова выканана, калі ласка, перазапусьціце яго.';
+$string['versiontoolow'] = 'Прабачце, глабальны пошук патрабуе PHP 5.0.0 ці вышэйшы';
+$string['viewresultincontext'] = 'Праглядець вынікі ў кантэксьце';
+$string['wordsintitle'] = 'Словы ў назьве';
