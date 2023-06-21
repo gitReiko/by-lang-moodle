7b7af0561bef079a58fef4519b4b3fd765f88390
Denis <denis.makouski@bseu.by>
2023-06-21 16:06:04 +0300

    ++

---
+++ перакладзена 4.0/userkey.php
@@ -0,0 +1,51 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'userkey', language 'en', version '4.0'.
+ *
+ * @package     userkey
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['adduserkey'] = 'Дадаць ключ карыстальніка';
+$string['createnewkey'] = 'Стварыць новы ключ карыстальніка';
+$string['createuserkey'] = 'Стварыць ключ карыстальніка';
+$string['deletekeyconfirm'] = 'Вы сапраўды, жадаеце выдаліць гэты ключ карыстальніка?';
+$string['deleteuserkey'] = 'Выдаліць ключ карыстальніка';
+$string['edituserkey'] = 'Рэдагаваць ключ карыстальніка';
+$string['keyiprestriction'] = 'Абмежаваньне ключоў па IP';
+$string['keyiprestriction_help'] = 'Надрукуйце адмысловы IP адрас ці дыяпазон IP адрасоў, што будуць адзінымі IP адрасамі, якім дазволены доступ да гэтых дадзеных. Пакіньце пустым, каб адключыць абмежаваньне IP адраў (не рэкамендуецца).';
+$string['keymanager'] = 'Кіраўнік ключамі';
+$string['keyvaliduntil'] = 'Ключ валідны да';
+$string['keyvaliduntil_help'] = 'Абярыце дадатковую дату, пасьля якой ключ перастане быць валідным (рэкамендуецца для дадатковай бясьпекі).';
+$string['keyvalue'] = 'Значэньне ключа';
+$string['newuserkey'] = 'Новы ключ карыстальніка';
+$string['privacy:metadata:user_private_key'] = 'Прыватныя ключы для карыстальніка.';
+$string['privacy:metadata:user_private_key:instance'] = 'Экзэмпляр скрыпту.';
+$string['privacy:metadata:user_private_key:iprestriction'] = 'Дыяпазон IP адрасоў, зь якім можа выкарыстоўвацца гэты ключ.';
+$string['privacy:metadata:user_private_key:script'] = 'Скрыпт, які адказвае за ключ карыстальніка.';
+$string['privacy:metadata:user_private_key:timecreated'] = 'Дата і час стварэньня ключа.';
+$string['privacy:metadata:user_private_key:userid'] = 'Карыстальнік, зьвязаный з ключом.';
+$string['privacy:metadata:user_private_key:validuntil'] = 'Дата, да якой прыватны ключ валідны.';
+$string['privacy:metadata:user_private_key:value'] = 'Значэньне ключа.';
+$string['userkey'] = 'Ключ карыстальніка';
+$string['userkey_help'] = 'Абярыце захаваны ключ, які дасьць карыстальнікам доступ да дадзеных, апублікаваных гэтым плагінам экспарту без патрэбы ўваходу ў Moodle. Абярыце "Стварыць новы ключ карыстальніка", каб згенераваць новы ключ падчас адпраўленьня гэтай формы.';
+$string['userkeys'] = 'Ключы карыстальніка';
