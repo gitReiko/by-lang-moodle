f8bc9be1f9606245962bc7cc56068c585932ac0f
Denis <denis.makouski@bseu.by>
2023-06-22 15:17:46 +0300

    ++

---
+++ перакладзена 4.0/assignfeedback_file.php
@@ -0,0 +1,58 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'assignfeedback_file', language 'en', version '4.0'.
+ *
+ * @package     assignfeedback_file
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['batchoperationconfirmuploadfiles'] = 'Запампаваць адзін ці некалькі файлаў водгукаў для ўсіх абраных карыстальнікаў?';
+$string['batchuploadfiles'] = 'Запампаваць файлы водгукаў для некалькіх карыстальнікаў';
+$string['batchuploadfilesforusers'] = 'Адправіць файлы водгукаў да {$a} абраных карыстальнікаў.';
+$string['configmaxbytes'] = 'Максімальны памер файлу';
+$string['confirmuploadzip'] = 'Пацьвердзіць zip запампоўку';
+$string['countfiles'] = '{$a} файлаў';
+$string['default'] = 'Уключана па стандарту';
+$string['default_help'] = 'Калі ўсталявана, гэты метад водгуку будзе ўключаны па стандарту для ўсіх заданьняў.';
+$string['enabled'] = 'Водгук файл';
+$string['enabled_help'] = 'Калі ўключана, настаўнік будзе здольны запампоўваць файлы з водгукамі падчас ацэньваньня прадстаўленьняў заданьняў. Гэтыя файлы здольны быць, але не абмежаваны, ацэненымі прадстаўленьнямі студэнтаў, дакументамі з каментарамі ці агучанымі аўдыё водгукамі.';
+$string['feedbackfileadded'] = 'Новы файл водгука "{$a->filename}" для студэнта "{$a->student}"';
+$string['feedbackfileupdated'] = 'Зьменены файл водгука "{$a->filename}" для студэнта "{$a->student}"';
+$string['feedbackzip'] = 'Zip файл з файламі водгукаў';
+$string['feedbackzip_help'] = 'Zip файл утрымлівае сьпіс файлаў водгукаў для аднога ці некалькіх студэнтаў. Файлы водгукаў будуць прызначаны студэнтам на грунце ID карыстальніка, які мусіць быць другой часткай назвы файлу адразу пасьля поўнага імя карыстальніка. Гэтае пагадненьне названьня выкарыстоўваецца падчас спампоўкі прадстаўленьняў, каб вы былі здольны спампаваць усе прадстаўленьні, дадаць каментары да файлаў, а потым паўторна заархіваваць і запампаваць усе файлы. Файлы бяз зьменаў будуць праігнараваны.';
+$string['file'] = 'Файлы водгукаў';
+$string['filesadded'] = 'Файлы водгукаў даданы: {$a}';
+$string['filesupdated'] = 'Файлы водгукаў абноўлены: {$a}';
+$string['importfeedbackfiles'] = 'Імпартаваць файлы водгукаў';
+$string['maxbytes'] = 'Максімальны памер файлу';
+$string['maxfiles'] = 'Максімальная колькасьць пампаваных файлаў';
+$string['maximumsize'] = 'Максімальны памер файлу';
+$string['moreusers'] = 'яшчэ {$a}...';
+$string['nochanges'] = 'Бяз зьменаў';
+$string['pluginname'] = 'Водгук файл';
+$string['privacy:metadata:filepurpose'] = 'Файлы водгукаў ад настаўніка для студэнта.';
+$string['privacy:path'] = 'Файлы водгукаў';
+$string['selectedusers'] = 'Абраныя карыстальнікі';
+$string['uploadfiles'] = 'Адправіць файлы водгукаў';
+$string['uploadzip'] = 'Запампаваць некалькі файлаў водгукаў у zip';
+$string['uploadzipsummary'] = 'Файлы водгукаў імпартаваны з zip';
+$string['userswithnewfeedback'] = 'Карыстальнікі з абноўленым водгукам: {$a}';
