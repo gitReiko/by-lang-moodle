0cb6cad75330395de2683bab1511cfe8dc6b23f2
Denis <denis.makouski@bseu.by>
2023-06-22 12:25:19 +0300

    ++

---
+++ перакладзена 4.0/assignfeedback_editpdf.php
@@ -0,0 +1,110 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'assignfeedback_editpdf', language 'en', version '4.0'.
+ *
+ * @package     assignfeedback_editpdf
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addtoquicklist'] = 'Дадаць да хуткага сьпісу';
+$string['annotationcolour'] = 'Колер анатацыя';
+$string['black'] = 'Чорны';
+$string['blue'] = 'Блакітны';
+$string['cannotopenpdf'] = 'Нельга адкрыць PDF. Файл можа быць пашкоджаным ці мець непадтрымліваемы фармат.';
+$string['clear'] = 'Ачысьціць';
+$string['colourpicker'] = 'Выбарнік колеру';
+$string['command'] = 'Каманда:';
+$string['comment'] = 'Каментары';
+$string['commentcolour'] = 'Колер каментару';
+$string['commentcontextmenu'] = 'Кантэкснае меню каментару';
+$string['commentindex'] = 'Індэкс каментару';
+$string['commentlabel'] = '{$a->pnum}.{$a->cnum}';
+$string['couldnotsavepage'] = 'Нельга захаваць старонку {$a}';
+$string['currentstamp'] = 'Пячатка';
+$string['default'] = 'Уключана па стандарту';
+$string['default_help'] = 'Калі ўсталявана, гэты метад водгуку будзе ўключаны па стандарту для ўсіх заданьняў.';
+$string['deleteannotation'] = 'Выдаліць анатацыю';
+$string['deletecomment'] = 'Выдаліць каментар';
+$string['deletefeedback'] = 'Выдаліць PDF водгук';
+$string['downloadablefilename'] = 'водгук.pdf';
+$string['downloadfeedback'] = 'Спампаваць PDF водгук';
+$string['draftchangessaved'] = 'Чарнавая анатацыя захаваная';
+$string['drag'] = 'Перасунуць';
+$string['editpdf'] = 'PDF анатацыя';
+$string['editpdf_help'] = 'Анатаваньне прадстаўленьняў студэнтаў непасрэдна ў браўзары і стварэньне рэдагуемага пампаванага PDF.';
+$string['enabled'] = 'PDF анатацыя';
+$string['enabled_help'] = 'Калі ўключана, настаўнік будзе здольны ствараць анатацыю PDF файлам падчас ацэньваньня прадстаўленьняў заданьняў. Гэта дазволе настаўніку дадаваць каментары, маляваньні і пячаткі непасрэдна над працай студэнта. Анатаваньне зьдзяйсьняецца ў браўзары, ніякай дадатковай праграмы не патрабуецца.';
+$string['errorgenerateimage'] = 'Памылка генераваньня выявы ghostscript, адладачная інфа: {$a}';
+$string['errorpdfpage'] = 'Адбылася памылка падчас генераваньня гэтай старонкі.';
+$string['expcolcomments'] = 'Разгарнуць/згарнуць усе каментары';
+$string['filter'] = 'Фільтраваць каментары...';
+$string['generatefeedback'] = 'Генераваць PDF водгук';
+$string['generatingpdf'] = 'Генераваньне PDF...';
+$string['gotopage'] = 'Ісьці да старонкі';
+$string['green'] = 'Зялёны';
+$string['gsimage'] = 'Тэставая выява ghostscript';
+$string['highlight'] = 'Вылучыць';
+$string['jsrequired'] = 'JavaScript патрабуецца для анатаваньня PDF. Калі ласка, уключыце JavaScript у вашым браўзары, каб выкарыстоўваць гэтую функцыю.';
+$string['launcheditor'] = 'Запусьціць PDF редактор...';
+$string['line'] = 'Лінія';
+$string['loadingeditor'] = 'Загрузка PDF редактара';
+$string['navigatenext'] = 'Наступная старонка (Alt/Shift-Alt/Ctrl-Option + {$a})';
+$string['navigateprevious'] = 'Папярэдняя старонка (Alt/Shift-Alt/Ctrl-Option + {$a})';
+$string['output'] = 'Выхад:';
+$string['oval'] = 'Авал';
+$string['pagenumber'] = 'Старонка {$a}';
+$string['pagexofy'] = 'Старонка {$a->page} з {$a->total}';
+$string['partialwarning'] = 'Частка файлаў у гэтым прадстаўленьні можа быць даступнай толькі шляхам непасрэднай спампоўкі.';
+$string['pathtogspathdesc'] = 'Зьвярніце ўвагу, што PDF анатацыя патрабуе, каб шлях да ghostscript быў усталяваны ў {$a}.';
+$string['pen'] = 'Пяро';
+$string['pluginname'] = 'PDF анатацыя';
+$string['preparesubmissionsforannotation'] = 'Падрыхтуйце прадстаўленьні для анатацыі';
+$string['privacy:metadata:colourpurpose'] = 'Колер каментару для анатацыі';
+$string['privacy:metadata:conversionpurpose'] = 'Файлы канвертуюцца ў PDF, тое дазваляе рабіць анатацыі.';
+$string['privacy:metadata:filepurpose'] = 'Захоўвае анатаваны PDF з водгукам для карыстальніка.';
+$string['privacy:metadata:rawtextpurpose'] = 'Захоўвае чарнавы тэкст для хуткіх дадзеных.';
+$string['privacy:metadata:tablepurpose'] = 'Захоўвае азначаныя настаўнікам каментары хуткага сьпісу';
+$string['privacy:metadata:userid'] = 'ID карыстальніка';
+$string['privacy:path'] = 'PDF водгук';
+$string['rectangle'] = 'Прастакутнік';
+$string['red'] = 'Чырвоны';
+$string['result'] = 'Вынік:';
+$string['rotateleft'] = 'Павярнуць на 90 градусаў налева';
+$string['rotateright'] = 'Павярнуць на 90 градусаў направа';
+$string['searchcomments'] = 'Шукаць каментары';
+$string['select'] = 'Абраць';
+$string['stamp'] = 'Пячатка';
+$string['stamppicker'] = 'Выбарнік пячаткі';
+$string['stamps'] = 'Пячаткі';
+$string['stampsdesc'] = 'Пячаткі мусяць быць файламі-выявамі (рэкамендаваны памер: 40x40). Гэтыя выявы льга выкарыстоўваць з дапамогай прылады пячатка падчас анатаваньня PDF.';
+$string['test_doesnotexist'] = 'Шлях ghostscript указвае на няісны файл';
+$string['test_empty'] = 'Шлях ghostscript пусты. Калі ласка, надрукуйце слушны шлях';
+$string['test_isdir'] = 'Шлях ghostscript указвае на тэчку, калі ласка, дадайце ghostscript у азначаны шлях';
+$string['test_notestfile'] = 'Тэставы PDF адсутнічае';
+$string['test_notexecutable'] = 'ghostscript указвае на файл, які нельга выканаць';
+$string['test_ok'] = 'Здаецца, шлях да ghostscript слушны, калі ласка, праверце, ці бачыце вы паведамленьне на ніжэйшай выяве';
+$string['testgs'] = 'Тэставаць шлях ghostscript';
+$string['tool'] = 'Прылада';
+$string['toolbarbutton'] = '{$a->tool} {$a->shortcut}';
+$string['viewfeedbackonline'] = 'Глядзець анатаваны PDF...';
+$string['white'] = 'Белы';
+$string['yellow'] = 'Жоўты';
