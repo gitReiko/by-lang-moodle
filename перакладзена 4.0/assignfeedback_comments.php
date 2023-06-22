1103f4b5fa1470439b9fe2e783ad5bb05234ab9c
Denis <denis.makouski@bseu.by>
2023-06-22 11:20:16 +0300

    ++

---
+++ перакладзена 4.0/assignfeedback_comments.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'assignfeedback_comments', language 'en', version '4.0'.
+ *
+ * @package     assignfeedback_comments
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['commentinline'] = 'Каментаваць у радке';
+$string['commentinline_help'] = 'Калі гэтая налада ўключана, тэкст прадстаўленьня будзе скапіяваны ў водгук каментара падчас ацэньваньня, што палягчае каментаваньне ў радке (магчыма, выкарыстаньне іншага колеру) ці рэдагаваньне арыгінальнага тэксту.';
+$string['commentinlinedefault'] = 'Каментаваць у радке па стандарту';
+$string['commentinlinedefault_help'] = 'Калі ўсталявана, функцыя каментаваньня ў радке будзе ўключана па стандарту для ўсіх новых заданьняў.';
+$string['default'] = 'Уключана па стандарту';
+$string['default_help'] = 'Калі ўсталявана, гэты метад водгукаў будзе ўключаны па стандарту для ўсіх новых заданьняў.';
+$string['enabled'] = 'Каментары водгукаў';
+$string['enabled_help'] = 'Калі ўключана, паказьнік можа пакідаць каментары водгукаў да кожнага прадстаўленьня.';
+$string['pluginname'] = 'Каментары водгукаў';
+$string['privacy:commentpath'] = 'Каментары водгукаў';
+$string['privacy:metadata:assignmentid'] = 'ID заданьня';
+$string['privacy:metadata:commentpurpose'] = 'Тэкст каментару.';
+$string['privacy:metadata:filepurpose'] = 'Файлы водгукаў ад настаўніка да студэнта.';
+$string['privacy:metadata:gradepurpose'] = 'ID адзнакі, зьвязанай з каментаром.';
+$string['privacy:metadata:tablesummary'] = 'Тут захоўваюцца каментары, зробленыя ацэньнікамі ў якасьці водгукаў для прадстаўленьняў студэнтаў.';
