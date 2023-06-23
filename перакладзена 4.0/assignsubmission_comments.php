03386b1029769e7fe214bbec1edec50ab3a1c612
Denis <denis.makouski@bseu.by>
2023-06-23 09:40:27 +0300

    ++

---
+++ перакладзена 4.0/assignsubmission_comments.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'assignsubmission_comments', language 'en', version '4.0'.
+ *
+ * @package     assignsubmission_comments
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['blindmarkingname'] = 'Удзельнік {$a}';
+$string['blindmarkingviewfullname'] = 'Удзельнік {$a->participantnumber} ({$a->participantfullname})';
+$string['default'] = 'Уключана па стандарту';
+$string['default_help'] = 'Калі ўключана, гэты метад прадстаўленьня будзе ўключаны аўтаматычна для ўсіх новых заданьняў.';
+$string['enabled'] = 'Каментары прадстаўленьня';
+$string['enabled_help'] = 'Калі ўключана, студэнты змогуць пакідаць каментары да сваіх уласных прадстаўленьняў. Напрыклад, гэта льга скарыстаць для азначэньня галоўнага файлу падчас адпраўленьня узаемазьвязаных файлаў.';
+$string['pluginname'] = 'Каментары прадстаўленьня';
+$string['privacy:metadata:commentpurpose'] = 'Каментары прадстаўленьня паміж студэнтам і настаўнікам.';
