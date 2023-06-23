a7e8467e45ea8e63b40c52d8d9e88a1d46564c27
Denis <denis.makouski@bseu.by>
2023-06-23 10:06:04 +0300

    ++

---
+++ перакладзена 4.0/assignsubmission_file.php
@@ -0,0 +1,50 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'assignsubmission_file', language 'en', version '4.0'.
+ *
+ * @package     assignsubmission_file
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['acceptedfiletypes'] = 'Прымальныя тыпы файлаў';
+$string['acceptedfiletypes_help'] = 'Прымальныя тыпы файлаў льга абмежаваць шляхам друку сьпіса пашырэньняў файлаў. Калі поле пакінуць пустым, то ўсе тыпы файлаў будуць прымальныя.';
+$string['configmaxbytes'] = 'Максімальны памер файлу';
+$string['countfiles'] = '{$a} файлаў';
+$string['default'] = 'Уключана па стандарту';
+$string['default_help'] = 'Калі ўсталявана, гэты метад адпраўленьня будзе ўключаны па стандарту для ўсіх новых прадстаўленьняў.';
+$string['defaultacceptedfiletypes'] = 'Стандартныя прымальныя тыпы файлаў';
+$string['enabled'] = 'Прадстаўленьні-файлы';
+$string['enabled_help'] = 'Калі ўключана, студэнты здольныя запампоўваць адзін ці некалькі файлаў у якасьці сваіх прадстаўленьняў.';
+$string['eventassessableuploaded'] = 'Файл быў запампаваны.';
+$string['file'] = 'Прадстаўленьні-файлы';
+$string['maxbytes'] = 'Максімальны памер файлу';
+$string['maxfiles'] = 'Максімальная колькасьць файлаў у прадстаўленьні';
+$string['maxfiles_help'] = 'Калі ўключаны прадстаўленьні-файлы, для кожнага заданьня льга ўсталяваць магчымую колькасьць файлаў у прадстаўленьні.';
+$string['maxfilessubmission'] = 'Максімальная колькасьць запампаваных файлаў';
+$string['maxfilessubmission_help'] = 'Калі ўключаны прадстаўленьні-файлы, кожны студэнт здольны запампаваць да азначанай колькасьці файлаў у сваіх прадстаўленьнях.';
+$string['maximumsubmissionsize'] = 'Максімальны памер прадстаўленьня';
+$string['maximumsubmissionsize_help'] = 'Файлы запампаваныя студэнтамі могуць быць да азначанага памеру.';
+$string['numfilesforlog'] = 'Колькасьць файлаў: {$a}.';
+$string['pluginname'] = 'Прадстаўленьні-файлы';
+$string['privacy:metadata:filepurpose'] = 'Файлы запампаваныя для гэтага прадстаўленьня заданьня';
+$string['siteuploadlimit'] = 'Ліміт запампоўкі сайту';
+$string['submissionfilearea'] = 'Запампаваныя файлы прадстаўленьняў';
