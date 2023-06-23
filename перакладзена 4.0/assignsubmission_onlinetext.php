1b72c78347dbfb24e0b1576390e542780097afd5
Denis <denis.makouski@bseu.by>
2023-06-23 10:46:45 +0300

    ++

---
+++ перакладзена 4.0/assignsubmission_onlinetext.php
@@ -0,0 +1,49 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'assignsubmission_onlinetext', language 'en', version '4.0'.
+ *
+ * @package     assignsubmission_onlinetext
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['allowonlinetextsubmissions'] = 'Уключана';
+$string['default'] = 'Уключана па стандарту';
+$string['default_help'] = 'Калі ўсталявана, гэты метад прадстаўленьня будзе ўключаны па стандарту для ўсіх новых заданьняў.';
+$string['enabled'] = 'Анлайн тэкст';
+$string['enabled_help'] = 'Калі ўключана, студэнты будуць здольныя друкаваць складаны тэкст непасрэдна ў поле рэдактару для сваіх прадстаўленьняў.';
+$string['eventassessableuploaded'] = 'Анлайн тэкст быў абноўлены.';
+$string['nosubmission'] = 'Гэтае заданьне ня мае прадстаўленьняў';
+$string['numwords'] = '({$a} словаў)';
+$string['numwordsforlog'] = 'Колькасьць словаў прадстаўленьня: {$a}';
+$string['onlinetext'] = 'Анлайн тэкст';
+$string['onlinetextfilename'] = 'анлайнтэкст.html';
+$string['onlinetextsubmission'] = 'Дазволіць прадстаўленьні анлайн тэкст';
+$string['pluginname'] = 'Прадстаўленьні анлайн тэкст';
+$string['privacy:metadata:assignmentid'] = 'ID заданьня';
+$string['privacy:metadata:filepurpose'] = 'Файлы, убудаваныя ў тэкставыя прадстаўленьні.';
+$string['privacy:metadata:submissionpurpose'] = 'ID прадстаўленьня, якое зьвязвае прадстаўленьне з карыстальнікам.';
+$string['privacy:metadata:tablepurpose'] = 'Захоўвае тэкст прадстаўленьня для кожнай спробы.';
+$string['privacy:metadata:textpurpose'] = 'Актуальны тэкст, адпраўлены для гэтай спробы заданьня.';
+$string['privacy:path'] = 'Тэкст прадстаўленьня';
+$string['wordlimit'] = 'Ліміт словаў';
+$string['wordlimit_help'] = 'Калі ўключаны прадстаўленьні анлайн тэкст, гэтая налада вызначае максімальную колькасьць словаў, дазволеную для друку кожнаму студэнту.';
+$string['wordlimitexceeded'] = 'Ліміт словаў для заданьня {$a->limit}. Вы спрабуеце адправіць {$a->count} словаў. Калі ласка, скарэктуйце сваё прадстаўленьне і паспрабуйце зноў.';
