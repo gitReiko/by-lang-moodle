1fe4273f0ca4a39e462ce22edd5fb73e84f21741
Denis <denis.makouski@bseu.by>
2023-06-28 17:16:22 +0300

    ++

---
+++ праца/book.php
@@ -0,0 +1,117 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'book', language 'en', version '4.0'.
+ *
+ * @package     book
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addafter'] = 'Дадаць новую главу';
+$string['addafterchapter'] = 'Дадаць новую главу пасьля "{$a->title}"';
+$string['book:addinstance'] = 'Дадаць новую кнігу';
+$string['book:edit'] = 'Рэдагаваць главы кнігі';
+$string['book:read'] = 'Глядзець кнігу';
+$string['book:viewhiddenchapters'] = 'Глядзець схаваныя главы кнігі';
+$string['chapterandsubchaptersdeleted'] = 'Глава "{$a->title}" і яе падглавы {$a->subchapters} былі выдалены';
+$string['chapterdeleted'] = 'Глава "{$a->title}" была выдалена';
+$string['chapters'] = 'Главы';
+$string['chaptertitle'] = 'Назва главы';
+$string['confchapterdelete'] = 'Вы сапраўды жадаеце выдаліць гэтую главу?';
+$string['confchapterdeleteall'] = 'Вы сапраўды жадаеце выдаліць гэтую главу і ўсе яе падглавы?';
+$string['content'] = 'Зьмесьціва';
+$string['customtitles'] = 'Уласныя назвы';
+$string['customtitles_help'] = 'Звычайна назва главы адлюстроўваецца ў зьмесьце і як загаловак зьмесьціву.
+
+Калі ўсталяваны гэты сьцяжок, назва главы НЕ адлюстроўваецца як загаловак зьмесьціву. Іншую назву (напрыклад, даўжэйшую за магчымую назву главы) льга надрукаваць як частку зьмесьціва.';
+$string['deletechapter'] = 'Выдаліць главу "{$a}"';
+$string['editchapter'] = 'Рэдагаваць главу "{$a}"';
+$string['editingchapter'] = 'Рэдагаваньне главы';
+$string['errorchapter'] = 'Памылка чытаньня главы кнігі.';
+$string['eventchaptercreated'] = 'Глава створана';
+$string['eventchapterdeleted'] = 'Глава выдалена';
+$string['eventchapterupdated'] = 'Глава абноўлена';
+$string['eventchapterviewed'] = 'Глава прагледжана';
+$string['hidechapter'] = 'Схаваць главу "{$a}"';
+$string['indicator:cognitivedepth'] = 'Пазнаваньне кнігі';
+$string['indicator:cognitivedepth_help'] = 'Гэты паказьнік заснаваны на глыбіні пазнаваньня, якую дасягнуў студэнт у рэсурсе-Кнізе.';
+$string['indicator:cognitivedepthdef'] = 'Пазнаваньне кнігі';
+$string['indicator:cognitivedepthdef_help'] = 'Удзельнік дасягнуў гэтага адсотку пазнавальнага ўцягненьня, прапанаваны рэсурсам-Кнігай ў аналізаваны тэрмін (Роўні = Без прагляду, Прагляд)';
+$string['indicator:cognitivedepthdef_link'] = 'Learning_analytics_indicators#Cognitive_depth';
+$string['indicator:socialbreadth'] = 'Сацыяльнасьць кнігі';
+$string['indicator:socialbreadth_help'] = 'Гэты паказьнік заснаваны на сацыяльнай шырыні, якую дасягнуў студэнт у рэсурсе-Кнізе.';
+$string['indicator:socialbreadthdef'] = 'Сацыяльнасьць кнігі';
+$string['indicator:socialbreadthdef_help'] = 'Удзельнік дасягную гэтага адсотку сацыяльнай шырыні, прапанаваны рэсурсам-Кнігай у аналізаваны тэрмін (Роўні = Без удзельнікаў, Адзін удзельнік)';
+$string['indicator:socialbreadthdef_link'] = 'Learning_analytics_indicators#Social_breadth';
+$string['modulename'] = 'Кніга';
+$string['modulename_help'] = 'Модуль кніга дазваляе настаўнікам стварыць рэсурс з мноствам старонак ў фармаце падобным на кнігу з главамі і падглавамі. Кнігі могуць утрымліваць медыя файлы і тэкст; яны карысныя для адлюстраваньня доўгіх урыўкаў інфармацыі, падзеленых на главы.
+
+Кнігу льга выкарыстоўваць
+
+* У якасьці навучальных чытальных матэрыялаў
+* У якасьці карыстальніцкага даведніка 
+* У якасьці паказу партфоліа студэнцкай працы';
+$string['modulename_link'] = 'mod/book/view';
+$string['modulenameplural'] = 'Кнігі';
+$string['movechapterdown'] = 'Перасунуць главу згары "{$a}"';
+$string['movechapterup'] = 'Перасунуць главу угару "{$a}"';
+$string['navexit'] = 'Выйсьці з кнігі';
+$string['navimages'] = 'Выявы';
+$string['navnext'] = 'Далей';
+$string['navnexttitle'] = 'Далей: {$a}';
+$string['navoptions'] = 'Даступныя опцыі навігацыі па спасылках';
+$string['navoptions_desc'] = 'Опцыі адлюстраваньня навігацыі на старонках кнігі';
+$string['navprev'] = 'Назад';
+$string['navprevtitle'] = 'Назад: {$a}';
+$string['navstyle'] = 'Стылі навігацыі';
+$string['navstyle_help'] = '* Выявы - Выкарыстоўваць выявы для навігацыі
+* Тэкст - Выкарыстоўваць назвы главаў для навігацыі';
+$string['navtext'] = 'Тэкст';
+$string['navtoc'] = 'Толькі зьмесьціва';
+$string['nocontent'] = 'Пакуль гэтая кніга ня мае зьмесьціва.';
+$string['numbering'] = 'Фарматаваньне главы';
+$string['numbering0'] = 'Адсутнічае';
+$string['numbering1'] = 'Нумары';
+$string['numbering2'] = 'Кулі';
+$string['numbering3'] = 'Водступ';
+$string['numbering_help'] = '* Адсутнічае - Chapter and subchapter titles have no formatting
+* Numbers - Chapters and subchapter titles are numbered 1, 1.1, 1.2, 2, ...
+* Кулі - Subchapters are indented and displayed with bullets in the table of contents
+* Водступ - Subchapters are indented in the table of contents';
+$string['numberingoptions'] = 'Available options for chapter formatting';
+$string['numberingoptions_desc'] = 'Options for displaying chapters and subchapters in the table of contents';
+$string['page-mod-book-x'] = 'Any book module page';
+$string['pluginadministration'] = 'Book administration';
+$string['pluginname'] = 'Book';
+$string['previouschapter'] = 'Previous chapter';
+$string['privacy:metadata'] = 'The book activity module does not store any personal data.';
+$string['removeallbooktags'] = 'Remove all book tags';
+$string['search:activity'] = 'Book - resource information';
+$string['search:chapter'] = 'Book - chapters';
+$string['showchapter'] = 'Show chapter "{$a}"';
+$string['subchapter'] = 'Subchapter';
+$string['subchapternotice'] = '(Only available once the first chapter has been created)';
+$string['subplugintype_booktool'] = 'Book tool';
+$string['subplugintype_booktool_plural'] = 'Book tools';
+$string['tagarea_book_chapters'] = 'Book chapters';
+$string['tagsdeleted'] = 'Book tags have been deleted';
+$string['toc'] = 'Table of contents';
+$string['top'] = 'top';
