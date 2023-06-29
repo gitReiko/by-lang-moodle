31096b6c44bfe5d43618a40bc9c7d217c85406d5
Denis <denis.makouski@bseu.by>
2023-06-29 09:22:23 +0300

    ++

---
+++ перакладзена 4.0/booktool_importhtml.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'booktool_importhtml', language 'en', version '4.0'.
+ *
+ * @package     booktool_importhtml
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['doimport'] = 'Імпарт';
+$string['errornochapters'] = 'Нельга знайсьці главы ў абраным файле';
+$string['import'] = 'Імпартаваць главу';
+$string['importhtml:import'] = 'Імпартаваць главы';
+$string['importing'] = 'Імпартаваньне';
+$string['importingchapters'] = 'Імпартаваньне главаў у кнігу';
+$string['pluginname'] = 'Імпарт главаў кнігі';
+$string['privacy:metadata'] = 'Плагін імпарту главаў кнігі не захоўвае аніякіх асабістых дадзеных.';
+$string['relinking'] = 'Пераспасыланьне';
+$string['type'] = 'Тып';
+$string['typeonefile'] = 'Адзіны HTML файл з загалоўкамі-главамі';
+$string['typezipdirs'] = 'Кожная тэчка рэпрэзентуе адну главу';
+$string['typezipfiles'] = 'Кожны HTML файл рэпрэзентуе адну главу';
+$string['ziparchive'] = 'Zip файл';
+$string['ziparchive_help'] = 'Абярыце zip файл, які ўтрымлівае HTML файлы і дадатковыя мультымедыйныя файлы і тэчкі. Каб запампаваць падглавы, дадайце "_sub" у канец HTML файлу ці назваў тэчак.';
