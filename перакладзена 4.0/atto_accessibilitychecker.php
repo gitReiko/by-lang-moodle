59e7805fa092a96682d172262d929fc7ab3cebe3
Denis <denis.makouski@bseu.by>
2023-06-29 10:22:30 +0300

    ++

---
+++ перакладзена 4.0/atto_accessibilitychecker.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'atto_accessibilitychecker', language 'en', version '4.0'.
+ *
+ * @package     atto_accessibilitychecker
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['emptytext'] = 'Пусты тэкст';
+$string['entiredocument'] = 'Увесь дакумент';
+$string['imagesmissingalt'] = 'Выявы патрабуюць альтэрнатыўны тэкст. Каб прыбраць гэтае папярэджаньне, запоўніце атрыбут alt у вашых тэгах img (выявы). Льга пакінуць атрыбут alt пустым, але толькі калі выява чыста дэкаратыўная і не нясе аніякай інфармацыі.';
+$string['needsmorecontrast'] = 'Колеры пярэдняга і задняга фону тэксту недастаткова кантрасныя. Каб прыбраць гэтае папярэджаньне, зьмяніце колер пярэдняга ці задняга фону тэксту, каб яго было лягчэй чытаць.';
+$string['needsmoreheadings'] = 'На старонцы зашмат тэксту без загалоўкаў. Загалоўкі дазволяць карыстальнікам праграм чытаньня лягчэй арыентавацца на старонцы і зробяць яе зручнейшай для ўсіх.';
+$string['nowarnings'] = 'Віншуем, праблем з даступнасьцю ня выяўлена!';
+$string['pluginname'] = 'Правяральнік даступнасьці';
+$string['privacy:metadata'] = 'Плагін atto_accessibilitychecker не захоўвае аніякіх асабістых дадзеных.';
+$string['report'] = 'Справаздача даступнасьці:';
+$string['tablesmissingcaption'] = 'Табліцы мусяць мець подпісы. Хаця не абавязкова, каб кожная табліца мела подпіс, але подпіс, зазвычай, вельмі карысны.';
+$string['tablesmissingheaders'] = 'Табліцы мусяць мець загалоўкі радкоў і/ці слупкоў.';
+$string['tableswithmergedcells'] = 'Табліцы ня мусяць мець аб\'яднаных вочак. Нягледзячы на стандартнасьці гэтай разьметкі для табліцаў цягам многіх гадоў, частка праграмаў экраннага чытаньня дагэтуль ня поўнасьцю падтрымлівае складаныя табліцы. Па магчымасьці, спрабуйце "сплюснуць" табліцу і пазьбягаць зьлітых вочак.';
---
+++ перакладзена 4.0/atto_accessibilityhelper.php
@@ -0,0 +1,37 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'atto_accessibilityhelper', language 'en', version '4.0'.
+ *
+ * @package     atto_accessibilityhelper
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['listimages'] = 'Выявы ў тэкставым рэдактары:';
+$string['listlinks'] = 'Спасылкі ў тэкставым рэдактары:';
+$string['liststyles'] = 'Стылі цяперашняга абраньня:';
+$string['noimages'] = 'Выявы адсутнічаюць';
+$string['nolinks'] = 'Спасылкі адсутнічаюць';
+$string['nostyles'] = 'Стылі адсутнічаюць';
+$string['pluginname'] = 'Дапаможнік экраннага чытаньня';
+$string['privacy:metadata'] = 'Плагін atto_accessibilityhelper не захоўвае аніякіх асабістых дадзеных.';
+$string['selectimage'] = 'Абраць выяву';
+$string['selectlink'] = 'Абраць спасылку';
