1bf72d452ea732eaf1238459cae3d408e38d5449
Denis <denis.makouski@bseu.by>
2023-06-29 16:24:17 +0300

    ++

---
+++ перакладзена 4.0/atto_collapse.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'atto_collapse', language 'en', version '4.0'.
+ *
+ * @package     atto_collapse
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Паказаць/згарнуць прасунутыя кнопкі';
+$string['privacy:metadata'] = 'Плагін atto_collapse не захоўвае аніякіх асабістых дадзеных.';
+$string['settings'] = 'Налады згортваньня панэлі прыладаў';
+$string['showfewer'] = 'Паказваць меней кнопак';
+$string['showgroups'] = 'Паказваць першыя (n) груп падчас згорнутасьці.';
+$string['showgroups_desc'] = 'Калі панэль прыладаў згорнутая (так па стандарту), вызначае колькасьць адначасна адлюстраваных групаў.';
+$string['showmore'] = 'Паказваць болей кнопак';
+$string['youareonsecondrow'] = 'Цяпер адкрыты другі радок панэлі прыладаў рэдактару, дзе знаходзіцца болей кнопак.';
