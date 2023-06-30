5b493a062e937416167062f9f4472d4d5660cf2a
Denis <denis.makouski@bseu.by>
2023-06-30 11:02:38 +0300

    ++

---
+++ перакладзена 4.0/atto_media.php
@@ -0,0 +1,86 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'atto_media', language 'en', version '4.0'.
+ *
+ * @package     atto_media
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['add'] = 'Дадаць';
+$string['addcaptionstrack'] = 'Дадаць сьцежку тытраў';
+$string['addchapterstrack'] = 'Дадаць сьцежку главы';
+$string['adddescriptionstrack'] = 'Дадаць сьцежку апісаньня';
+$string['addmetadatatrack'] = 'Дадаць сьцежку метададзеных';
+$string['addsource'] = 'Дадаць альтэрнатыўную крыніцу';
+$string['addsource_help'] = 'Рэкамендуецца падаць альтэрнатыўную крыніцу мультымедыя, бо браўзары настольных і мабільных кампутараў адрозьніваюцца падтрыманымі фарматамі файлаў.';
+$string['addsubtitlestrack'] = 'Дадаць сьцежку субтытраў';
+$string['addtrack'] = 'Дадаць сьцежку';
+$string['advancedsettings'] = 'Прасунутыя налады';
+$string['audio'] = 'Аўдыё';
+$string['audiosourcelabel'] = 'Спасылка крыніцы аўдыё';
+$string['autoplay'] = 'Аўтаматычнае прайграваньне';
+$string['browserepositories'] = 'Агляд рэпазітароў...';
+$string['captions'] = 'Тытры';
+$string['captions_help'] = 'У тытрах льга апісваць усё, што адбываецца ў сьцежцы, у тым ліку невербальныя гукі, напрыклад, звон тэлефона.';
+$string['captionssourcelabel'] = 'Спасылка на сьцежку тытраў';
+$string['chapters'] = 'Главы';
+$string['chapters_help'] = 'Для навігацыі па медыярэсурсу льга выкарыстоўваць главы.';
+$string['chapterssourcelabel'] = 'Спасылка на сьцежку главы';
+$string['controls'] = 'Паказаць элементы кіраваньня';
+$string['createmedia'] = 'Уставіць медыя';
+$string['default'] = 'Стандартнае';
+$string['descriptions'] = 'Апісаньне';
+$string['descriptions_help'] = 'Аўдыё апісаньне льга выкарыстоўваць для забесьпячэньня сказу, што тлумачыць візуальныя дэталі, не відавочныя толькі з аўдыё.';
+$string['descriptionssourcelabel'] = 'Спасылка на сьцежку апісаньня';
+$string['displayoptions'] = 'Налады адлюстраваньня';
+$string['entername'] = 'Надрукуйце назву';
+$string['entersource'] = 'Спасылка на крыніцу';
+$string['entertitle'] = 'Надрукуйце тытры';
+$string['enterurl'] = 'Надрукуйце спасылку';
+$string['height'] = 'Вышыня';
+$string['kind'] = 'Тып';
+$string['label'] = 'Пазнака';
+$string['languagesavailable'] = 'Даступныя мовы';
+$string['languagesinstalled'] = 'Усталяваныя мовы';
+$string['link'] = 'Спасылка';
+$string['loop'] = 'Зацыкліць';
+$string['metadata'] = 'Метададзеныя';
+$string['metadata_help'] = 'Сьцежкі метададзеных для выкарыстаньня ў скрыпце, могуць выкарыстоўвацца толькі, калі прайгравальнік падтрымлівае метададзеныя.';
+$string['metadatasourcelabel'] = 'Спасылка на сьцежку метададзеных';
+$string['mute'] = 'Заглушана';
+$string['pluginname'] = 'Уставіць ці адрэгаваць аўдыё/відэа файлу';
+$string['poster'] = 'Спасылка на мініяцюру';
+$string['privacy:metadata'] = 'Плагін atto_media не захоўвае аніякіх асабістых дадзеных.';
+$string['remove'] = 'Выдаліць';
+$string['size'] = 'Памер';
+$string['srclang'] = 'Мова';
+$string['subtitles'] = 'Субтытры';
+$string['subtitles_help'] = 'Субтытры могуць выкарыстоўвацца для транскрыпцыі ці перакладу размоваў.';
+$string['subtitlessourcelabel'] = 'Спасылка на сьцежку субтытраў';
+$string['track'] = 'Спасылка на сьцежку';
+$string['tracks'] = 'Субтытры і тытры';
+$string['tracks_help'] = 'Субтытры, тытры, главы і апісаньні льга дадаць з дапамогай файлу фармату WebVTT (Web Video Text Tracks). Пазнакі сьцежак будуць паказаны ў выпадным меню абраньня. Для сьцежкі кожнага тыпу напачатку відэа будзе абірацца і ўсталёўваецца стандартная сьцежка.';
+$string['video'] = 'Відэа';
+$string['videoheight'] = 'Вышыня відэа';
+$string['videosourcelabel'] = 'Спасылка на крыніцу відэа';
+$string['videowidth'] = 'Шырыня відэа';
+$string['width'] = 'Шырыня';
