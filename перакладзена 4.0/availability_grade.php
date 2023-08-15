ea6be9597ff93c73b692b34c4573fed3fca0cfc8
Denis <denis.makouski@bseu.by>
2023-08-15 16:19:50 +0300

    ++

---
+++ перакладзена 4.0/availability_grade.php
@@ -0,0 +1,47 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'availability_grade', language 'en', version '4.0'.
+ *
+ * @package     availability_grade
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['cachedef_items'] = 'Прадметы адзнакі кэшуюцца для ўмоваў даступнасьці ацэньваньня';
+$string['cachedef_scores'] = 'Адзнакі карыстальніка кэшуюцца для ўмоваў даступнасьці ацэньваньня';
+$string['description'] = 'Патрабаваць ад студэнтаў дасягненьня пэўнай адзнакі.';
+$string['error_backwardrange'] = 'Падчас вызначэньня дыяпазону адзнак, мінімальная адзнака мусіць быць меней максімальнай.';
+$string['error_invalidnumber'] = 'Дыяпазоны адзнак мусяць быць вызначаны са слушнымі адсоткамі.';
+$string['error_selectgradeid'] = 'Вы мусіце абраць прадмет адзнакі для ўмовы-адзнакі.';
+$string['label_max'] = 'Максімальны адсотак адзнакі (эксклюзіўны)';
+$string['label_min'] = 'Мінімальны адсотак адзнакі (інклюзіўны)';
+$string['missing'] = '(адсутнічае актыўнасьць)';
+$string['option_max'] = 'мусіць быць <';
+$string['option_min'] = 'мусіць быць &#x2265;';
+$string['pluginname'] = 'Абмежаваньне адзнакамі';
+$string['privacy:metadata'] = 'Плагін Абмежаваньне адзнакамі не захоўвае аніякіх асабістых дадзеных.';
+$string['requires_any'] = 'Вы маеце адзнаку ў <strong>{$a}</strong>';
+$string['requires_max'] = 'Вы атрымалі належную адзнаку ў <strong>{$a}</strong>';
+$string['requires_min'] = 'Вы дасягнулі неабходную адзнаку ў <strong>{$a}</strong>';
+$string['requires_notany'] = 'Вы ня маеце адзнакі ў <strong>{$a}</strong>';
+$string['requires_notgeneral'] = 'Вы не атрымалі пэўную адзнаку ў <strong>{$a}</strong>';
+$string['requires_range'] = 'Вы атрымалі пэўную адзнаку ў <strong>{$a}</strong>';
+$string['title'] = 'Адзнака';
