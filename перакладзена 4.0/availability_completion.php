7d08324d93f036e51ef096c28834c6a76cb4c5a1
Denis <denis.makouski@bseu.by>
2023-08-15 15:53:29 +0300

    ++

---
+++ перакладзена 4.0/availability_completion.php
@@ -0,0 +1,54 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'availability_completion', language 'en', version '4.0'.
+ *
+ * @package     availability_completion
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['cachedef_previous_cache'] = 'Інфармацыя пра залежнасьць папярэдняй актыўнасьці';
+$string['description'] = 'Патрабаваць ад студэнтаў выкананьня (ці не выкананьне) іншай актыўнасьці.';
+$string['error_selectcmid'] = 'Вы мусіце абраць актыўнасьць для ўмовы выкананьня.';
+$string['error_selectcmidpassfail'] = 'Вы мусіце абраць актыўнасьць з усталяванай умовай выкананьня "Патрабуе адзнаку".';
+$string['label_cm'] = 'Актыўнасьць ці рэсурс';
+$string['label_completion'] = 'Неабходны статус выкананьня';
+$string['missing'] = '(Адсутнічае актыўнасьць)';
+$string['option_complete'] = 'мусіць быць адзначана як выкананае';
+$string['option_fail'] = 'мусіць быць выканана з няўдалай адзнакай';
+$string['option_incomplete'] = 'ня мусіць быць адзначана як выкананае';
+$string['option_pass'] = 'мусіць быць выканана з прахадной адзнакай';
+$string['option_previous'] = 'Папярэдняя актыўнасьць з выкананьнем';
+$string['pluginname'] = 'Абмежаваньне выкананьнем актыўнасьці';
+$string['privacy:metadata'] = 'Плагін абмежаваньне выкананьнем актыўнасьці не захоўвае аніякіх асабістых дадзеных.';
+$string['requires_complete'] = 'Актыўнасьць <strong>{$a}</strong> адзначана як выкананая';
+$string['requires_complete_fail'] = 'Актыўнасьць <strong>{$a}</strong> выканана і мае няўдалую адзнаку';
+$string['requires_complete_pass'] = 'Актыўнасьць <strong>{$a}</strong> выканана і мае прахадную адзнаку';
+$string['requires_incomplete'] = 'Актыўнасьць <strong>{$a}</strong> ня выканана';
+$string['requires_not_complete_fail'] = 'Актыўнасьць <strong>{$a}</strong> ня выканана і мае няўдалую адзнаку';
+$string['requires_not_complete_pass'] = ' Актыўнасьць <strong>{$a}</strong> ня выканана і мае прахадную адзнаку';
+$string['requires_previous_complete'] = '<strong>Папярэдняя актыўнасьць з выкананьнем</strong> адзначана як выкананая';
+$string['requires_previous_complete_fail'] = '<strong>Папярэдняя актыўнасьць з выкананьнем</strong> выканана і мае няўдалую адзнаку';
+$string['requires_previous_complete_pass'] = '<strong>Папярэдняя актыўнасьць з выкананьнем</strong> выканана і мае прахадную адзнаку';
+$string['requires_previous_incomplete'] = '<strong>Папярэдняя актыўнасьць з выкананьнем</strong> ня выканана';
+$string['requires_previous_not_complete_fail'] = '<strong>Папярэдняя актыўнасьць з выкананьнем</strong> ня выканана і мае няўдалую адзнаку';
+$string['requires_previous_not_complete_pass'] = '<strong>Папярэдняя актыўнасьць з выкананьнем</strong> ня выканана і мае прахадную адзнаку';
+$string['title'] = 'Выкананьне актыўнасьці';
