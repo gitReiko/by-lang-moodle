235db8b7098a1bee4af329ca8feb07fa75e875e6
Denis <denis.makouski@bseu.by>
2023-08-15 16:49:43 +0300

    ++

---
+++ перакладзена 4.0/availability_profile.php
@@ -0,0 +1,54 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'availability_profile', language 'en', version '4.0'.
+ *
+ * @package     availability_profile
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['conditiontitle'] = 'Поле профілю карыстальніка';
+$string['description'] = 'Кантраляваць доступ на грунце палёў профілю студэнту.';
+$string['error_selectfield'] = 'Вы мусіце абраць поле профілю.';
+$string['error_setvalue'] = 'Вы мусіце ўвесьці значэньне.';
+$string['label_operator'] = 'Метад параўнаньня';
+$string['label_value'] = 'Значэньне параўнаньня';
+$string['missing'] = '(Адсутнічае наладжанае поле: {$a})';
+$string['op_contains'] = 'утрымлівае';
+$string['op_doesnotcontain'] = 'не ўтрымлівае';
+$string['op_endswith'] = 'канчаецца на';
+$string['op_isempty'] = 'пустое';
+$string['op_isequalto'] = 'роўнае';
+$string['op_isnotempty'] = 'не пустое';
+$string['op_startswith'] = 'пачынаецца на';
+$string['pluginname'] = 'Абмежаваньне па профілю';
+$string['privacy:metadata'] = 'Плагін Абмежаваньне па профілю не захоўвае аніякіх асабістых дадзеных.';
+$string['requires_contains'] = 'Ваша поле <strong>{$a->field}</strong> утрымлівае <strong>{$a->value}</strong>';
+$string['requires_doesnotcontain'] = 'Ваша поле <strong>{$a->field}</strong> не ўтрымлівае <strong>{$a->value}</strong>';
+$string['requires_endswith'] = 'Ваша поле <strong>{$a->field}</strong> канчаецца на <strong>{$a->value}</strong>';
+$string['requires_isempty'] = 'Ваша поле <strong>{$a->field}</strong> пустое';
+$string['requires_isequalto'] = 'Вашае поле <strong>{$a->field}</strong> роўнае <strong>{$a->value}</strong>';
+$string['requires_isnotempty'] = 'Вашае поле <strong>{$a->field}</strong> не пустое';
+$string['requires_notendswith'] = 'Вашае поле <strong>{$a->field}</strong> не канчаецца на <strong>{$a->value}</strong>';
+$string['requires_notisequalto'] = 'Вашае поле <strong>{$a->field}</strong> ня роўнае <strong>{$a->value}</strong>';
+$string['requires_notstartswith'] = 'Вашае поле <strong>{$a->field}</strong> не пачынаецца на <strong>{$a->value}</strong>';
+$string['requires_startswith'] = 'Вашае поле <strong>{$a->field}</strong> пачынаецца на <strong>{$a->value}</strong>';
+$string['title'] = 'Поле профілю';
