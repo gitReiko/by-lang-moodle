9e8b6757991cee10fd84b070d95699e6ae72231d
Denis <denis.makouski@bseu.by>
2023-08-16 11:19:42 +0300

    ++

---
+++ перакладзена 4.0/enrol_cohort.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'enrol_cohort', language 'en', version '4.0'.
+ *
+ * @package     enrol_cohort
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addgroup'] = 'Дадаць у групу';
+$string['assignrole'] = 'Прызначыць ролю';
+$string['cohort:config'] = 'Наладзіць экземпляры кагорт';
+$string['cohort:unenrol'] = 'Адлічыць заблакаваных карыстальнікаў';
+$string['creategroup'] = 'Стварыць новую групу';
+$string['defaultgroupnametext'] = '{$a->name} кагорта {$a->increment}';
+$string['enrolcohortsynctask'] = 'Заданьне сінхр залічэньня кагортаў';
+$string['instanceexists'] = 'Кагорта ўжо сінхранізавана з абранай рольлю';
+$string['pluginname'] = 'Сінхранізаваньне з кагортай';
+$string['pluginname_desc'] = 'Плагін Сінхранізаваньня з кагортай сінхранізуе чальцоў кагорты з чальцамі курса.';
+$string['privacy:metadata:core_group'] = 'Плагін Сінхранізаваньня з кагортай можа ствараць новую групу ці выкарыстоўваць існую для даданьня ўсіх чальцоў кагорты.';
+$string['status'] = 'Акціўны';
