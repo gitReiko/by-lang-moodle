3334db8767f9cc546192f4f45bcfdd8550607bc9
Denis <denis.makouski@bseu.by>
2023-08-18 16:39:40 +0300

    ++

---
+++ перакладзена 4.0/enrol_guest.php
@@ -0,0 +1,49 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'enrol_guest', language 'en', version '4.0'.
+ *
+ * @package     enrol_guest
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['allowguests'] = 'Гэты курс дазваляе гасьцявым карыстальнікам уваходзіць';
+$string['guest:config'] = 'Наладзіць экземпляр гасьцявога доступа';
+$string['guestaccess_withoutpassword'] = 'Гасьцявы ўваход';
+$string['guestaccess_withpassword'] = 'Гасьцявы ўваход патрабуе пароль';
+$string['password'] = 'Пароль';
+$string['password_help'] = 'Пароль дазваляе абмежаваць гасьцявы доступ да курса толькі тымі, хто ведае пароль. Госьць павінен будзе ўводзіць пароль падчас кожнага ўвахода ў курс.';
+$string['passwordinvalid'] = 'Хібны пароль доступа, калі ласка, паспрабуйце зноў';
+$string['passwordinvalidhint'] = 'Гэты пароль доступа хібны, калі ласка, паспрабуйце зноў<br />
+(Вось падказка - ён пачынаецца з \'{$a}\')';
+$string['pluginname'] = 'Гасьцявы доступ';
+$string['pluginname_desc'] = 'Плагін Гасьцявы доступ забясьпечвае толькі часовы доступ да курса, насамрэч ён не залічае карыстальнікаў.';
+$string['privacy:metadata'] = 'Плагін Гасьцявы доступ не захоўвае аніякіх асабістых дадзеных.';
+$string['requirepassword'] = 'Патрабаваць пароль для гасьцявога доступа';
+$string['requirepassword_desc'] = 'Патрабаваць пароль у новых курсах і прадухіляць выдаленьне пароля доступа ў існых курсах.';
+$string['showhint'] = 'Паказаць падказку';
+$string['showhint_desc'] = 'Паказаць першую літару пароля гасьцявога доступа.';
+$string['status'] = 'Дазволіць гасьцявы доступ';
+$string['status_desc'] = 'Дазволіць часовы гасьцявы доступ па стандарту.';
+$string['status_help'] = 'Гэтая налада вызначае магчымасьць доступа да курса ў якасьці госьця без неабходнасьці залічэньня.';
+$string['status_link'] = 'enrol/guest';
+$string['usepasswordpolicy'] = 'Выкарыстоўваць палітыку пароляў';
+$string['usepasswordpolicy_desc'] = 'Выкарыстоўваць стандартную палітыку пароляў для пароляў гасьцявога доступа.';
