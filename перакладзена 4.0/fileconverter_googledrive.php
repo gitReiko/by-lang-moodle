6629b4b19c38f9bbfe7940e202745223f331291a
Denis <denis.makouski@bseu.by>
2023-08-22 14:53:02 +0300

    ++

---
+++ перакладзена 4.0/fileconverter_googledrive.php
@@ -0,0 +1,43 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'fileconverter_googledrive', language 'en', version '4.0'.
+ *
+ * @package     fileconverter_googledrive
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['disabled'] = 'Адключана';
+$string['issuer'] = 'сэрвіс OAuth 2';
+$string['issuer_help'] = 'Для доступа да Google Дыск (Google Drive) выкарыстоўваецца сэрвіс OAuth 2.';
+$string['pluginname'] = 'Google Дыск';
+$string['privacy:metadata:fileconverter_googledrive:externalpurpose'] = 'Гэтая інфармацыя дасылаецца да Google Drive API для канвертаваньня файла ў іншы фармат. Файл часова захоўваецца на Google Дыске. Пасьля завяршэньня канвертацыі файл выдаляецца.';
+$string['privacy:metadata:fileconverter_googledrive:filecontent'] = 'Зьмесьціва файла.';
+$string['privacy:metadata:fileconverter_googledrive:filemimetype'] = 'MIME тып файла.';
+$string['privacy:metadata:fileconverter_googledrive:params'] = 'Параметры запыту, што перадаюцца ў Google Drive API.';
+$string['test_conversion'] = 'Тэставае канвертаваньне дакумента';
+$string['test_conversionnotready'] = 'Канвертар дакументаў наладжаны хібна.';
+$string['test_conversionready'] = 'Канвертар дакументаў наладжаны слушна.';
+$string['test_converter'] = 'Праверыць працаздольнасьць канвертара.';
+$string['test_issuerinvalid'] = 'Для сэрвіса OAuth у наладах канвертара дакументаў усталявана хібнае значэньне.';
+$string['test_issuernotconnected'] = 'Сэрвіса OAuth у наладах канвертара дакументаў не далучаны да сістэмнага акаўнта.';
+$string['test_issuernotenabled'] = 'Сэрвіс OAuth у наладах канвертара не ўключаны.';
+$string['test_issuernotset'] = 'Сэрвіс OAuth не ўсталяваны ў наладах канвертара.';
