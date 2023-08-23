b7ef6979768cf72bcbb0a6d0e42f71b94fccce1c
Denis <denis.makouski@bseu.by>
2023-08-23 09:54:41 +0300

    ++

---
+++ перакладзена 4.0/fileconverter_unoconv.php
@@ -0,0 +1,40 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'fileconverter_unoconv', language 'en', version '4.0'.
+ *
+ * @package     fileconverter_unoconv
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pathtounoconv'] = 'Шлях да канвертару дакументаў unoconv';
+$string['pathtounoconv_help'] = 'Шлях да канвертару дакументаў unoconv. Гэта выканальны файл, здольны канверсаваць дакументы паміж фарматамі, што падтрымваюцца LibreOffice. Гэтая налада не абавязковая, але калі яе ўвесьці, то Moodle будзе выкарыстоўваць яе дзеля аўтаматычнага канверсаваньня фарматаў дакументаў. Яна патрэбна для падтрымкі больш шырокага сьпектра ўваходных файлаў для функцыі PDF анатаваньня заданьняў (assignment).';
+$string['pluginname'] = 'Unoconv';
+$string['privacy:metadata'] = 'Плагін Канвертар дакументаў unoconv не захоўвае аніякіях асабістых дадзеных.';
+$string['test_unoconv'] = 'Тэставаць шлях unoconv';
+$string['test_unoconvdoesnotexist'] = 'Шлях unoconv не ўказвае на праграму unoconv. Калі ласка, спраўдзіце вашыя налады шляху.';
+$string['test_unoconvdownload'] = 'Спампаваць канвертаваны тэставы pdf файл.';
+$string['test_unoconvempty'] = 'Шлях unoconv не ўсталяваны. Калі ласка, спраўдзіце вашыя налады шляху.';
+$string['test_unoconvisdir'] = 'Шлях unoconv указвае на тэчку, калі ласка, дадайце праграму unoconv ува азначаны шлях.';
+$string['test_unoconvnotestfile'] = 'Тэставы дакумент, які трэба канвертаваць у PDF адсутнічае.';
+$string['test_unoconvnotexecutable'] = 'Шлях unoconv указвае на файл, які ня з\'яўляецца выканальным';
+$string['test_unoconvok'] = 'Верагодна, шлях unoconv наладжаны слушна.';
+$string['test_unoconvversionnotsupported'] = 'Усталяваная версія unoconv не падтрымліваецца.';
