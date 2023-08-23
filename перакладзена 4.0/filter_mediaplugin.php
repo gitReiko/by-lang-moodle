92b07161362112148eb44962e12f782ba9687668
Denis <denis.makouski@bseu.by>
2023-08-23 11:48:10 +0300

    ++

--- перакладзена 4.0/filter_mediaplugin.php
+++ перакладзена 4.0/filter_mediaplugin.php
@@ -25,10 +25,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$string['fallbackaudio'] = 'Audio link';
-$string['fallbackvideo'] = 'Video link';
-$string['filtername'] = 'Multimedia plugins';
-$string['linktomedia'] = 'The multimedia plugins filter finds a link in text that points to a multimedia resource and replaces the link with an appropriate multimedia player code which can play the resource. It also replaces &lt;video&gt; and &lt;audio&gt; tags with selected video and audio players.
+$string['fallbackaudio'] = 'Аўдыё спасылка';
+$string['fallbackvideo'] = 'Відэа спасылка';
+$string['filtername'] = 'Мультымедыйныя плагіны';
+$string['linktomedia'] = 'Фільтар мультымедыйных плагінаў знаходзіць у тэксьце спасылку на мультымедыйны рэсурс, і замяняе яе адпаведным мультымедыйным прайгравальнікам, які здольны прайграваць рэсурс. Ён таксама замяняе тэгі &lt;video&gt; і &lt;audio&gt; на абраныя відэа і аўдыё прайгравальнікі.
 
-<a href="{$a}">Manage media players</a>';
-$string['privacy:metadata'] = 'The Multimedia plugins plugin does not store any personal data.';
+<a href="{$a}">Кіраваць медыя прайгравальнікамі</a>';
+$string['privacy:metadata'] = 'Мультымедыйныя плагіны не захоўваюць аніякіх асабістых дадзеных.';
