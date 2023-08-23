a4fdd0bb900d027c0682ec38e110c55d199d7e96
Denis <denis.makouski@bseu.by>
2023-08-23 15:29:47 +0300

    ++

---
+++ перакладзена 4.0/format_singleactivity.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'format_singleactivity', language 'en', version '4.0'.
+ *
+ * @package     format_singleactivity
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['activitytype'] = 'Тып актыўнасьці';
+$string['activitytype_help'] = 'Абярыце тып актыўнасьці ці рэсурс, які жадаеце выкарыстоўваць у курсе';
+$string['defactivitytype'] = 'Стандартны тып актыўнасьці';
+$string['defactivitytypedesc'] = 'Вызначце тып актыўнасьці, які будзе абірацца па-стандарту падчас стварэньня новага курса';
+$string['erroractivitytype'] = 'Тып актыўнасьці не вызначаны ў наладах курса';
+$string['orphaned'] = 'Асірацелая';
+$string['orphanedwarning'] = 'Гэтыя актыўнасьці не даступныя для карыстальнікаў!';
+$string['pluginname'] = 'Фармат адзінай актыўнасьці';
+$string['privacy:metadata'] = 'Плагін Фармат адзінай актыўнасьці не захоўвае аніякіх асабістых дадзеных.';
+$string['sectionname'] = '';
+$string['warningchangeformat'] = 'Падчас зьмяненьня існага фармата курса на "Адзіную актыўнасьць", пераканайцеся, што вы выдалілі ўсе дадатковыя актыўнасьці з курса ў тым ліку "Аб\'явы". Памятайце структура можа курса быць зьменена.';
