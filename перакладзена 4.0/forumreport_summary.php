e6a71451308c45bced202bb6a7dc272de76d1dc6
Denis <denis.makouski@bseu.by>
2023-08-23 15:58:47 +0300

    ++

---
+++ перакладзена 4.0/forumreport_summary.php
@@ -0,0 +1,58 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'forumreport_summary', language 'en', version '4.0'.
+ *
+ * @package     forumreport_summary
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['attachmentcount'] = 'Колькасьць укладаньняў';
+$string['charcount'] = 'Колькасьць знакаў';
+$string['earliestpost'] = 'Ранейшы пост';
+$string['eventreportdownloaded'] = 'Справаздача агляд форума спампавана';
+$string['eventreportviewed'] = 'Справаздача агляд форума прагледжана';
+$string['exportposts'] = 'Экспартаваць пасты';
+$string['exportpostslabel'] = 'Экспартаваць пасты для {$a}';
+$string['filter:datesbuttonlabel'] = 'Фільтар адкрытых датаў';
+$string['filter:datesfrom'] = 'З {$a}';
+$string['filter:datesfromto'] = '{$a->datefrom} - {$a->dateto}';
+$string['filter:datesname'] = 'Даты';
+$string['filter:datesorderwarning'] = 'Дата з ня можа быць ранейшай за дату да.';
+$string['filter:datesto'] = 'Да {$a}';
+$string['filter:groupsbuttonlabel'] = 'Адкрыць фільтар групаў';
+$string['filter:groupscountall'] = 'Групы (усе)';
+$string['filter:groupscountnumber'] = 'Групы ({$a})';
+$string['filter:groupsname'] = 'Групы';
+$string['forumselectcourseoption'] = 'Усе форумы ў курсе';
+$string['forumselectlabel'] = 'Абраны форум';
+$string['latestpost'] = 'Самы новы пост';
+$string['nodetitle'] = 'Справаздача агляд форума';
+$string['pluginname'] = 'Справаздача агляд форума';
+$string['postcount'] = 'Колькасьць абмеркаваньняў';
+$string['privacy:metadata'] = 'Плагін Справаздача агляд форума не захоўвае аніякіх асабістых дадзеных.';
+$string['replycount'] = 'Колькасьць адказаў';
+$string['summary:view'] = 'Доступ да справаздачы агляд у межах пададзенага форума ці форумаў';
+$string['summary:viewall'] = 'Доступ да справаздачы агляд у межах пададзенага форума ці форумаў для ўсіх карыстальнікаў';
+$string['summarytitle'] = 'Справаздача агляд форума - {$a}';
+$string['viewcount'] = 'Колькасьць праглядаў';
+$string['viewsdisclaimer'] = 'Слупок колькасьці праглядаў не фільтруецца па групе';
+$string['wordcount'] = 'Колькасьць словаў';
