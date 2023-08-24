5e1b085ebcc2a10924c4e9c4dc12e203d20b2c36
Denis <denis.makouski@bseu.by>
2023-08-24 11:37:01 +0300

    ++

---
+++ перакладзена 4.0/gradereport_history.php
@@ -0,0 +1,61 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'gradereport_history', language 'en', version '4.0'.
+ *
+ * @package     gradereport_history
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['allgradeitems'] = 'Усе прадметы адзнак';
+$string['allgraders'] = 'Усе адзначнікі';
+$string['datefrom'] = 'З даты';
+$string['datetime'] = 'Дата і час';
+$string['dateto'] = 'Па дату';
+$string['deleteditemid'] = 'Выдаліць прадмет з id {$a}';
+$string['errajaxsearch'] = 'Памылка падчас пошука карыстальнікаў';
+$string['eventgradereportviewed'] = 'Справаздача гісторыі адзнак прагледжана';
+$string['excluded'] = 'Выключана з разьліку';
+$string['exportfilename'] = 'гісторыя_адзнак';
+$string['feedbacktext'] = 'Тэкст водгука';
+$string['finishselectingusers'] = 'Скончыць абраньне карыстальнікаў';
+$string['foundnusers'] = '{$a} карыстальнікаў знойдзена';
+$string['foundoneuser'] = '1 карыстальнік знойдзены';
+$string['gradenew'] = 'Перагледжаная адзнака';
+$string['gradeold'] = 'Орыгінальная адзнака';
+$string['grader'] = 'Адзначнік';
+$string['history:view'] = 'Праглядзець гісторыю адзнак';
+$string['historyperpage'] = 'Запісаў гісторыі на адной старонцы';
+$string['historyperpage_help'] = 'Гэтая налада вызначае колькасьць запісаў гісторыі, што будзе адлюстроўвацца на кожнай старонцы справаздачы гісторыя адзнак.';
+$string['loadmoreusers'] = 'Загрузіць болей карыстальнікаў...';
+$string['pluginname'] = 'Гісторыя адзнак';
+$string['preferences'] = 'Перавагі гісторыі адзнак';
+$string['privacy:metadata'] = 'Справаздача гісторыя адзнак толькі паказвае дадзеных, што захоўваюцца ў іншых месцах.';
+$string['revisedonly'] = 'Толькі перагледжаныя адзнакі';
+$string['revisedonly_help'] = 'Паказвае толькі адзнакі, якія былі перагледжаны.
+
+То бок у сьпіс пападуць толькі тыя запісы, якія зьвязаны са зьменай адзнакі.';
+$string['selectedusers'] = 'Абраныя карыстальнікі';
+$string['selectuser'] = 'Абраць карыстальніка';
+$string['selectusers'] = 'Абраць карыстальнікаў';
+$string['source'] = 'Крыніца';
+$string['useractivityfeedback'] = 'водгук {$a}';
+$string['useractivitygrade'] = 'адзнака {$a}';
