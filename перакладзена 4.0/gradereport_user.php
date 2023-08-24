8ac21ae9c6103518767acd316b3a22d986d8e6cc
Denis <denis.makouski@bseu.by>
2023-08-24 14:30:48 +0300

    ++

---
+++ перакладзена 4.0/gradereport_user.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'gradereport_user', language 'en', version '4.0'.
+ *
+ * @package     gradereport_user
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['eventgradereportviewed'] = 'Справаздача адзнак карыстальніка прагледжана';
+$string['myself'] = 'Мая';
+$string['otheruser'] = 'Карыстальніцкая';
+$string['pluginname'] = 'Справаздача адзнак карыстальніка';
+$string['privacy:metadata:preference:gradereport_user_view_user'] = 'Патрэба прагляду справаздачы ў ролі цяперашняга карыстальніка ці іншага карыстальніка ў справаздачах журнала адзнак';
+$string['tablesummary'] = 'Табліца з\'яўляецца сьпісам прадметаў, што ацэньваюцца, уключае катэгорыі прадметаў адзнакі. Калі прадметы знаходзяцца ў катэгорыі, яны адлюстроўваюцца адпаведным чынам.';
+$string['user:view'] = 'Праглядзець сваю ўласную справаздачу адзнак';
+$string['viewas'] = 'Праглядзець справаздачу ў ролі';
