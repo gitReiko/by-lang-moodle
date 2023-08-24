b3ca113ec98525ce31abaf120d1e97185acbad15
Denis <denis.makouski@bseu.by>
2023-08-24 12:02:37 +0300

    ++

---
+++ перакладзена 4.0/gradereport_singleview.php
@@ -0,0 +1,66 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'gradereport_singleview', language 'en', version '4.0'.
+ *
+ * @package     gradereport_singleview
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['all_grades'] = 'Усе адзнакі';
+$string['assessmentname'] = 'Прадмет адзнак';
+$string['blanks'] = 'Пустыя адзнакі';
+$string['bulkappliesto'] = 'Для';
+$string['bulkfor'] = 'Адзнакі для {$a}';
+$string['bulkinsertvalue'] = 'Увесьці значэньне';
+$string['bulklegend'] = 'Масавы ўвод';
+$string['bulkperform'] = 'Ажыцявіць масавы ўвод';
+$string['entrypage'] = 'Карыстальнік адзнакі ці прадмет адзнакі';
+$string['eventgradereportviewed'] = 'Справаздача асобны прагляд адзнак прагледжана.';
+$string['exclude'] = 'Выключыць';
+$string['excludeall'] = 'Выключыць усе адзнакі';
+$string['excludefor'] = 'Выключыць для {$a}';
+$string['excludenone'] = 'Ня выключаць аніякія адзнакі';
+$string['feedbackfor'] = 'Водгук для {$a}';
+$string['filtergrades'] = 'Паказаць адзнакі для {$a}.';
+$string['gradefor'] = 'Адзнака для {$a}';
+$string['gradeitem'] = 'Прадмет адзнакі: {$a}';
+$string['gradeitemcannotbeoverridden'] = 'Гэты прадмет адзнакі нельга перавызначыць.';
+$string['gradeuser'] = 'Ацаніў карыстальнік: {$a}';
+$string['itemsperpage'] = 'Прадметаў на старонцы';
+$string['noscreens'] = 'Нельга знайсьці прыдатны экран асобнага прагляду.';
+$string['notvalid'] = 'Хібны экран асобнага прагляду: {$a}';
+$string['override'] = 'Перавызначыць';
+$string['overrideall'] = 'Перавызначыць усе адзнакі';
+$string['overridefor'] = 'Перавызначыць для {$a}';
+$string['overridenone'] = 'Не перавызначаць усе адзнакі';
+$string['overridenoneconfirm'] = 'Вы зьбіраецеся скасаваць перавызначэньне адзнак. Гэтае дзеяньне выдаліць усе папярэднія перавызначэньні адзнак. Вы ўпэўнены, што жадаеце працягнуць?';
+$string['pluginname'] = 'Асобны прагляд адзнак';
+$string['privacy:metadata'] = 'Справаздача Асобны прагляд адзнак паказвае толькі тыя дадзеныя, што захоўваюцца ў іншых месцах.';
+$string['save'] = 'Захаваць';
+$string['savegrades'] = 'Захаваньне адзнак';
+$string['savegradessuccess'] = 'Адзнакі былі ўсталяваныя для {$a} прадметаў';
+$string['selectgrade'] = 'Абраць прадмет адзнакі...';
+$string['selectuser'] = 'Абраць карыстальніка...';
+$string['singleview:view'] = 'Праглядзець справаздачу';
+$string['summarygrade'] = 'Табліца карыстальнікаў са слупкамі для дыяпазону, адзнакі, водгука ды магчымага перавызначыньня ці выключэньня пэўнай адзнакі.';
+$string['summaryuser'] = 'Табліца прадметаў са слупкамі для катэгорыі адзнакі, дыяпазону, адзнакі, водгука ды магчымага перавызначыньня ці выключэньня пэўнай адзнакі.';
+$string['userselect'] = 'Абраць актыўнасьць';
