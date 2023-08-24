2cb4e87fdc5aee482be0c5a8cd6c8e1abcfeef93
Denis <denis.makouski@bseu.by>
2023-08-24 09:13:00 +0300

    ++

---
+++ перакладзена 4.0/gradeimport_xml.php
@@ -0,0 +1,44 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'gradeimport_xml', language 'en', version '4.0'.
+ *
+ * @package     gradeimport_xml
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['errbadxmlformat'] = 'Памылка - хібны фармат XML';
+$string['errduplicategradeidnumber'] = 'Памылка - ў гэтым курсе існуе два прадмета адзнакі з id нумарам \'{$a}\'. Гэты павінна быць немагчымым.';
+$string['errduplicateidnumber'] = 'Памылка - дублікат id нумара';
+$string['errincorrectgradeidnumber'] = 'Памылка - id нумар \'{$a}\' з файлу імпарту не адносіцца да ніводнага прадмета адзнакі.';
+$string['errincorrectidnumber'] = 'Памылка - хібны id нумар';
+$string['errincorrectuseridnumber'] = 'Памылка - id нумар \'{$a}\' з файлу імпарту не адносіцца да ніводнага карыстальніка.';
+$string['error'] = 'Адбылася памылка';
+$string['errorduringimport'] = 'Падчас спробы імпарту адбылася памылка: {$a}';
+$string['fileurl'] = 'Вонкавая спасылка на файл';
+$string['fileurl_help'] = 'Поле дзеля вонкавай спасылкі на файл для атрыманьня дадзеных з вонкавага сэрвера, напрыклад, інфармацыйнай студэнцкай сістэмы.';
+$string['importxml'] = 'XML імпарт';
+$string['importxml_help'] = 'Адзнакі льга імпартаваць праз XML файл, які ўтрымлівае ID нумары карыстальнікаў і ID нумары актыўнасьцяў. Каб атрымаць слушны фармат, спачатку экспартуйце адзнакі ўва XML файл, потым праглядзіце яго.';
+$string['importxml_link'] = 'grade/import/xml/index';
+$string['pluginname'] = 'XML файл';
+$string['privacy:metadata'] = 'Плагін Імпарт адзнак з XML не захоўвае аніякіх асабістых дадзеных.';
+$string['xml:publish'] = 'Публікаваць імпарт адзнак з XML';
+$string['xml:view'] = 'Імпартаваць адзнакі з XML';
