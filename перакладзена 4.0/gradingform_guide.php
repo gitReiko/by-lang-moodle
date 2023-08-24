2a45b2dfa2175305e3179700ccdb8a1caa4b2c79
Denis <denis.makouski@bseu.by>
2023-08-24 15:59:00 +0300

    ++

---
+++ перакладзена 4.0/gradingform_guide.php
@@ -0,0 +1,105 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'gradingform_guide', language 'en', version '4.0'.
+ *
+ * @package     gradingform_guide
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addcomment'] = 'Дадаць часта карыстаны каментар';
+$string['addcriterion'] = 'Дадаць крытэр';
+$string['additionalcomments'] = 'Дадатковыя каментары';
+$string['additionalcommentsforcriterion'] = 'Дадатковыя каментары для крытэра, {$a}';
+$string['alwaysshowdefinition'] = 'Паказаць студэнтам вызначэньне дапаможніка';
+$string['backtoediting'] = 'Вярнуцца да рэдагаваньня';
+$string['clicktocopy'] = 'Націсьніце, каб скапіяваць гэты тэкст ува крытэр водгука';
+$string['clicktoedit'] = 'Націсьніце, каб рэдагаваць';
+$string['clicktoeditname'] = 'Націсьніце, каб рэдагаваць назву крытэра';
+$string['comment'] = 'Каментар';
+$string['commentpickerforcriterion'] = 'Часта карыстаны выбарнік каментароў для {$a} дадатковых каментароў';
+$string['comments'] = 'Часта карыстаныя каментары';
+$string['commentsdelete'] = 'Выдаліць каментар';
+$string['commentsempty'] = 'Націсьніце, каб рэдагаваць каментар';
+$string['commentsmovedown'] = 'Апусьціць уніз';
+$string['commentsmoveup'] = 'Падняць уверх';
+$string['confirmdeletecriterion'] = 'Вы ўпэўнены, што жадаеце выдаліць гэты прадмет?';
+$string['confirmdeletelevel'] = 'Вы ўпэўнены, што жадаеце выдаліць гэты ровень?';
+$string['criterion'] = 'Назва крытэра';
+$string['criteriondelete'] = 'Выдаліць крытэр';
+$string['criterionempty'] = 'Націсьніце, каб рэдагаваць крытэр';
+$string['criterionmovedown'] = 'Апусьціць уніз';
+$string['criterionmoveup'] = 'Падняць уверх';
+$string['criterionname'] = 'Назва крытэра';
+$string['criterionremark'] = '{$a} пераадзнак крытэра';
+$string['definemarkingguide'] = 'Вызначыць дапаможнік правяральніка';
+$string['description'] = 'Апісаньне';
+$string['descriptionmarkers'] = 'Апісаньне правяральнікаў';
+$string['descriptionstudents'] = 'Апісаньне студэнтаў';
+$string['err_maxscoreisnegative'] = 'Макс бал хібны, адмоўныя значэньні не дапускаюцца';
+$string['err_maxscorenotnumeric'] = 'Макс бал крытэра мусіць быць лічбавым';
+$string['err_nocomment'] = 'Каментар ня можа быць пустым';
+$string['err_nodescription'] = 'Апісаньне студэнта ня можа быць пустым';
+$string['err_nodescriptionmarkers'] = 'Апісаньне правяральніка ня можа быць пустым';
+$string['err_nomaxscore'] = 'Макс бал крытэра ня можа быць пустым';
+$string['err_noshortname'] = 'Назва крытэра ня можа быць пустой';
+$string['err_scoreinvalid'] = 'Дадзены бал \'{$a->criterianame}\' хібны, макс бал: {$a->maxscore}';
+$string['err_scoreisnegative'] = 'Дадзены бал \'{$a->criterianame}\' хібны, адмоўныя значэньні не дапускаюцца';
+$string['err_shortnametoolong'] = 'Назва крытэра мусіць быць меней 256 знакаў';
+$string['gradingof'] = 'адзнака {$a}';
+$string['guide'] = 'Дапаможнік правяральніка';
+$string['guidemappingexplained'] = 'ПАПЯРЭДЖАНЬНЕ: Вашы дапаможнік правяральніка мае максімальную адзнаку <b>{$a->maxscore} балаў</b>, але максімальная адзнака, усталяваная ў вашай актыўнасьці, {$a->modulegrade} балаў. Максімальны бал усталяваны ў вашым дапаможніке правяральніка будзе маштабавацца да максімальнай адзнакі ў модуле.<br />
+    Прамежныя балы будуць адпаведна канвертавацца і акругляцца да найблізкай даступнай адзнакі.';
+$string['guidenotcompleted'] = 'Калі ласка, вызначце слушную адзнаку для кожнага крытэра';
+$string['guideoptions'] = 'Налады дапаможніка правяральніка';
+$string['guidestatus'] = 'Цяперашні статус дапаможніка правяральніка';
+$string['hidemarkerdesc'] = 'Схаваць апісаньні дапаможніка правяральніка';
+$string['hidestudentdesc'] = 'Схаваць апісаньні студэнцкіх крытэраў';
+$string['informationforcriterion'] = '{$a} інфармаваньняў';
+$string['insertcomment'] = 'Уставіць часта карыстаны каментар';
+$string['maxscore'] = 'Максімальны бал';
+$string['name'] = 'Назва';
+$string['needregrademessage'] = 'Вызначэньне дапаможніка правяральніка было зьменена пасьля таго, як студэнт атрымаў адзнаку. Студэнт не ўбачыць гэты дапаможнік правяральніка пакуль вы не праверыце дапаможнік правяральніка і не абнавіце адзнаку.';
+$string['outof'] = 'Бал з {$a}';
+$string['pluginname'] = 'Дапаможнік правяральніка';
+$string['previewmarkingguide'] = 'Прадпрагляд дапаможніка правяральніка';
+$string['privacy:metadata:criterionid'] = 'Індэнтыфікатар крытэра для прасунутага правяральніка.';
+$string['privacy:metadata:fillingssummary'] = 'Захоўвае інфармацыю пра адзнаку і водгук карыстальніка для дапаможніка правяральніка.';
+$string['privacy:metadata:instanceid'] = 'Індэнтыфікатар адзнакі, што выкарыстоўваецца актыўнасьцю.';
+$string['privacy:metadata:preference:showmarkerdesc'] = 'Патрэба паказваньня апісаньняў крытэраў правяральніка';
+$string['privacy:metadata:preference:showstudentdesc'] = 'Патрэба паказваньня апісаньняў студэнцкіх крытэраў';
+$string['privacy:metadata:remark'] = 'Пераадзнакі, зьвязаныя з гэтым крытэрам.';
+$string['privacy:metadata:score'] = 'Бал для гэтага крытэра адзнакі.';
+$string['regrademessage1'] = 'Вы зьбіраецеся захаваць зьмены для дапаможніка правяральніка, якія ўжо быў выкарыстаны для ацэньваньня. Азначце, ці трэба пераглядзець існыя адзнакі. Калі вы ўсталюеце гэтае, то дапаможнік правяральніка будзе схаваны ад студэнтаў да моманту, пакуль іх прадмет ня будзе ацэнены паўторна.';
+$string['regrademessage5'] = 'Вы зьбіраецеся захаваць зьмены істотныя зьмены для дапаможніка правяральніка, якія ўжо быў выкарыстаны для ацэньваньня. Значэньні ў журнале адзнак ня будуць зьменены, але дапаможнік правяральніка будзе схаваны ад студэнтаў, пакуль іх прадмет ня будзе ацэнены паўторна.';
+$string['regradeoption0'] = 'Не адзначаць для пераацэнкі';
+$string['regradeoption1'] = 'Адзначыць для пераацэнкі';
+$string['remark_help'] = 'Надрукуйце любыя дадатковыя каментары пра гэты крытэр.';
+$string['restoredfromdraft'] = 'НАТАТКА: Апошняя спроба ацаніць гэтую асобу не была захавана належным чынам, таму чарнавыя адзнакі былі адноўлены. Калі вы жадаеце скасаваць гэтыя зьмены, скарыстайцеся ніжэйшай кнопкай \'Скасаваць\'.';
+$string['save'] = 'Захаваць';
+$string['saveguide'] = 'Захаваць дапаможнік правяральніка і зрабіць яго гатовым';
+$string['saveguidedraft'] = 'Захаваць як чарнавік';
+$string['score'] = 'бал';
+$string['score_help'] = 'Увядзіце бал для {$a->criterion} ад 0 да {$a->maxscore}.';
+$string['scoreforcriterion'] = 'бал {$a}';
+$string['showmarkerdesc'] = 'Паказаць апісаньні крытэра правяральніка';
+$string['showmarkspercriterionstudents'] = 'Паказаць студэнтам адзнакі за кожны крытэр';
+$string['showstudentdesc'] = 'Паказаць студэнтам апісаньні крытэраў';
