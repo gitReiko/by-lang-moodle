4320722fd830f9ee933be7a528930f4a4a71ae3d
Denis <denis.makouski@bseu.by>
2023-08-24 10:54:06 +0300

    ++

---
+++ перакладзена 4.0/gradereport_grader.php
@@ -0,0 +1,63 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'gradereport_grader', language 'en', version '4.0'.
+ *
+ * @package     gradereport_grader
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['ajaxchoosescale'] = 'Абраць';
+$string['ajaxclicktoclose'] = 'Націсьніце на гэтую скрынку, каб прыбраць яе';
+$string['ajaxerror'] = 'Памылка';
+$string['ajaxfailedupdate'] = 'Нельга абнавіць [1] для [2]';
+$string['ajaxfieldchanged'] = 'Поле, якое вы цяпер рэдагуеце было зьменена, жадаеце карыстацца абноўленым значэньнем?';
+$string['eventgradereportviewed'] = 'Справаздача адзначніка прагледжана';
+$string['grader:manage'] = 'Кіраваць справаздачай адзначніка';
+$string['grader:view'] = 'Праглядзець справаздачу адзначніка';
+$string['overriddengrade'] = 'Перавызначаная адзнака';
+$string['pluginname'] = 'Справаздача адзначніка';
+$string['preferences'] = 'Перавагі справаздачы адзначніка';
+$string['privacy:metadata:preference:grade_report_aggregationposition'] = 'Патрэба адлюстраваньня слупкоў вынікаў курса і катэгорыяў. Адлюстроўваньне першымі ці апошнімі ў справаздачах журнала адзнак';
+$string['privacy:metadata:preference:grade_report_averagesdecimalpoints'] = 'Колькасьць дзясяткавых знакаў, што будзе адлюстроўвацца для кожнага сярэдняга значэньня альбо выкарыстаньне агульнай налады дзясяткавых знакаў для катэгорыяў і прадметаў адзнакі (спадкуецца)';
+$string['privacy:metadata:preference:grade_report_averagesdisplaytype'] = 'Шлях адлюстраваньня прыблізнага сярэдняга. У форме рэальных адзнак, адсоткаў ці літар. Месца выкарыстаньня тыпу адлюстраваньня, для катэгорыі ці прадмета адзнак (спадкуецца)';
+$string['privacy:metadata:preference:grade_report_enableajax'] = 'Патрэба даданьня пласта AJAX функцыйнасьці ў справаздачу адзначніка, ён спрашчае і паскарае выкананьне звычайных аперацыяў';
+$string['privacy:metadata:preference:grade_report_grader_collapsed_categories'] = 'Згорнутасьць сьпіса катэгорыяў журнала адзнак';
+$string['privacy:metadata:preference:grade_report_meanselection'] = 'Патрэба ўключэньня вочак без адзнак у разьлік прыблізных сярэдніх для кожнай катэгорыі ці прадмета адзнак';
+$string['privacy:metadata:preference:grade_report_quickgrading'] = 'Патрэба адлюстраваньня для кожнай адзнакі тэкставай скрынкі ўвода, што дазваляе рэдагаваць мноства адзнак адначасова';
+$string['privacy:metadata:preference:grade_report_rangesdecimalpoints'] = 'Колькасьць дзясяткавых знакаў, што будзе адлюстроўвацца для кожнага дыяпазону, ці выкарыстаньне агульнай налады дзясяткавых знакаў для катэгорыі ці прадмета адзнакі (спадкуецца)';
+$string['privacy:metadata:preference:grade_report_rangesdisplaytype'] = 'Шлях адлюстраваньня дыяпазону. У форме рэальных адзнак, адсоткаў ці літар. Месца выкарыстаньня тыпу адлюстраваньня, для катэгорыі ці прадмета адзнак (спадкуецца)';
+$string['privacy:metadata:preference:grade_report_showactivityicons'] = 'Патрэба адлюстраваньня выяваў актыўнасьцяў побач зь іх назвамі';
+$string['privacy:metadata:preference:grade_report_showanalysisicon'] = 'Патрэба адлюстраваньня выявы аналіза адзнак па-стандарту. Калі модуль актыўнасьці падтрымлівае гэта, то выява аналізу адзнакі будзе спасылацца на старонку з больш дэталёвым тлумачэньнем адзнакі і шляху яе атрыманьня';
+$string['privacy:metadata:preference:grade_report_showaverages'] = 'Патрэба адлюстраваньня дадатковага радка з прыблізным сярэднім значэньнем для кожнай катэгорыі і прадмета адзнак';
+$string['privacy:metadata:preference:grade_report_showcalculations'] = 'Патрэба адлюстраваньня выявы разьлічніка для кожнай катэгорыі і прадмета адзнакі з візуальным індыкатарам разьлічанасьці адзнакі, яе адлюстраваньня ці не адлюстраваньня падчас уключанага рэдагаваньня';
+$string['privacy:metadata:preference:grade_report_showeyecons'] = 'Патрэба адлюстраваньня выявы паказаць/схаваць для кожнай адзнакі дзеля кантролю бачнасьці адзнакі ў студэнта';
+$string['privacy:metadata:preference:grade_report_showlocks'] = 'Патрэба адлюстраваньня выявы блакаваньня/адблакаваньня для кожнай адзнакі дзеля кантролю магчымасьці аўтаматычнага абнаўленьня адзнакі, зьвязанай актыўнасьці, падчас уключанага рэдагаваньня';
+$string['privacy:metadata:preference:grade_report_shownumberofgrades'] = 'Патрэба адлюстраваньня ў дужках пасьля кожнага сярэдняга значэньня колькасьці адзнак, выкарыстанай падчас разьліку сярэдняга';
+$string['privacy:metadata:preference:grade_report_showonlyactiveenrol'] = 'Патрэба адлюстраваньня ў справаздачы журнала адзнак толькі актыўных залічаных карыстальнікаў (не заблакаваных) ці ўсіх залічаных карыстальнікаў';
+$string['privacy:metadata:preference:grade_report_showquickfeedback'] = 'Патрэба адлюстрваньня для кожнай адзнакі тэкставай скрынкі ўводу водгукаў з пункцірнымі межамі, што дазваляе рэдагаваць водгукі для мноства адзнак адначасова';
+$string['privacy:metadata:preference:grade_report_showranges'] = 'Патрэба адлюстраваньня дадатковага радка з дыяпазонам для кожнай катэгорыі і прадмета адзнак';
+$string['privacy:metadata:preference:grade_report_showuserimage'] = 'Патрэба адлюстраваньня выявы профілю карыстальніка побач зь яго імем';
+$string['privacy:metadata:preference:grade_report_studentsperpage'] = 'Колькасьць студэнтаў, якая будзе адлюстроўвацца на кожнай старонцы справаздач адзначніка';
+$string['privacy:request:preference:grade_report_grader_collapsed_categories'] = 'Вы маеце катэгорыі журнала адзнак згорнутыя ў курсе "{$a->name}"';
+$string['summarygrader'] = 'Табліца зь імёнамі студэнтаў у першым слупке, актыўнасьцямі, што ацэньваюцца, згрупаванымі па катэгорыям і курсам уверсе';
+$string['useractivityfeedback'] = 'водгук {$a}';
+$string['useractivitygrade'] = 'адзнака {$a}';
