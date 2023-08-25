2d96284ddc7e1f880d8700dd76e46d3ec70c7229
Denis <denis.makouski@bseu.by>
2023-08-25 16:09:42 +0300

    ++

---
+++ перакладзена 4.0/media_videojs.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'media_videojs', language 'en', version '4.0'.
+ *
+ * @package     media_videojs
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['audiocssclass'] = 'CSS клас для аўдыё';
+$string['audioextensions'] = 'Пашырэньні аўдыё файлаў';
+$string['configaudiocssclass'] = 'CSS клас, які будзе даданы да &lt;аўдыё&gt; элемента.';
+$string['configaudioextensions'] = 'Сьпіс падтрыманых пашырэньняў аўдыёфайлаў, падзеленых коскамі. VideoJS будзе спрабаваць выкарыстоўваць стандартны відэапрайгравальнік браўзара, калі ён даступны, ці стандартную функцыйнасьць VideoJS.';
+$string['configlimitsize'] = 'Калі ўключана, а шырыні і вышыня ня вызначаны, то відэа будзе адлюстроўвацца са стандартнай шырынёй і вышынёй. У адваротным выпадку відэа будзе расьцягвацца да максімальнай магчымай шырыні.';
+$string['configrtmp'] = 'Калі ўключана, то спасылкі, што пачынаюцца з rtmp:// , будуць адлюстроўвацца плагінам, незалежна ад уключэньня пашырэньня ў наладзе пашырэньняў Відэа файла (videoextensions). Для працаваньня RTMP трэба ўключыць Flash fallback.';
+$string['configuseflash'] = 'Выкарыстоўваць Flash прайгравальнік, калі фармат відэа не падтрымліваецца браўзарам і/або прайгравальнікам VideoJS. Калі ўключана, VideoJS будзе ўжывацца для ўсіх пашырэньняў файла са сьпісу, прыведзенага вышэй, без праверкі браўзара. Зьвярніце ўвагу, Flash не даступны ў мабільных браўзарах і не рэкамендуецца мноствам настольных.';
+$string['configvideocssclass'] = 'CSS клас, які будзе даданы да &lt;відэа&gt; элемента. Напрыклад, CSS клас "vjs-big-play-centered" перасуне кнопку прайграваньня ўсярэдзіну. Дэталі, і шлях усталяваньня свайго стылю, льга знайсьці на docs.videojs.com.';
+$string['configvideoextensions'] = 'Сьпіс пашырэньняў відэафайлаў, падзеленых коскамі. VideoJS будзе спрабаваць выкарыстоўваць стандартный відэапрайгравальнік браўзара, калі ён даступны.';
+$string['configyoutube'] = 'Выкарыстоўваць VideoJS для прайграваньня YouTube відэа. Зьвярніце ўвагу, плэй-лісты YouTube пакуль не падтрымліваюцца VideoJS.';
+$string['limitsize'] = 'Ліміт памеру';
+$string['pluginname'] = 'Прайгравальнік VideoJS';
+$string['pluginname_help'] = 'JavaScript абгортка для відэафайлаў, што прайграюцца стандартным відэапрайгравальнікам браўзара (Падтрымка залежыць ад браўзара).';
+$string['privacy:metadata'] = 'Плагін Прайгравальнік VideoJS не захоўвае аніякіх асабістых дадзеных.';
+$string['rtmp'] = 'RTMP стрымы';
+$string['useflash'] = 'Выкарыстоўваць Flash fallback';
+$string['videocssclass'] = 'CSS клас для відэа';
+$string['videoextensions'] = 'Пашырэньні відэа файла';
+$string['youtube'] = 'YouTube відэа';
