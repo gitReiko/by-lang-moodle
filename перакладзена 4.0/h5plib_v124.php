fac1e756bc1c5d062e5dd1c704998c975d19173c
Denis <denis.makouski@bseu.by>
2023-08-25 12:29:01 +0300

    ++

---
+++ перакладзена 4.0/h5plib_v124.php
@@ -0,0 +1,245 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'h5plib_v124', language 'en', version '4.0'.
+ *
+ * @package     h5plib_v124
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['editor:add'] = 'Дадаць';
+$string['editor:addauthor'] = 'Захаваць аўтара';
+$string['editor:addentity'] = 'Дадаць :entity';
+$string['editor:addfile'] = 'Дадаць файл';
+$string['editor:addnewchange'] = 'Дадаць новую зьмену';
+$string['editor:addtitle'] = 'Дадаць назву';
+$string['editor:addvideodescription'] = 'H5P падтрымлівае ўсе вонкавыя відэа крыніцы фарматаў: mp4, webm ці ogv, напрыклад, Vimeo Pro, і падтрымлівае спасылкі на YouTube.';
+$string['editor:atoz'] = 'А да Я';
+$string['editor:avtablistlabel'] = 'Уставіць выкарыстаньне';
+$string['editor:belowmin'] = 'Значэньне :property ніжэйшае за мінімум :min.';
+$string['editor:cancel'] = 'Скасаваць';
+$string['editor:cancellabel'] = 'Скасаваць';
+$string['editor:changefile'] = 'Зьмяніць файл';
+$string['editor:changelanguage'] = 'Зьмяніць мову на :language?';
+$string['editor:changelibrary'] = 'Зьмяніць тып кантэнта?';
+$string['editor:changelogdescription'] = 'Некаторыя ліцэнзіі патрабуюць, каб зьмены ў зыходніках ці іх вытворных рэгістраваліся і адлюстроўваліся. Тут вы можаце рэгістраваць свае зьмены па ліцэнзійных нагодах ці проста, каб адсочваць зьмены гэтага кантэнту.';
+$string['editor:close'] = 'Закрыць';
+$string['editor:commonfields'] = 'Перавызначэньні тэксту і пераклады';
+$string['editor:commonfieldsdescription'] = 'Тут вы можаце рэдагаваць налады ці перакладаць тэкст, што выкарыстоўваецца гэтым кантэнтам.';
+$string['editor:confirmchangelibrary'] = 'Зрабіўшы гэтае, вы страціце ўсю працу з цяперашнім тыпам кантэнту. Вы ўпэўненыя, што жадаеце зьмяніць тып кантэнту?';
+$string['editor:confirmdeletechangelog'] = 'Вы ўпэўненыя, што жадаеце выдаліць гэты запіс логах зьменаў?';
+$string['editor:confirmimageremoval'] = 'Гэта выдаліць вашую выяву. Вы ўпэўненыя, што жадаеце працягнуць?';
+$string['editor:confirmpastebuttontext'] = 'Замяніць кантэнт';
+$string['editor:confirmpastecontent'] = 'Зрабіўшы гэтае, вы заменіце цяперашні кантэнт кантэнтам з вашага буферу абмену. Цяперашні кантэнт будзе страчаны. Вы ўпэўненыя, што жадаеце працягнуць?';
+$string['editor:confirmremoval'] = 'Вы ўпэўненыя, што жадаеце выдаліць гэты :type?';
+$string['editor:confirmremoveauthor'] = 'Вы ўпэўненыя, што жадаеце выдаліць гэтага аўтара?';
+$string['editor:contenttypebackbuttonlabel'] = 'Назад';
+$string['editor:contenttypecacheoutdated'] = 'Сьпіс тыпаў кантэнту пратэрмінаваны';
+$string['editor:contenttypecacheoutdateddesc'] = 'Вашы сайт мае складанасьці з далучэньнем да H5P.org дзеля праверкі абноваў тыпаў кантэнту. Магчыма вам будзе недаступна абнаўленьне альбо ўсталёўка новых тыпаў кантэнту.';
+$string['editor:contenttypedemobuttonlabel'] = 'Дэма кантэнту';
+$string['editor:contenttypedetailbuttonlabel'] = 'Дэталі';
+$string['editor:contenttypegetbuttonlabel'] = 'Атрымаць';
+$string['editor:contenttypeiconalttext'] = 'Выява';
+$string['editor:contenttypeinstallbuttonlabel'] = 'Усталяваць';
+$string['editor:contenttypeinstallerror'] = ':contentType ня можа быць усталяваны. Кантактуйце з адміністратарам.';
+$string['editor:contenttypeinstallingbuttonlabel'] = 'Усталёўка';
+$string['editor:contenttypeinstallsuccess'] = ':contentType пасьпяхова ўсталяваны!';
+$string['editor:contenttypelicensepaneltitle'] = 'Ліцэнзія';
+$string['editor:contenttypenotinstalled'] = 'Тып кантэнту не ўсталяваны';
+$string['editor:contenttypenotinstalleddesc'] = 'Вы ня маеце дазвола на ўсталёўку тыпаў кантэнту.';
+$string['editor:contenttypeowner'] = ':owner';
+$string['editor:contenttyperestricted'] = 'Забаронены тып кантэнту';
+$string['editor:contenttyperestricteddesc'] = 'Выкарыстаньне гэтага тыпу кантэнту было забаронена адміністратарам.';
+$string['editor:contenttypesearchfieldplaceholder'] = 'Шукаць тыпы кантэнту';
+$string['editor:contenttypesectionall'] = 'Усе тыпы кантэнту';
+$string['editor:contenttypeunsupportedapiversioncontent'] = 'Кантактуйце з вашым сістэмным адміністратарам дзеля забесьпячэньня патрэбных абноваў';
+$string['editor:contenttypeunsupportedapiversiontitle'] = 'Гэты тып кантэнту патрабуе новейшую версію ядра';
+$string['editor:contenttypeupdateavailable'] = 'Абнаўленьне даступнае';
+$string['editor:contenttypeupdatebuttonlabel'] = 'Абнавіць';
+$string['editor:contenttypeupdatesuccess'] = ':contentType пасьпяхова абноўлены!';
+$string['editor:contenttypeupdatingbuttonlabel'] = 'Абнаўленьне';
+$string['editor:contenttypeusebuttonlabel'] = 'Выкарыстаць';
+$string['editor:contributetranslations'] = 'Калі вы жадаеце ажыцявіць пераклад на :language, скарыстайцеся <a href=":url" target="_new">унёскамі перакладаў для H5P</a>.';
+$string['editor:copiedbutton'] = 'Скапіявана';
+$string['editor:copiedtoclipboard'] = 'Кантэнт скапіяваны ў буфер абмену';
+$string['editor:copybutton'] = 'Капіяваць';
+$string['editor:copytoclipboard'] = 'Капіяваць кантэнт H5P у буфер абмену';
+$string['editor:createcontenttablabel'] = 'Стварыць кантэнт';
+$string['editor:currentmenuselected'] = 'цяперашняе абраньне';
+$string['editor:editcopyright'] = 'Рэдагаваць аўтарскае права';
+$string['editor:editimage'] = 'Рэдагаваць выявы';
+$string['editor:editmode'] = 'Рэжым рэдагаваньня';
+$string['editor:enteraudiotitle'] = 'Устаўце спасылку аўдыё крыніцу';
+$string['editor:enteraudiourl'] = 'Увядзіце спасылку на аўдыё крыніцу';
+$string['editor:enterfullscreenbuttonlabel'] = 'Увайсьці ў поўны экран';
+$string['editor:entervideotitle'] = 'Увядзіце спасылку на YouTube ці іншую відэа крыніцу';
+$string['editor:entervideourl'] = 'Увядзіце спасылку на відэа крыніцу ці YouTube';
+$string['editor:errorcalculatingmaxscore'] = 'Нельга разьлічыць максімальны бал для гэтага кантэнта. Максімальны бал лічыцца роўным 0. Скантактуйце з вашым адміністратарам, калі гэта хібна.';
+$string['editor:errorcommunicatinghubcontent'] = 'Адбылася памылка. Калі ласка, паспрабуйце зноў.';
+$string['editor:errorcommunicatinghubtitle'] = 'Нельга скамунікаваць з хабам.';
+$string['editor:errorheader'] = 'Адбылася памылка';
+$string['editor:errornotsupported'] = 'Параметры ўтрымліваюць %used, што не падтрымліваюцца.';
+$string['editor:errorparamsbroken'] = 'Параметры хібныя.';
+$string['editor:errortoohighversion'] = 'Параметры ўтрымліваюць %used, што падтрымліваюцца толькі версіяй %supported ці ранейшай.';
+$string['editor:example'] = 'Прыклад';
+$string['editor:exceedsmax'] = 'Значэньне :property перасягае максімальнае :max.';
+$string['editor:exitfullscreenbuttonlabel'] = 'Выйсьці з поўнага экрана';
+$string['editor:expandcollapse'] = 'Разгарнуць/Згарнуць';
+$string['editor:filetolarge'] = 'Файл, які вы спрабуеце запампаваць, верагодна, завялікі.';
+$string['editor:fillinthefieldsbelow'] = 'Запоўніце ніжэйшыя палі';
+$string['editor:gethelp'] = 'Атрымаць дапамогу';
+$string['editor:h5pfileuploadservererrorcontent'] = 'Адбылася нечаканая памылка. Праверце логі памылак вашага сэрвера, каб атрымаць болей дэталяў.';
+$string['editor:h5pfileuploadservererrortitle'] = 'H5P файл ня можа быць запампованы';
+$string['editor:h5pfilevalidationfailedcontent'] = 'Пераканайцеся, што запампованы H5P мае слушны H5P кантэнт. H5P файлы, што ўтрымліваюць толькі бібліятэкі, мусяць быць запампованы праз старонку H5P бібліятэк.';
+$string['editor:h5pfilevalidationfailedtitle'] = 'Хібны H5P файл.';
+$string['editor:h5pfilewrongextensioncontent'] = 'Дазволены толькі файлы з .h5p пашырэньнем.';
+$string['editor:h5pfilewrongextensiontitle'] = 'Абраны файл нельга запампаваць';
+$string['editor:height'] = 'вышыня';
+$string['editor:hide'] = 'Схаваць';
+$string['editor:hideimportantinstructions'] = 'Схаваць важныя інструкцыі';
+$string['editor:hubpanellabel'] = 'Абраць тып кантэнту';
+$string['editor:illegaldecimalnumber'] = ':property можа ўтрымліваць толькі лічбы з макс :decimals знакамі.';
+$string['editor:imagelightboxprogress'] = ':num з :total';
+$string['editor:imagelightboxtitle'] = 'Выявы';
+$string['editor:importantinstructions'] = 'Важныя інструкцыі';
+$string['editor:insert'] = 'Уставіць';
+$string['editor:invalidformat'] = 'Значэньне поля мае хібны фармат ці забароненыя знакі.';
+$string['editor:language'] = 'Мова';
+$string['editor:librarymissing'] = 'Адсутнічае патрэбная бібліятэка %lib.';
+$string['editor:licensecandistribute'] = 'Льга распаўсюджваць';
+$string['editor:licensecanholdliable'] = 'Льга прыцягнуць да адказнасьці';
+$string['editor:licensecanmodify'] = 'Льга зьмяняць';
+$string['editor:licensecannotholdliable'] = 'Нельга прыцягваць да адказнасьці';
+$string['editor:licensecansublicense'] = 'Льга субліцэнзаваць';
+$string['editor:licensecanusecommercially'] = 'Льга камерцыйна выкарыстоўваць';
+$string['editor:licensedescription'] = 'Частка ўмоваў гэтай ліцэнзіі прыведзена ніжэй. Націсьніце на вышэйшую інфармацыйную выяву, каб прачытаць зыходны тэкст ліцэнзіі.';
+$string['editor:licensefetchdetailsfailed'] = 'Памылка атрыманьня ліцэнзійных дэталяў';
+$string['editor:licensemodalsubtitle'] = 'Абярыце ліцэнзію, каб праглядзець інфармацыю пра належнае выкарыстаньне';
+$string['editor:licensemodaltitle'] = 'Дэталі ліцэнзіі';
+$string['editor:licensemustincludecopyright'] = 'Мусіць мець аўтарскія правы';
+$string['editor:licensemustincludelicense'] = 'Мусіць мець ліцэнзію';
+$string['editor:licenseunspecified'] = 'Не вызначана';
+$string['editor:listbelowmin'] = 'Сьпіс мусіць мець прынамсі :min прадметаў, каб прадмет належна працаваў.';
+$string['editor:listexceedsmax'] = 'Сьпіс перасягнуў максімальныя :max прадметаў.';
+$string['editor:listlabel'] = 'Сьпіс';
+$string['editor:loading'] = 'Загрузка, калі ласка, пачакайце...';
+$string['editor:loadingimageeditor'] = 'Загрузка рэдактара выяваў, калі ласка, пачакайце...';
+$string['editor:loggedchanges'] = 'Залагаваныя зьмены';
+$string['editor:logthischange'] = 'Лагаваць гэтую зьмену';
+$string['editor:maxscoresemanticsmissing'] = 'Нельга атрымаць чаканую семантыку ў кантэнце.';
+$string['editor:metadata'] = 'Метададзеныя';
+$string['editor:metadatasharingandlicensinginfo'] = 'Метададзеныя (інфа пра ліцэнзаваньне і сумеснае выкарыстаньне)';
+$string['editor:missingproperty'] = 'Уласьцівасьць :property адсутнічае ў полі :index.';
+$string['editor:missingtranslation'] = '[Адсутнічае пераклад :key]';
+$string['editor:newchangehasbeenlogged'] = 'Новая зьмена была залагавана';
+$string['editor:newestfirst'] = 'Спачатку найновае';
+$string['editor:nextimage'] = 'Наступная выява';
+$string['editor:nochangeshavebeenlogged'] = 'Зьмены не былі залагаваны';
+$string['editor:nocontenttypesavailable'] = 'Тыпы кантэнту не даступныя';
+$string['editor:nocontenttypesavailabledesc'] = 'Вашы сайт мае складанасьці з далучэньнем да H5P.org і атрыманьнем сьпісу магчымых тыпаў кантэнту.';
+$string['editor:nofollow'] = 'Нельга знайсьці поле ":path".';
+$string['editor:nolanguagessupported'] = 'Мовы не падтрымліваюцца';
+$string['editor:noresultsfound'] = 'Вынікі ня знойдзены';
+$string['editor:noresultsfounddesc'] = 'Адсутнічаюць тыпы кантэнту, што адпавядаюць крытэрам пошуку.';
+$string['editor:nosemantics'] = 'Памылка, нельга загрузіць форму тыпу кантэнту.';
+$string['editor:notalltextschanged'] = 'Не ўсе тэксты былі захаваныя, таму маецца толькі частковая падтрымка :language.';
+$string['editor:notimagefield'] = '":path" ня з\'яўляецца выявай.';
+$string['editor:notimageordimensionsfield'] = '":path" ня з\'яўляецца выявай ці полем пашырэньня.';
+$string['editor:numberfield'] = 'поле нумар';
+$string['editor:numresults'] = ':num вынікаў';
+$string['editor:ok'] = 'Добра';
+$string['editor:onlynumbers'] = 'Значэньне :property можа ўтрымліваць толькі лічбы.';
+$string['editor:or'] = 'ці';
+$string['editor:orderitemdown'] = 'Адсартаваць прадметы па ўбываньню';
+$string['editor:orderitemup'] = 'Адсартаваць прадметы па павелічэньню';
+$string['editor:outofstep'] = 'Значэньне :property льга толькі зьмяніць на кроках з :step.';
+$string['editor:pasteandreplacebutton'] = 'Уставіць і замяніць';
+$string['editor:pasteandreplacefromclipboard'] = 'Замяніць існы кантэнт на H5P кантэнт з буфера абмена';
+$string['editor:pastebutton'] = 'Уставіць';
+$string['editor:pastecontent'] = 'Замяніць кантэнт';
+$string['editor:pastecontentnotsupported'] = 'Кантэнт H5P з буфера абмену не падтрымлівае гэты кантэкст';
+$string['editor:pastecontentrestricted'] = 'Кантэнт з буфера абмену быў забаронены на гэтым сайце';
+$string['editor:pasteerror'] = 'Нельга ўставіць з буфера абмену';
+$string['editor:pastefromclipboard'] = 'Уставіць H5P кантэнт з буфера абмену';
+$string['editor:pastenocontent'] = 'У буферы абмену няма H5P кантэнту';
+$string['editor:pastetoonew'] = 'H5P кантэнт з буфера абмену мае вышэйшую версіі (:clip) за падтрымліваемую ў гэтым кантэксьце (:local). Калі магчыма, спачатку паспрабуйце абнавіць кантэнт, у які ўстаўляеце, потым паспрабуйце ўставіць сюды кантэнт наноў.';
+$string['editor:pastetooold'] = 'H5P кантэнт з буфера абмену мае ніжэйшую версіі (:clip) за падтрымліваемую ў гэтым кантэксьце (:local). Калі магчыма, спачатку паспрабуйце абнавіце кантэнт, які ўстаўляеце, потым паспрабуйце ўставіць сюды кантэнт наноў.';
+$string['editor:popularfirst'] = 'Спачатку папулярнае';
+$string['editor:previousimage'] = 'Папярэдняя выява';
+$string['editor:proceedbuttonlabel'] = 'Працягнуць, каб захаваць';
+$string['editor:readless'] = 'Чытаць меней';
+$string['editor:readmore'] = 'Чытаць болей';
+$string['editor:recentlyusedfirst'] = 'Спачатку апошняе карыстанае';
+$string['editor:reloadbuttonlabel'] = 'Перазагрузіць';
+$string['editor:removefile'] = 'Выдаліць файл';
+$string['editor:removeimage'] = 'Выдаліць выяву';
+$string['editor:removeitem'] = 'Выдаліць прадмет';
+$string['editor:requiredproperty'] = ':property патрабуецца і мусіць мець значэньне.';
+$string['editor:resettooriginallabel'] = 'Скінуць да зыходнага';
+$string['editor:savelabel'] = 'Захаваць';
+$string['editor:savemetadata'] = 'Захаваць метададзеныя';
+$string['editor:screenshots'] = 'Скрыншоты';
+$string['editor:scriptmissing'] = 'Нельга загрузіць абнаўленьні скрыпта для %lib.';
+$string['editor:searchresults'] = 'Шукаць вынікі';
+$string['editor:selectfiletoupload'] = 'Абраць файл для запампоўкі';
+$string['editor:selectlibrary'] = 'Абраць бібліятэку, якую жадаеце выкарыстоўваць для вашага кантэнта.';
+$string['editor:semanticserror'] = 'Семантычная памылка: :error';
+$string['editor:show'] = 'Паказаць';
+$string['editor:showimportantinstructions'] = 'Паказаць інструкцыі';
+$string['editor:tabtitlebasicfileupload'] = 'Запампаваць файл';
+$string['editor:tabtitleinputlinkurl'] = 'Спасылка';
+$string['editor:textfield'] = 'тэкставае поле';
+$string['editor:thecontenttype'] = 'тып кантэнта';
+$string['editor:thiswillpotentially'] = 'Гэта патэнцыйна скіне ўсе пераклады і тэкст. Гэтае дзеяньне нельга скасаваць. Кантэнт самастойна не зьменіцца. Жадаеце працягнуць?';
+$string['editor:title'] = 'Назва';
+$string['editor:toolong'] = 'Значэньне поля задоўгае; яно мусіць мець :max знакаў ці меней.';
+$string['editor:tryagain'] = 'Паспрабаваць зноў';
+$string['editor:tutorial'] = 'Дапаможнік';
+$string['editor:unabletointerpreterror'] = 'Нельга інтэрпрэтавць адказ.';
+$string['editor:unabletointerpretsolution'] = 'Калі ласка, праверце вашы лог памылак.';
+$string['editor:unknownfieldpath'] = 'Нельга знайсьці ":path".';
+$string['editor:unknownfileuploaderror'] = 'Памылка запампоўкі невядомага файла';
+$string['editor:unknownlibrary'] = 'На жаль, абраны тып кантэнта \'%lib\' не ўсталяваны на гэтай сістэме.';
+$string['editor:untitled'] = ':libraryTitle без назвы';
+$string['editor:uploadaudiotitle'] = 'Запампаваць аўдыё файл';
+$string['editor:uploaderror'] = 'Памылка файла запампоўкі';
+$string['editor:uploadfilebuttonchangelabel'] = 'Зьмяніць файл';
+$string['editor:uploadfilebuttonlabel'] = 'Запампаваць файл';
+$string['editor:uploading'] = 'Запампоўка, калі ласка, пачакайце...';
+$string['editor:uploadingthrobber'] = 'Запампоўваецца цяпер...';
+$string['editor:uploadinstructionscontent'] = 'Вы мусіце пачаць з прыкладаў з <a href="https://h5p.org/content-types-and-applications" target="blank">H5P.org</a>.';
+$string['editor:uploadinstructionstitle'] = 'Запампаваць H5P файл.';
+$string['editor:uploadplaceholder'] = 'Файл не абраны';
+$string['editor:uploadsuccess'] = ':title пасьпяхова абноўлена!';
+$string['editor:uploadtablabel'] = 'Запампаваць';
+$string['editor:uploadvideotitle'] = 'Запампаваць відэа файл';
+$string['editor:usedforsearchingreportsandcopyrightinformation'] = 'Выкарыстоўваецца для пошуку, справаздач і інфармацыі пра аўтарскія правы';
+$string['editor:videoquality'] = 'Назва якасьці відэа';
+$string['editor:videoqualitydefaultlabel'] = 'Якасьць :index';
+$string['editor:videoqualitydescription'] = 'Гэтая назва дапамагае карыстальніку зразумець цяперашнюю якасьць відэа. Напрыклад, 1080p, 720p, HD ці Мабільнае';
+$string['editor:warningchangebrowsingtoseeresults'] = 'Націсьніце <em>Усе</em>, каб атрымаць сьпіс усіх тыпаў кантэнта, якія вы можаце ўсталяваць.';
+$string['editor:warningnocontenttypesinstalled'] = 'Вы ня маеце ніякіх усталяваных тыпаў кантэнту.';
+$string['editor:warningupdateavailablebody'] = 'Абнавіць да апошняй версіі для палепшэньня досьведа.';
+$string['editor:warningupdateavailabletitle'] = 'Даступна новая версія :contentType.';
+$string['editor:width'] = 'шырыня';
+$string['pluginname'] = 'Фрэймворк H5P v1.24';
+$string['pluginname_help'] = 'Фрэймворк H5P версіі 1.24.';
+$string['privacy:metadata'] = 'Фрэймворк H5P v1.24 не захоўвае аніякіх асабістых дадзеных.';
