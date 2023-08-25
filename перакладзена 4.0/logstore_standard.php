bf701bef4404f28964e27908ea0a0136c9f7110a
Denis <denis.makouski@bseu.by>
2023-08-25 15:07:06 +0300

    ++

---
+++ перакладзена 4.0/logstore_standard.php
@@ -0,0 +1,43 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'logstore_standard', language 'en', version '4.0'.
+ *
+ * @package     logstore_standard
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['buffersize'] = 'Памер буфера запісу';
+$string['jsonformat'] = 'JSON фармат';
+$string['jsonformat_desc'] = 'Выкарыстоўваць стандартны фармат JSON заміж серылізаваных дадзеных PHP у полі базы дадзеных \'іншае\'.';
+$string['pluginname'] = 'Стандартнае лагаваньне';
+$string['pluginname_desc'] = 'Плагін лагаваньня захоўвае запісы логаў у табліцы базы дадзеных Moodle.';
+$string['privacy:metadata:log'] = 'Калекцыя апошніх падзеяў';
+$string['privacy:metadata:log:anonymous'] = 'Ці была падзея пазначана як ананімная';
+$string['privacy:metadata:log:eventname'] = 'Назва падзеі';
+$string['privacy:metadata:log:ip'] = 'IP-адрас, выкарыстаны падчас падзеі';
+$string['privacy:metadata:log:origin'] = 'Крыніца падзеі';
+$string['privacy:metadata:log:other'] = 'Дадатковая інфа пра падзею';
+$string['privacy:metadata:log:realuserid'] = 'ID сапраўднага карыстальніка падзеі, які маскаваўся пад карыстальніка';
+$string['privacy:metadata:log:relateduserid'] = 'ID карыстальніка, зьвязанага з гэтай падзеяй';
+$string['privacy:metadata:log:timecreated'] = 'Час адбыцьця падзеі';
+$string['privacy:metadata:log:userid'] = 'ID карыстальніка, пачаўшага падзею';
+$string['taskcleanup'] = 'Ачыстка табліцы логаў';
