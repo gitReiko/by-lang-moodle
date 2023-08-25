5c618521d416675d8036e6453b0e8d6c25a4704c
Denis <denis.makouski@bseu.by>
2023-08-25 15:01:58 +0300

    ++

---
+++ перакладзена 4.0/logstore_database.php
@@ -0,0 +1,67 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'logstore_database', language 'en', version '4.0'.
+ *
+ * @package     logstore_database
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['buffersize'] = 'Памер буфера';
+$string['buffersize_help'] = 'Колькасьць запісаў лога, што ўстаўляецца за адну пакетную аперацыю базы дадзеных, павышае прадукцыйнасьць.';
+$string['conectexception'] = 'Нельга далучыцца да базы дадзеных.';
+$string['create'] = 'Стварыць';
+$string['databasecollation'] = 'Сартаваньне базы дадзеных';
+$string['databasehandlesoptions'] = 'Налады апрацоўкі базы дадзеных';
+$string['databasehandlesoptions_help'] = 'Ці мусіць вонкавая база дадзеных апрацоўваць свае уласныя налады.';
+$string['databasepersist'] = 'Сталыя далучэньні базы дадзеных';
+$string['databaseschema'] = 'Схема базы дадзеных';
+$string['databasesettings'] = 'Налады базы дадзеных';
+$string['databasesettings_help'] = 'Дэталі пра далучэньне да вонкавай базы дадзеных логаў: {$a}';
+$string['databasetable'] = 'Табліца базы дадзеных';
+$string['databasetable_help'] = 'Назва табліцы, у якой будуць захоўвацца логі. Гэтая табліца мусіць мець структуру, індэнтычную logstore_standard (mdl_logstore_standard_log).';
+$string['filters'] = 'Фільтры логаў';
+$string['filters_help'] = 'Уключыць фільтры, якія будуць выключаць частку дзеяньняў з логаў.';
+$string['includeactions'] = 'Уключыць дзеяньні наступных тыпаў';
+$string['includelevels'] = 'Уключыць дзеяньні наступных адукацыйных роўняў';
+$string['jsonformat'] = 'JSON фармат';
+$string['jsonformat_desc'] = 'Выкарыстоўваць стандартны фармат JSON заміж серылізаваных дадзеных PHP у полі базы дадзеных \'іншае\'.';
+$string['logguests'] = 'Лагаваць гасьцявыя дзеяньні';
+$string['other'] = 'Іншае';
+$string['participating'] = 'Удзельніцтва';
+$string['pluginname'] = 'Логі вонкавай базы дадзеных';
+$string['pluginname_desc'] = 'Плагін, які захоўвае запісы логаў у табліцу вонкавай базы дадзеных.';
+$string['privacy:metadata:log'] = 'Калекцыя апошніх падзей';
+$string['privacy:metadata:log:anonymous'] = 'Ці была падзея пазначана як ананімная';
+$string['privacy:metadata:log:eventname'] = 'Назва падзеі';
+$string['privacy:metadata:log:ip'] = 'IP-адрас, выкарыстаны падчас падзеі';
+$string['privacy:metadata:log:origin'] = 'Крыніца падзеі';
+$string['privacy:metadata:log:other'] = 'Дадатковая інфа пра падзею';
+$string['privacy:metadata:log:realuserid'] = 'ID сапраўднага карыстальніка падзеі, які маскаваўся пад карыстальніка';
+$string['privacy:metadata:log:relateduserid'] = 'ID карыстальніка, зьвязанага з гэтай падзеяй';
+$string['privacy:metadata:log:timecreated'] = 'Час адбыцьця падзеі';
+$string['privacy:metadata:log:userid'] = 'ID карыстальніка, пачаўшага падзею';
+$string['read'] = 'Чытаньне';
+$string['tablenotfound'] = 'Азначаная табліца ня знойдзена';
+$string['teaching'] = 'Навучаньне';
+$string['testingsettings'] = 'Тэсітаваньне наладаў базы дадзеных...';
+$string['testsettings'] = 'Тэставаць далучэньне';
+$string['update'] = 'Абнавіць';
