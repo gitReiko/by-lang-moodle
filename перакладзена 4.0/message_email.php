b8cc5bf75539d899b62318e3dc06347c87f6b4a1
Denis <denis.makouski@bseu.by>
2023-08-25 16:36:01 +0300

    ++

---
+++ перакладзена 4.0/message_email.php
@@ -0,0 +1,51 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'message_email', language 'en', version '4.0'.
+ *
+ * @package     message_email
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['email'] = 'Адправіць імэйл абвесткі да';
+$string['emaildigestsubject'] = 'Дайджэст паведамленьняў';
+$string['emaildigestunreadmessages'] = 'Непрачытаныя паведамленьні';
+$string['emaildigestviewallmessages'] = 'Праглядзець усе паведамленьні';
+$string['emailonlyfromnoreplyaddress'] = 'Заўжды адправляць імэйлы з адрасу ня дзеля адказу (no-reply)?';
+$string['ifemailleftempty'] = 'Пакінуць пустым для адпраўкі абвестак да {$a}';
+$string['messagedigestemailsubject'] = '{$a}: Дайджэст паведамленьняў';
+$string['pluginname'] = 'Імэйл';
+$string['privacy:metadata:attachment'] = 'Файл у файлавай сістэме.';
+$string['privacy:metadata:attachname'] = 'Назва прымацаванага файла (пашырэньне паказвае MIME).';
+$string['privacy:metadata:externalpurpose'] = 'Гэтая інфармацыя дасылаецца на вонкавы SMTP-сэрвер дзеля наступнага адпраўленьня атрымальніку ў форме імэйла.';
+$string['privacy:metadata:fullmessage'] = 'Поўнае паведамленьне ў пададзеным фармаце.';
+$string['privacy:metadata:fullmessagehtml'] = 'Поўная версія паведамленьня.';
+$string['privacy:metadata:message_email_messages'] = 'Сьпіс карыстальнікаў залічаных праз LTI правайдар';
+$string['privacy:metadata:message_email_messages:conversationid'] = 'ID гутаркі, што адпраўлена да';
+$string['privacy:metadata:message_email_messages:messageid'] = 'ID паведамленьня, што адпраўлена да';
+$string['privacy:metadata:message_email_messages:useridto'] = 'ID карыстальніка, што атрымае паведамленьне';
+$string['privacy:metadata:recipient'] = 'Атрымальнік паведамленьня.';
+$string['privacy:metadata:replyto'] = 'Імэйл дзеля адказу.';
+$string['privacy:metadata:replytoname'] = 'Навза адказу для атрымальніка.';
+$string['privacy:metadata:subject'] = 'Тэма паведамленьня.';
+$string['privacy:metadata:userfrom'] = 'Карыстальнік, што адправіў паведамленьне.';
+$string['privacy:preference:email'] = 'Пераважны імэйл абвяшчэньня';
+$string['tasksendemail'] = 'Рассыланьні дайджэстаў паведамленьняў';
