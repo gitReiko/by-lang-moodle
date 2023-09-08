dcbe7738feb07b33f122ae914bf80a95b230f755
Denis <denis.makouski@bseu.by>
2023-09-08 17:25:54 +0300

    ++

---
+++ перакладзена 4.0/label.php
@@ -0,0 +1,62 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'label', language 'en', version '4.0'.
+ *
+ * @package     label
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['configdndmedia'] = 'Прапаноўваць ствараць цэтлік падчас перасоўваньня медыя-файлаў у курс';
+$string['configdndresizeheight'] = 'Падчас стварэньня цэтліка з перасунутай выявы, зьмяняць вышыню выявы, калі яна перасягае гэтую колькасьць пікселяў (0 - бяз зьмены памеру)';
+$string['configdndresizewidth'] = 'Падчас стварэньня цэтліка з перасунутай выявы, зьмяняць шырыню выявы, калі яна перасягае гэтую колькасьць пікселяў (0 - бяз зьмены памеру)';
+$string['dndmedia'] = 'Перасоўваньне медыя';
+$string['dndresizeheight'] = 'Зьмена вышыні пры перасоўваньні';
+$string['dndresizewidth'] = 'Зьмена шырыні пры перасоўваньні';
+$string['dnduploadlabel'] = 'Дадаць медыя на старонку курса';
+$string['dnduploadlabeltext'] = 'Дадаць цэтлік на старонку курса';
+$string['indicator:cognitivedepth'] = 'Пазнаваньне цэтліка';
+$string['indicator:cognitivedepth_help'] = 'Гэты паказьнік заснаваны на глыбіні пазнаваньня, якую дасягнуў студэнт у рэсурсе-Цэтліке.';
+$string['indicator:cognitivedepthdef'] = 'Пазнаваньне цэтліка';
+$string['indicator:cognitivedepthdef_help'] = 'Удзельнік дасягнуў гэтага адсотку пазнавальнага ўцягненьня, прапанаванага рэсурсам-Цэтлікам у аналізаваны тэрмін (Роўні = Без прагляду, Прагляд)';
+$string['indicator:cognitivedepthdef_link'] = 'Learning_analytics_indicators#Cognitive_depth';
+$string['indicator:socialbreadth'] = 'Сацыяльнасьць цэтліка';
+$string['indicator:socialbreadth_help'] = 'Гэты паказьнік заснаваны на сацыяльнай шырыні, якую дасягнуў студэнт у рэсурсе-Цэтліке.';
+$string['indicator:socialbreadthdef'] = 'Сацыяльнасьць цэтліка';
+$string['indicator:socialbreadthdef_help'] = 'Удзельнік дасягную гэтага адсотку сацыяльнай шырыні, прапанаванай рэсурсам-Цэтлікам у аналізаваны тэрмін (Роўні = Без удзельнікаў, Самотны удзел)';
+$string['indicator:socialbreadthdef_link'] = 'Learning_analytics_indicators#Social_breadth';
+$string['label:addinstance'] = 'Дадаць новы цэтлік';
+$string['label:view'] = 'Праглядзець цэтлік';
+$string['labeltext'] = 'Тэкст цэтліка';
+$string['modulename'] = 'Цэтлік';
+$string['modulename_help'] = 'Модуль-цэтлік дазваляе ўстаўляць тэкст і мультымедыя на старонку курса паміж спасылкамі на іншыя рэсурсы і актыўнасьці. Цэтлікі вельмі ўніверсальныя і пры прадуманым выкарыстаньні могуць палепшыць зьнешні выгляд курса.
+
+Цэтлікі льга выкарыстоўваць дзеля
+
+* Падзелу доўгага сьпісу актыўнасьцяў падзагалоўкам ці выявай
+* Адлюстраваньня ўбудаванага аўдыя- ці відэа-файлу на старонцы курса
+* Даданьня кароткага апісаньня падзелу курса';
+$string['modulename_link'] = 'mod/label/view';
+$string['modulenameplural'] = 'Цэтлікі';
+$string['pluginadministration'] = 'Адміністраваньне цэтліка';
+$string['pluginname'] = 'Цэтлік';
+$string['privacy:metadata'] = 'Плагін Цэтлік не захоўвае аніякіх асабістых дадзеных.';
+$string['search:activity'] = 'Цэтлік';
