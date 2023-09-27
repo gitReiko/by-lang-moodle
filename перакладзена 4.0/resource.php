5d3e7824e31f3857d46df3b7f4ea0695ec7c4641
Denis <denis.makouski@bseu.by>
2023-09-27 16:57:39 +0300

    ++

---
+++ перакладзена 4.0/resource.php
@@ -0,0 +1,132 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'resource', language 'en', version '4.0'.
+ *
+ * @package     resource
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['clicktodownload'] = 'Націсьніце па спасылцы {$a}, каб спампаваць файл.';
+$string['clicktoopen2'] = 'Націсьніце па спасылцы {$a}, каб праглядзець файл.';
+$string['configdisplayoptions'] = 'Абярыце ўсе опцыя, якія мусяць быць даступнымі, наступныя налады ня зьменяцца. Утрымлівайце кнопку CTRL дзеля абраньня некалькіх палёў.';
+$string['configframesize'] = 'Калі вэб старонка ці запампаваны файл адлюстроўваецца ў рамцы, гэтае значэньне вызначае вышыню (у пікселях) верхняй рамцы (якая зьмяшчае навігацыю).';
+$string['configparametersettings'] = 'Гэтая налада ўсталёўвае стандартнае значэньне для Панэлі наладаў параметраў падчас даданьня новых рэсурсаў. Пасьля першага раза, гэта становіцца асабістымі перавагамі карыстальніка.';
+$string['configpopup'] = 'Ці трэба паказваць модуль ува ўсплыўным вакне па-стандарту падчас даданьня новага рэсурса?';
+$string['configpopupdirectories'] = 'Ці мусяць усплыўныя вокны паказваць прамыя спасылкі па-стандарту?';
+$string['configpopupheight'] = 'Якая стандартная вышыня мусіць быць у ўсплыўных вокнаў?';
+$string['configpopuplocation'] = 'Ці мусяць усплыўныя вокны паказваць панэль месцаваньня па-стандарту?';
+$string['configpopupmenubar'] = 'Ці мусяць усплыўныя вокны паказваць панэль меню па-стандарту?';
+$string['configpopupresizable'] = 'Ці змогуць усплыўныя вокны зьмяняць памеры па-стандарту?';
+$string['configpopupscrollbars'] = 'Ці мусяць усплыўныя вокны мець пракрутку па-стандарту?';
+$string['configpopupstatus'] = 'Ці мусяць усплыўныя вокны паказваць панэль статуса па-стандарту?';
+$string['configpopuptoolbar'] = 'Ці мусяць усплыўныя вокны паказваць панэль прыладаў па-стандарту?';
+$string['configpopupwidth'] = 'Якая стандартная шырыня мусіць быць у ўсплыўных вокнаў?';
+$string['contentheader'] = 'Зьмесьціва';
+$string['displayoptions'] = 'Даступныя опцыі адлюстраваньня';
+$string['displayselect'] = 'Адлюстраваньне';
+$string['displayselect_help'] = 'Гэтая налада, разам з тыпам спасылкі на файл і дазволам браўзара ўбудоўваць, вызначаюць шлях адлюстраваньня файла. Існуюць наступныя опцыі:
+
+* Аўта - Лепшая опцыя адлюстраваньня тыпа файла абіраецца аўтаматычна
+* Убудоўваць - Файл адлюстроўваецца пад панэлью навігацыі разам з апісаньнем спасылкі і ўсімі блокамі
+* Прымусова спампоўваць - Файл неадкладна спампоўваецца
+* Адкрываць - У вакне браўзара адлюстроўваецца толькі файл
+* Ва ўсплыўным акне - Файл адлюстроўваецца ў новым вакне браўзара без меню ці адраснага радка
+* У рамцы - Файл адлюстроўваецца ў рамцы пад панэлью навігацыі і апісаньнем спасылкі
+* Новае акно - Файл адлюстроўваецца ў новым вакне браўзара з меню і адрасным радком';
+$string['displayselect_link'] = 'mod/file/mod';
+$string['displayselectexplain'] = 'Абярыце тып адлюстраваньня, на жаль, не ўсе тыпы падыходзяць для ўсіх файлаў.';
+$string['dnduploadresource'] = 'Стварыць рэсурс-файл';
+$string['encryptedcode'] = 'Зашыфраваны код';
+$string['filenotfound'] = 'Файл ня знойдзены, прабачце.';
+$string['filterfiles'] = 'Выкарыстоўваць фільтры для зьмесьціва файла';
+$string['filterfilesexplain'] = 'Абярыце тып фільтрацыі зьмесіціва файла. Зьвярніце ўвагу, гэта можа выклікаць праблемы для некаторых Flash- і Java-аплетаў. Калі ласка, пераканайцеся, што ўсе тэкставыя файлы маюць кадаваньне UTF-8.';
+$string['filtername'] = 'Аўтастварэньне спасылак на назвы рэсурсаў';
+$string['forcedownload'] = 'Прымусовая спампоўка';
+$string['framesize'] = 'Вышыня рамцы';
+$string['indicator:cognitivedepth'] = 'Пазнаваньне файла';
+$string['indicator:cognitivedepth_help'] = 'Гэты паказьнік заснаваны на глыбіні пазнаваньня, якую дасягнуў студэнт у рэсурсе-Файле.';
+$string['indicator:cognitivedepthdef'] = 'Пазнаваньне файла';
+$string['indicator:cognitivedepthdef_help'] = 'Удзельнік дасягнуў гэтага адсотку пазнавальнага ўцягненьня, прапанаваны рэсурсам-Файлам у аналізаваны тэрмін (Роўні = Без прагляду, Прагляд)';
+$string['indicator:cognitivedepthdef_link'] = 'Learning_analytics_indicators#Cognitive_depth';
+$string['indicator:socialbreadth'] = 'Сацыяльнасьць файла';
+$string['indicator:socialbreadth_help'] = 'Гэты паказьнік заснаваны на сацыяльнай шырыні, якую дасягнуў студэнт у рэсурсе-Файле.';
+$string['indicator:socialbreadthdef'] = 'Сацыяльнасьць файла';
+$string['indicator:socialbreadthdef_help'] = 'Удзельнік дасягную гэтага адсотку сацыяльнай шырыні, прапанаванай рэсурсам-Файлам у аналізаваны тэрмін (Роўні = Без удзельнікаў, Самотны удзел)';
+$string['indicator:socialbreadthdef_link'] = 'Learning_analytics_indicators#Social_breadth';
+$string['legacyfiles'] = 'Міграцыя старых файлаў курса';
+$string['legacyfilesactive'] = 'Актыўна';
+$string['legacyfilesdone'] = 'Выканана';
+$string['modifieddate'] = 'Зьменена {$a}';
+$string['modulename'] = 'Файл';
+$string['modulename_help'] = 'Модуль-файл дазваляе настаўніку падаваць файл у якасьці рэсурса курса. Па магчымасьці файл будзе адлюстроўвацца ўва інтэрфэйсе курса, у адваротным выпадку студэнтам будзе прапанавана спампаваць яго. Файл можа мець дапаможныя файлы, напрыклад, HTML-старонка можа мець убудаваныя выявы.
+
+Зьвярніце ўвагу, дзеля адкрыцьця гэтага файла на кампутары студэнта можа спатрэбіцца ўсталёўка адпаведнага праграмнага забесьпячэньня.
+
+Файл льга выкарыстоўваць дзеля
+
+* Паданьня прэзентацыі класу
+* Даданьня міні вэб-сайта ў якасьці рэсурса курса
+* Паданьня чарнавога файла праграмнага забесьпячэньня, каб студэнты змаглі адрэдагаваць яго і адправіць на ацэнку';
+$string['modulename_link'] = 'mod/resource/view';
+$string['modulenameplural'] = 'Файлы';
+$string['notmigrated'] = 'Гэты састарэлы тып рэсурса ({$a}) дагэтуль не міграваў, прабачце.';
+$string['optionsheader'] = 'Опцыі адлюстраваньня';
+$string['page-mod-resource-x'] = 'Усякая старонка модуля-файла';
+$string['pluginadministration'] = 'Адміністраваньне модуля-файл';
+$string['pluginname'] = 'Файл';
+$string['popupheight'] = 'Вышыня ўсплыўнага акна (у пікселях)';
+$string['popupheightexplain'] = 'Вызначае стандартную вышыню ўсплыўнага акна.';
+$string['popupresource'] = 'Гэты рэсурс мусіць з\'явіцца ўва ўсплыўным вакне.';
+$string['popupresourcelink'] = 'Калі не адбылася, націсьніце тут: {$a}';
+$string['popupwidth'] = 'Шырыня ўсплыўнага акна (у пікселях)';
+$string['popupwidthexplain'] = 'Вызначае стандартную шырыню ўсплыўнага акна.';
+$string['printintro'] = 'Адлюстроўваць апісаньне рэсурса';
+$string['printintroexplain'] = 'Ці трэба адлюстроўваць апісаньне спасылкі пад зьмесьцівам? Некаторыя тыпы адлюстраваньня не паказваюць апісаньне, нават калі гэтая функція ўключана.';
+$string['privacy:metadata'] = 'Плагін рэсурс-Файл не захоўвае аніякіх асабістых дадзеных.';
+$string['resource:addinstance'] = 'Дадаць новы рэсурс';
+$string['resource:exportresource'] = 'Экспартаваць рэсурс';
+$string['resource:view'] = 'Праглядзець рэсурс';
+$string['resourcecontent'] = 'Файлы і падтэчкі';
+$string['resourcedetails_sizedate'] = '{$a->size} {$a->date}';
+$string['resourcedetails_sizetype'] = '{$a->size} {$a->type}';
+$string['resourcedetails_sizetypedate'] = '{$a->size} {$a->type} {$a->date}';
+$string['resourcedetails_typedate'] = '{$a->type} {$a->date}';
+$string['search:activity'] = 'Файл';
+$string['selectmainfile'] = 'Калі ласка, абярыце асноўны файл націскам па выяве, якая знаходзіцца побач з назвай файла.';
+$string['showdate'] = 'Паказаць дату запампоўкі/зьмены';
+$string['showdate_desc'] = 'Ці трэба паказваць дату запампоўкі/зьмены на старонцы курса?';
+$string['showdate_help'] = 'Адлюстроўвае дату запампоўкі/зьмены побач са спасылкай на файл.
+
+Калі ў дадзеным рэсурсе знаходзіцца некалькі файлаў, то адлюстроўваецца дата запампоўкі/зьмены пачатковага файла.';
+$string['showsize'] = 'Паказваць памер';
+$string['showsize_desc'] = 'Ці трэба паказваць памер файла на старонцы курса?';
+$string['showsize_help'] = 'Адлюстроўвае памер файла, напрыклад, \'3.1 МБ\', побач са спасылкай на файл.
+
+Калі ў дадзеным рэсурсе знаходзіцца некалькі файлаў, то адлюстроўваецца агульны памер усіх файлаў.';
+$string['showtype'] = 'Паказваць тып';
+$string['showtype_desc'] = 'Ці трэба паказваць тып файла на старонцы курса, напрыклад, \'Дакумент Word\'?';
+$string['showtype_help'] = 'Адлюстроўвае тып файла, напрыклад, \'Дакумент Word\', побач са спасылкай на файл.
+
+Калі ў дадзеным рэсурсе знаходзіцца некалькі файлаў, то адлюстроўваецца тып першага файла.
+
+Калі тып файла невядомы сістэме, ён не адлюстроўваецца.';
+$string['uploadeddate'] = 'Запампована {$a}';
