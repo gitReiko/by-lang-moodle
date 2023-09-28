ef2b8b5354b8bc131ca29020be881638a61b6310
Denis <denis.makouski@bseu.by>
2023-09-28 11:49:18 +0300

    ++

---
+++ перакладзена 4.0/choice.php
@@ -0,0 +1,163 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'choice', language 'en', version '4.0'.
+ *
+ * @package     choice
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addmorechoices'] = 'Дадаць болей выбараў';
+$string['allowmultiple'] = 'Дазволіць абіраць болей за адзін выбар';
+$string['allowupdate'] = 'Дазволіць зьмяняць выбар';
+$string['answered'] = 'Адказана';
+$string['atleastoneoption'] = 'Вы мусіце даць прынамсі адзін магчымы адказ.';
+$string['calendarend'] = '{$a} закрываецца';
+$string['calendarstart'] = '{$a} адкрываецца';
+$string['cannotsubmit'] = 'Прабачце, узьніклі праблемы з адпраўкай вашага выбара. Калі ласка, паспрабуйце яшчэ адзін раз.';
+$string['choice'] = 'Выбар';
+$string['choice:addinstance'] = 'Дадаць новы выбар';
+$string['choice:choose'] = 'Запісаць выбар';
+$string['choice:deleteresponses'] = 'Зьмяніць і выдаліць водгук';
+$string['choice:downloadresponses'] = 'Спампаваць водгукі';
+$string['choice:readresponses'] = 'Праглядзець водгукі';
+$string['choice:view'] = 'Праглядзець актыўнасьць-выбар';
+$string['choiceclose'] = 'Дазволіць водгукі да';
+$string['choicefull'] = 'Адна ці некалькі абраных вамі опцыяў ужо запоўнены. Вашы водгук ня быў захаваны. Калі ласка, зрабіце іншы выбар.';
+$string['choicename'] = 'Назва выбара';
+$string['choiceopen'] = 'Дазволіць водгукі ад';
+$string['choiceoptions'] = 'Опцыі выбару';
+$string['choiceoptions_help'] = 'Тут вызначаюцца опцыі, зь якіх удзельнікі могуць абіраць.
+
+Вы можаце стварыць любую колькасьць выбараў. Калі пакінуць частку опцыяў пустымі, яны ня будуць адлюстроўвацца. Калі вам трэба болей за 8 варыянтаў, націсьніце кнопку "Дадаць 3 поля ў форму".';
+$string['choicesaved'] = 'Вашы выбар быў захаваны';
+$string['choicetext'] = 'Тэкст выбару';
+$string['chooseaction'] = 'Абярыце дзеяньне ...';
+$string['chooseoption'] = 'Абраць: {$a}';
+$string['closebeforeopen'] = 'Вы вызначылі дату закрыцьця ранейшую за дату адкрыцьця.';
+$string['completiondetail:submit'] = 'Зрабіць выбар';
+$string['completionsubmit'] = 'Паказваць выкананым пасьля выбару карыстальніка';
+$string['description'] = 'Апісаньне';
+$string['deselectalloption'] = 'Скасаваць выбар усяго "{$a}"';
+$string['displayhorizontal'] = 'Адлюстроўваць гарызантальна';
+$string['displaymode'] = 'Рэжым адлюстраваньня опцыяў';
+$string['displayvertical'] = 'Адлюстроўваць вертыкальна';
+$string['eventanswercreated'] = 'Даданы адказ выбару';
+$string['eventanswerdeleted'] = 'Выдалены адказ выбару';
+$string['eventanswersubmitted'] = 'Выбар зроблены';
+$string['eventanswerupdated'] = 'Выбар абноўлены';
+$string['eventreportdownloaded'] = 'Справаздача выбару спампавана';
+$string['eventreportviewed'] = 'Справаздача выбару прагледжана';
+$string['expired'] = 'Гэтая актыўнасьць закрываецца {$a}.';
+$string['full'] = '(Поўны)';
+$string['havetologin'] = 'Для таго каб зрабіць свой выбар, вы мусіце спачатку ўвайсьці';
+$string['includeinactive'] = 'Уключыць водгукі ад неактыўных/заблакаваных карыстальнікаў';
+$string['indicator:cognitivedepth'] = 'Пазнаваньне выбару';
+$string['indicator:cognitivedepth_help'] = 'Гэты паказьнік заснаваны на глыбіні пазнаваньня, якую дасягнуў студэнт у актыўнасьці-Выбары.';
+$string['indicator:cognitivedepthdef'] = 'Пазнаваньне выбару';
+$string['indicator:cognitivedepthdef_help'] = 'Удзельнік дасягнуў гэтага адсотку пазнавальнага ўцягненьня, прапанаваны актыўнасьцю-Выбарам ў аналізаваны тэрмін (Роўні = Без прагляду, Прагляд, Адпраўка, Прагляд водгука)';
+$string['indicator:cognitivedepthdef_link'] = 'Learning_analytics_indicators#Cognitive_depth';
+$string['indicator:socialbreadth'] = 'Сацыяльнасьць выбару';
+$string['indicator:socialbreadth_help'] = 'Гэты паказьнік заснаваны на сацыяльнай шырыні, якую дасягнуў студэнт у актыўнасьці-Выбары.';
+$string['indicator:socialbreadthdef'] = 'Сацыяльнасьць выбару';
+$string['indicator:socialbreadthdef_help'] = 'Удзельнік дасягную гэтага адсотку сацыяльнай шырыні, прапанаванай актыўнасьцю-Выбарам у аналізаваны тэрмін (Роўні = Без удзельнікаў, Самотны удзел, Удзел зь іншымі)';
+$string['indicator:socialbreadthdef_link'] = 'Learning_analytics_indicators#Social_breadth';
+$string['limit'] = 'Ліміт';
+$string['limita'] = 'Ліміт: {$a}';
+$string['limitanswers'] = 'Лімітаваць колькасьць дазволеных водгукаў';
+$string['limitanswers_help'] = 'Гэтая опцыя дазваляе лімітаваць колькасьць удзельнікаў, якія могуць абраць кожную опцыю выбару. Калі ліміт дасягнуты, болей ніхто ня можа абраць гэтую опцыю.
+
+Зьвярніце ўвагу, калі вы карыстаецеся гэтай актыўнасьцю ў групавым рэжыме, то лімітаваньне распаўсюджваецца на кожную групу асобна. Напрыклад, калі лімітаваньне ўсталёвана на 5, то 5 чальцоў кожнай групы могуць абраць опцыю. Калі ўдзельнічае 3 групы, значыць, опцыю могуць абраць 5 x 3 = 15 удзельнікаў.
+
+Калі лімітаваньне адключана, то кожную опцыю зможа абраць любая калькасьць удзельнікаў.';
+$string['limitno'] = 'Ліміт {no}';
+$string['modulename'] = 'Выбар';
+$string['modulename_help'] = 'Модуль актыўнасьць-выбар дазваляе настаўніку задаць адно пытаньне і прапанаваць выбар магчымых адказаў.
+
+Вынікі выбару льга публікаваць пасьля адказу студэнтаў, пасьля пэўнай даты ці ўвогуле не публікаваць. Вынікі льга публікаваць зь імёнамі студэнтаў ці ананімна (пры гэтым настаўнікі заўжды бачаць імёны студэнтаў і водгукі).
+
+Актыўнасьць-выбар льга выкарыстоўваць дзеля
+
+* Хуткага апытаньня па тэме, каб стымуляваць разважаньні
+* Хуткага тэставаньне разуменьня студэнтаў
+* Спрыяньня прыняцьця рашэньня студэнтамі, напрыклад, прапанаваць студэнтам магчымасьць прагаласаваць за далейшы кірунак курса';
+$string['modulename_link'] = 'mod/choice/view';
+$string['modulenameplural'] = 'Выбары';
+$string['moveselectedusersto'] = 'Перасунуць абраных карыстальнікаў ува...';
+$string['multiplenotallowederror'] = 'Множныя адказы забаронены ў гэтым выбары';
+$string['mustchooseone'] = 'Дзеля захаваньня спачатку трэба абраць адказ. Нічога не захавана.';
+$string['noguestchoose'] = 'Прабачце, госьці ня могуць выбіраць.';
+$string['noresultsviewable'] = 'У гэты момант нельга праглядзець вынікі.';
+$string['notanswered'] = 'Пакуль не адказана';
+$string['notenrolledchoose'] = 'Прабачце, толькі запісаным карыстальнікам дазволена выбіраць.';
+$string['notopenyet'] = 'Гэтая актыўнасьць не даступна да {$a}.';
+$string['numberofuser'] = 'Колькасьць водгукаў';
+$string['numberofuserinpercentage'] = 'Адсотак водгукаў';
+$string['openafterclose'] = 'Вы вызначылі дату адкрыцьця пасьля даты закрыцьця';
+$string['option'] = 'Опцыя';
+$string['optionno'] = 'Опцыя {no}';
+$string['options'] = 'Опцыі';
+$string['page-mod-choice-x'] = 'Усякая старонка модуля-выбар';
+$string['pluginadministration'] = 'Адміністраваньне выбару';
+$string['pluginname'] = 'Выбар';
+$string['previewing'] = 'Гэта ўсяго толькі прадпрагляд даступных опцыяў дзеля гэнай актыўнасьці. Вы будзеце здольныя рабіць выбар пасьля адкрыцьця.';
+$string['previewonly'] = 'Гэта ўсяго толькі прадпрагляд даступных опцыяў дзеля гэнай актыўнасьці. Вы ня будзеце здольныя рабіць свой выбар да {$a}.';
+$string['privacy'] = 'Прыватнасьць вынікаў';
+$string['privacy:metadata:choice_answers'] = 'Інфармацыя пра абраныя карыстальнікам адказы для гэнай актыўнасьці-выбару';
+$string['privacy:metadata:choice_answers:choiceid'] = 'ID актыўнасьці-выбару';
+$string['privacy:metadata:choice_answers:optionid'] = 'ID опцыі, якую абраў карыстальнік.';
+$string['privacy:metadata:choice_answers:timemodified'] = 'Часовая метка (timestamp) зьмены выбару карыстальнікам';
+$string['privacy:metadata:choice_answers:userid'] = 'ID карыстальніка, што адказаў на гэтую актыўнасьць-выбар';
+$string['publish'] = 'Публікаваньне вынікаў';
+$string['publishafteranswer'] = 'Паказваць вынікі студэнтам пасьля іхняга адказу';
+$string['publishafterclose'] = 'Паказваць вынікі студэнтам пасьля закрыцьця выбару';
+$string['publishalways'] = 'Заўсёды паказваць вынікі студэнтам';
+$string['publishanonymous'] = 'Публікаваць ананімныя вынікі, не паказваць імёны студэнтаў';
+$string['publishinfoanonafter'] = 'Ананімныя вынікі будуць апублікаваны пасьля вашага адказу.';
+$string['publishinfoanonclose'] = 'Ананімныя вынікі будуць апублікаваны пасьля закрыцьця актыўнасьці.';
+$string['publishinfofullafter'] = 'Поўныя вынікі, якія паказваюць выбары ўсіх удзельнікаў, будуць апублікаваны пасьля вашага адказу.';
+$string['publishinfofullclose'] = 'Поўныя вынікі, якія паказваюць выбары ўсіх удзельнікаў, будуць апублікаваны пасьля акрыцьця актыўнасьці.';
+$string['publishinfonever'] = 'Вынікі гэтай актыўнасьці ня будуць апублікаваны пасьля вашага адказу.';
+$string['publishnames'] = 'Публікаваць поўныя вынікі, якія паказваюць імёны і іх адказы';
+$string['publishnot'] = 'Не публікаваць вынікі для студэнтаў';
+$string['removemychoice'] = 'Выдаліць мой выбар';
+$string['removeresponses'] = 'Выдаліць усе водгукі';
+$string['responses'] = 'Водгукі';
+$string['responsesa'] = 'Водгукі: {$a}';
+$string['responsesresultgraphheader'] = 'Адлюстраваньне графікаў';
+$string['responsesto'] = 'Водгукі на {$a}';
+$string['results'] = 'Вынікі';
+$string['savemychoice'] = 'Захаваць мой выбар';
+$string['search:activity'] = 'Выбар - інфармацыя актыўнасьці';
+$string['selectalloption'] = 'Абраць усе "{$a}"';
+$string['showavailable'] = 'Паказваць не занятыя опцыі';
+$string['showavailable_help'] = 'Паказвае удзельнікам ліміт на абраньне кожнай опцыі і колькасьць занятых опцыяў на гэты момант.';
+$string['showpreview'] = 'Паказваць прадпрагляд';
+$string['showpreview_help'] = 'Дазваляе студэнтам рабіць прадпрагляд даступных опцыяў да адкрыцьця выбару на адпраўку.';
+$string['showunanswered'] = 'Паказваць слупок для неадказанага';
+$string['spaceleft'] = 'опцыя не занята';
+$string['spacesleft'] = 'опцыяў не занята';
+$string['taken'] = 'Занята';
+$string['userchoosethisoption'] = 'Карыстальнікі, што абралі гэтыю опцыю';
+$string['viewallresponses'] = 'Праглядзець {$a} водгукаў';
+$string['viewchoices'] = 'Праглядзець выбары';
+$string['withselected'] = 'З абраным';
+$string['yourselection'] = 'Вашы выбар';
