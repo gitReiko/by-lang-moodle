b4b51f34da6061baf2e5e0c4de5e911ed7e4a8fe
Denis <denis.makouski@bseu.by>
2023-09-28 16:57:49 +0300

    ++

---
+++ перакладзена 4.0/imscp.php
@@ -0,0 +1,61 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'imscp', language 'en', version '4.0'.
+ *
+ * @package     imscp
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['areabackup'] = 'Архіў пакетаў';
+$string['areacontent'] = 'Зьмесьціва';
+$string['contentheader'] = 'Зьмесьціва';
+$string['deploymenterror'] = 'Памылка зьмесьціва пакета!';
+$string['imscp:addinstance'] = 'Дадаць новы пакет зьмесьціва IMS';
+$string['imscp:view'] = 'Праглядзець IMS зьмесьціва';
+$string['indicator:cognitivedepth'] = 'Пазнаваньне IMS пакета';
+$string['indicator:cognitivedepth_help'] = 'Гэты паказьнік заснаваны на глыбіні пазнаваньня, якую дасягнуў студэнт у рэсурсе IMS пакет.';
+$string['indicator:cognitivedepthdef'] = 'Пазнаваньне IMS пакета';
+$string['indicator:cognitivedepthdef_help'] = 'Удзельнік дасягнуў гэтага адсотку пазнавальнага ўцягненьня, прапанаваны рэсурсам IMS пакет у аналізаваны тэрмін (Роўні = Без прагляду, Прагляд)';
+$string['indicator:cognitivedepthdef_link'] = 'Learning_analytics_indicators#Cognitive_depth';
+$string['indicator:socialbreadth'] = 'Сацыяльнасьць IMS пакета';
+$string['indicator:socialbreadth_help'] = 'Гэты паказьнік заснаваны на сацыяльнай шырыні, якую дасягнуў студэнт у рэсурсе IMS пакет.';
+$string['indicator:socialbreadthdef'] = 'Сацыяльнасьць IMS пакета';
+$string['indicator:socialbreadthdef_help'] = 'Удзельнік дасягную гэтага адсотку сацыяльнай шырыні, прапанаванай рэсурсам IMS пакет у аналізаваны тэрмін (Роўні = Без удзельнікаў, Самотны удзел)';
+$string['indicator:socialbreadthdef_link'] = 'Learning_analytics_indicators#Social_breadth';
+$string['keepold'] = 'Архіўныя пакеты';
+$string['keepoldexplain'] = 'Як многа пакетаў мусіць заархіравана?';
+$string['modulename'] = 'Пакет зьмесьціва IMS';
+$string['modulename_help'] = 'Пакет зьмесьціва IMS з\'яўляецца наборам файлаў, упакаваных у адпаведнасьці з сагласаваным стандартам, што дазваляе паўторна выкарыстоўваць яго ў розных сістэмах. Пакет зьмесьціва IMS дазваляе запампоўваць гэткія пакеты ў форме zip-файла і дадаваць іх у курс у якасьці рэсурса.
+ 
+Зьмесьціва звычайна адлюстроўваецца на некалькіх старонках з навігацыяй паміж імі. Існуюць розныя шляхі адлюстраваньня зьмесьціва ўва ўсплыўным акне, з меню навігацыі, кнапкамі і г.д.
+
+Пакет зьмесьціва IMS льга выкарыстоўваць дзеля мультымедыйнага зьмесьціва і анімацыі.';
+$string['modulename_link'] = 'mod/imscp/view';
+$string['modulenameplural'] = 'Пакеты зьмесьціва IMS';
+$string['navigation'] = 'Навігацыя';
+$string['packagefile'] = 'Файл пакета';
+$string['page-mod-imscp-x'] = 'Усякая старонка модуля пакет зьмесьціва IMS';
+$string['pluginadministration'] = 'Адміністраваньне IMSCP';
+$string['pluginname'] = 'Пакет зьмесьціва IMS';
+$string['privacy:metadata'] = 'Плагін пакет зьмесьціва IMS не захоўвае аніякіх асабістых дадзеных.';
+$string['search:activity'] = 'Пакет зьмесьціва IMS - інфармацыя рэсурса';
+$string['toc'] = 'TOC';
