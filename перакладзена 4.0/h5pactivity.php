6c6b9ba2cfcaf21c459b6870d6a9b929299bced3
Denis <denis.makouski@bseu.by>
2023-09-28 15:57:19 +0300

    ++

---
+++ перакладзена 4.0/h5pactivity.php
@@ -0,0 +1,142 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'h5pactivity', language 'en', version '4.0'.
+ *
+ * @package     h5pactivity
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['all_attempts'] = 'Усе спробы карыстальніка';
+$string['answer_checked'] = 'Адказ правераны';
+$string['answer_correct'] = 'Вашы адказ слушны';
+$string['answer_fail'] = 'Хібны адказ';
+$string['answer_incorrect'] = 'Вашы адказ хібны';
+$string['answer_pass'] = 'Слушны адказ';
+$string['answer_text'] = 'Тэкст адказу';
+$string['answer_unchecked'] = 'Адказ не правераны';
+$string['answer_unknown'] = 'Невядомы адказ';
+$string['areapackage'] = 'Файл пакета';
+$string['attempt'] = 'Спроба';
+$string['attempt_answer'] = 'Адказ спробы';
+$string['attempt_average'] = 'Сярэдняя колькасьць спроб';
+$string['attempt_completion_no'] = 'Гэтая спроба не адзначана як выкананая';
+$string['attempt_completion_yes'] = 'Гэтая спроба выканана';
+$string['attempt_first'] = 'Першая спроба';
+$string['attempt_highest'] = 'Найбольшая колькасьць спробаў';
+$string['attempt_last'] = 'Апошняя спроба';
+$string['attempt_none'] = 'Спробы не выкарыстоўваюцца для ацэньваньня';
+$string['attempt_number'] = 'Спроба #{$a}';
+$string['attempt_success_fail'] = 'Хібна';
+$string['attempt_success_pass'] = 'Слушна';
+$string['attempt_success_unknown'] = 'Невядома';
+$string['attempts'] = 'Спробы';
+$string['attempts_none'] = 'Гэты карыстальнік ня мае спрабоў для адлюстраваньня.';
+$string['attempts_report'] = 'Справаздача спробаў';
+$string['choice'] = 'Выбар';
+$string['completion'] = 'Выкананьне';
+$string['contentbank'] = 'Болей інфармацыі пра базу кантэнту';
+$string['contentbank_help'] = 'У базе кантэнту вы можаце ствараць і захоўваць кантэнт з дапамогай некалькіх аўтарскіх інструмантаў, уключаючы інтэграваны H5P-стваральнік.';
+$string['correct_answer'] = 'Слушны адказ';
+$string['deleteallattempts'] = 'Выдаліць усе спробы H5P';
+$string['displaycopyright'] = 'Кнопка аўтарскага права';
+$string['displayembed'] = 'Кнопка ўбудовы';
+$string['displayexport'] = 'Дазволіць спампоўку';
+$string['dnduploadh5pactivity'] = 'Дадаць H5P-актыўнасьць';
+$string['duration'] = 'Працягласьць';
+$string['enabletracking'] = 'Уключыць адсочваньне спробаў';
+$string['false'] = 'Хлусьня';
+$string['grade_average_attempt'] = 'Сярэдняя адзнака';
+$string['grade_first_attempt'] = 'Першая спроба';
+$string['grade_grademethod'] = 'Метад ацэньваньня';
+$string['grade_grademethod_help'] = 'Падчас выкарыстаньня ацэньваньня баламі магчымы наступныя метады разьліку выніковай адзнакі:
+
+* Найвысокая адзнака з усіх спроб
+* Прыблізная сярэдняя адзнака ўсіх спроб
+* Першая спроба (усе іншыя спробы ігнаруюцца)
+* Апошняя спроба (усе іншыя спробы ігнаруюцца)
+* Не выкарыстоўваць спробы для ацэньваньня (адключае разьлік ацэньваньня)';
+$string['grade_h5p'] = 'Адзнака: {$a}';
+$string['grade_highest_attempt'] = 'Найвысокая адзнака';
+$string['grade_last_attempt'] = 'Апошняя спроба';
+$string['grade_manual'] = 'Не разьлічваць адзнаку';
+$string['h5pactivity:addinstance'] = 'Дадаць новы H5P';
+$string['h5pactivity:reviewattempts'] = 'Агляд H5P-спробаў';
+$string['h5pactivity:submit'] = 'Адправіць H5P-спробы';
+$string['h5pactivity:view'] = 'Праглядзець H5P';
+$string['h5pactivityfieldset'] = 'Налады H5P';
+$string['h5pactivityname'] = 'H5P';
+$string['h5pactivitysettings'] = 'Налады';
+$string['h5pattempts'] = 'Опцыі спробы';
+$string['h5pdisplay'] = 'Опцыі H5P';
+$string['maxscore'] = 'Макс бал';
+$string['modulename'] = 'H5P';
+$string['modulename_help'] = 'H5P - скарот ад Пакета HTML5, інтэрактыўны кантэнт, такі як прэзентацыі, відэа і іншыя мультымедыйныя матэрыялы, пытаньні, віктарыны, гульні і іншае. Актыўнасьць H5P дазваляе запампоўваць і дадаваць H5P у курс.
+
+Усе спробы выкананьня адзначаюцца аўтаматычна, а адзнака запісваецца ў журнал адзнак.';
+$string['modulename_link'] = 'mod/h5pactivity/view';
+$string['modulenameplural'] = 'H5P';
+$string['myattempts'] = 'Мае спробы';
+$string['newsubmissions'] = 'H5P адпраўлены';
+$string['no_compatible_track'] = 'Гэтае ўзаемадзеяньне ({$a}) ня мае інфармацыі для адсочваньня
+        ці адсочваньне не сумяшчальнае з цяперашняй версіяй актыўнасьці.';
+$string['noparticipants'] = 'Няма ўдзельнікаў для адлюстраваньня';
+$string['opencontentbank'] = '<a href="{$a}" target="_blank">Доступ да H5P-файла ў базе кантэнту</a> (адкрываецца ў новым вакне).';
+$string['outcome'] = 'Вынік';
+$string['package'] = 'Файл пакета';
+$string['package_help'] = 'Файл пакета - гэта h5p-файл, які ўтрымлівае інтэрактыўны H5P-кантэнт.';
+$string['page-mod-h5pactivity-x'] = 'Усякая старонка H5P-модуля';
+$string['pluginadministration'] = 'Адміністраваньне H5P';
+$string['pluginname'] = 'H5P';
+$string['previewmode'] = 'Гэты кантэнт адлюстроўваецца ў рэжыме прадпрагляда. Адсочваньне спробаў не захоўваецца.';
+$string['privacy:metadata:attempt'] = 'Нумар спробы';
+$string['privacy:metadata:rawscore'] = 'Атрыманы бал';
+$string['privacy:metadata:timecreated'] = 'Час стварэньня элемента, што адсочваецца';
+$string['privacy:metadata:timemodified'] = 'Апошні раз элемент адсочваўся';
+$string['privacy:metadata:userid'] = 'ID карыстальніка, што атрымаў доступ да H5P-актыўнасьці';
+$string['privacy:metadata:xapi_track'] = 'Інфармацыя адсочваньня спробы';
+$string['privacy:metadata:xapi_track_results'] = 'Інфармацыя адсочваньня вынікаў спробы';
+$string['report_viewed'] = 'Справаздача прагледжана';
+$string['result_compound'] = 'Камбінаваны частковы бал';
+$string['result_fill-in'] = 'Запаўняльны тэкс';
+$string['result_fill-in_gap'] = 'Зазор #{$a}';
+$string['result_matching'] = 'Выбар адпаведнасьці';
+$string['result_other'] = 'Невядомы тып узаемадзеяньня';
+$string['result_sequencing_answer'] = 'Значэньне пазіцыі';
+$string['result_sequencing_choice'] = 'Пазіцыі';
+$string['result_sequencing_position'] = '#{$a}';
+$string['review_all_attempts'] = 'Праглядзець усе спробы ({$a} адпраўлена)';
+$string['review_mode'] = 'Агляд спробаў';
+$string['review_my_attempts'] = 'Праглядзець мае спробы';
+$string['review_none'] = 'Удзельнікі ня могуць праглядваць свае ўласныя спробы';
+$string['review_on_completion'] = 'Удзельнікі могуць праглядваць свае ўласныя спробы';
+$string['review_user_attempts'] = 'Прагляд спробаў карыстальніка ({$a})';
+$string['score'] = 'Бал';
+$string['score_out_of'] = '{$a->rawscore} з {$a->maxscore}';
+$string['search:activity'] = 'H5P - інфармацыя актыўнасьці';
+$string['startdate'] = 'Дата пачынаньня';
+$string['statement_received'] = 'статус xAPI атрыманы';
+$string['totalscore'] = 'Выніковы бал';
+$string['tracking_messages'] = 'Некаторыя H5P падаюць дадзеныя адсочваньня спробы дзеля прасунутай справаздачы, напрыклад, нумар спробы, адказы і адзнакі. Зьвярніце ўвагу: Некаторыя H5P ня падаюць дадзеных пра адсочваньне спробы. У такіх выпадках наступныя налады ня будуць мець эфекту.';
+$string['true'] = 'Праўда';
+$string['usecontentbank'] = 'Выкарыстаць <a href="{$a}" target="_blank">базу кантэнту (адкрыецца ў новым вакне)</a> для кіраваньня сваймі H5P-файламі';
+$string['view'] = 'Праглядзець';
+$string['view_report'] = 'Праглядзець справаздачу';
