8776c6e6f52bfda635e3a7fcc170dcbdf9d0a3aa
Denis <denis.makouski@bseu.by>
2024-02-13 17:03:43 +0300

    ++

---
+++ перакладзена 4.0/qbank_columnsortorder.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qbank_columnsortorder', language 'en', version '4.0'.
+ *
+ * @package     qbank_columnsortorder
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Чарга сартаваньня слупкоў';
+$string['privacy:metadata'] = 'Плагін банка пытаньняў Чарга сартаваньня слупкоў не захоўвае аніякіх асабістых дадзеных.';
+$string['qbankcolumnname'] = '({$a})';
+$string['qbankcolumnsdisabled'] = 'Адключаныя на гэты момант плагіны банка пытаньняў:';
+$string['qbankcolumnsortorder'] = 'Чарга сартаваньня слупкоў';
+$string['qbankgotocolumnsort'] = 'Вы можаце зьмяніць чаргу слупкоў банка пытаньняў на старонцы {$a}.';
+$string['qbankgotomanageqbanks'] = 'Вы можаце прыбраць слупок, калі адключыце плагін ува <a href=\'{$a}\'>Кіраваньні плагінамі банка пытаньняў</a>.';
+$string['qbanksortdescription'] = 'Чарга, у якой пералічаны плагіны ніжэй, вызначае чаргу слупкоў у банке пытаньняў.';
