6857628837f44cda5fe8520e0576a5c6cb144e9f
Denis <denis.makouski@bseu.by>
2024-02-21 16:40:56 +0300

    ++

---
+++ праца/qtype_multichoice.php
@@ -0,0 +1,86 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qtype_multichoice', language 'en', version '4.0'.
+ *
+ * @package     qtype_multichoice
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['answerhowmany'] = 'One or multiple answers?';
+$string['answerhowmany_desc'] = 'Whether the default should be one response (i.e. radio buttons) or multiple responses (i.e. checkboxes).';
+$string['answernumbering'] = 'Number the choices?';
+$string['answernumbering123'] = '1., 2., 3., ...';
+$string['answernumberingABCD'] = 'A., B., C., ...';
+$string['answernumberingIIII'] = 'I., II., III., ...';
+$string['answernumbering_desc'] = 'The default numbering style.';
+$string['answernumberingabc'] = 'a., b., c., ...';
+$string['answernumberingiii'] = 'i., ii., iii., ...';
+$string['answernumberingnone'] = 'No numbering';
+$string['answersingleno'] = 'Multiple answers allowed';
+$string['answersingleyes'] = 'One answer only';
+$string['choiceno'] = 'Choice {$a}';
+$string['choices'] = 'Available choices';
+$string['clearchoice'] = 'Clear my choice';
+$string['clozeaid'] = 'Enter missing word';
+$string['correctansweris'] = 'The correct answer is: {$a}';
+$string['correctanswersare'] = 'The correct answers are: {$a}';
+$string['correctfeedback'] = 'For any correct response';
+$string['deletedchoice'] = 'This choice was deleted after the attempt was started.';
+$string['errfractionsaddwrong'] = 'The positive grades you have chosen do not add up to 100%<br />Instead, they add up to {$a}%';
+$string['errfractionsnomax'] = 'One of the choices should be 100%, so that it is<br />possible to get a full grade for this question.';
+$string['errgradesetanswerblank'] = 'Grade set, but the Answer is blank';
+$string['feedback'] = 'Feedback';
+$string['fillouttwochoices'] = 'You must fill out at least two choices. Choices left blank will not be used.';
+$string['fractionsaddwrong'] = 'The positive grades you have chosen do not add up to 100%<br />Instead, they add up to {$a}%<br />Do you want to go back and fix this question?';
+$string['fractionsnomax'] = 'One of the choices should be 100%, so that it is<br />possible to get a full grade for this question.<br />Do you want to go back and fix this question?';
+$string['incorrectfeedback'] = 'For any incorrect response';
+$string['notenoughanswers'] = 'This type of question requires at least {$a} choices';
+$string['overallcorrectfeedback'] = 'Feedback for any correct response';
+$string['overallfeedback'] = 'Overall feedback';
+$string['overallincorrectfeedback'] = 'Feedback for any incorrect response';
+$string['overallpartiallycorrectfeedback'] = 'Feedback for any partially correct response';
+$string['partiallycorrectfeedback'] = 'For any partially correct response';
+$string['pleaseselectananswer'] = 'Please select an answer.';
+$string['pleaseselectatleastoneanswer'] = 'Please select at least one answer.';
+$string['pluginname'] = 'Multiple choice';
+$string['pluginname_help'] = 'In response to a question (that may include an image) the respondent chooses from multiple answers. A multiple choice question may have one or multiple correct answers.';
+$string['pluginname_link'] = 'question/type/multichoice';
+$string['pluginnameadding'] = 'Adding a Multiple choice question';
+$string['pluginnameediting'] = 'Editing a Multiple choice question';
+$string['pluginnamesummary'] = 'Allows the selection of a single or multiple responses from a pre-defined list.';
+$string['privacy:metadata'] = 'Multiple choice question type plugin allows question authors to set default options as user preferences.';
+$string['privacy:preference:answernumbering'] = 'Which numbering style should be used (\'1, 2, 3, ...\', \'a, b, c, ...\' etc.).';
+$string['privacy:preference:defaultmark'] = 'The default mark set for a given question.';
+$string['privacy:preference:penalty'] = 'The penalty for each incorrect try when questions are run using the \'Interactive with multiple tries\' or \'Adaptive mode\' behaviour.';
+$string['privacy:preference:showstandardinstruction'] = 'Whether showing standard instruction.';
+$string['privacy:preference:shuffleanswers'] = 'Whether the answers should be automatically shuffled.';
+$string['privacy:preference:single'] = 'Whether the answer is single with radio buttons or multiple with checkboxes.';
+$string['regradeissuenumchoiceschanged'] = 'The number of choices in the question has changed.';
+$string['selectmulti'] = 'Select one or more:';
+$string['selectone'] = 'Select one:';
+$string['showstandardinstruction'] = 'Show standard instructions';
+$string['showstandardinstruction_help'] = 'Whether to show the instructions "Select one:" or "Select one or more:" before multiple choice answers.';
+$string['shuffleanswers'] = 'Shuffle the choices?';
+$string['shuffleanswers_desc'] = 'Whether options should be randomly shuffled for each attempt by default.';
+$string['shuffleanswers_help'] = 'If enabled, the order of the answers is randomly shuffled for each attempt, provided that "Shuffle within questions" in the activity settings is also enabled.';
+$string['singleanswer'] = 'Choose one answer.';
+$string['toomanyselected'] = 'You have selected too many options.';
