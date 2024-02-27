8fa6371ca87ceab52664a9d9e4507c926aa52c1e
Denis <denis.makouski@bseu.by>
2024-02-27 17:12:50 +0300

    ++

---
+++ перакладзена 4.0/qtype_calculatedmulti.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qtype_calculatedmulti', language 'en', version '4.0'.
+ *
+ * @package     qtype_calculatedmulti
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['answeroptions'] = 'Опцыі выбару';
+$string['answeroptions_help'] = 'Прапанаваная формула выбару ...<strong>{={x}+..}</strong>...';
+$string['pluginname'] = 'Вылічвальны множны выбар';
+$string['pluginname_help'] = 'Вылічвальныя пытаньні множнага выбару падобныя на множны выбар, але ў варыянты выбару ўстаўляюцца лікавыя вынікі формул з падстановаў у фігурных дужках, што замяняюцца на асобныя значэньні падчас выкананьня тэсту. Напрыклад, пытаньне "Назавіце плошчу прастакутніка даўжынёй {l} і шырынёй {w}?" будзе мець слушную формулу адказу "{l}*{w}" (дзе * азначае памнажэньне).';
+$string['pluginname_link'] = 'question/type/calculatedmulti';
+$string['pluginnameadding'] = 'Даданьне вылічвальнага пытаньня множны выбар';
+$string['pluginnameediting'] = 'Рэдагаваньне вылічвальнага пытаньня множны выбар';
+$string['pluginnamesummary'] = 'Вылічвальныя пытаньні множны выбар падобныя на множны выбар, але прапанаваныя варыянту выбару ўтрымліваюць вынікі формулы з лікавых значэньняў, што абіраюцца выпадковым чынам з набору падчас выкананьня тэсту.';
+$string['privacy:metadata'] = 'Плагін тыпу пытаньня Вылічвальны множны выбар не захоўвае аніякіх асабістых дадзеных.';
