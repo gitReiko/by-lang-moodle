b3f5d73e3f81a222589929828d6f6a3cbffb1782
Denis <denis.makouski@bseu.by>
2024-02-28 17:18:27 +0300

    ++

---
+++ перакладзена 4.0/qtype_calculatedsimple.php
@@ -0,0 +1,51 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qtype_calculatedsimple', language 'en', version '4.0'.
+ *
+ * @package     qtype_calculatedsimple
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['atleastonewildcard'] = 'У формулах для слушных адказаў мусіць прысутнічаць прынамсі адна падстанова <strong>{x..}</strong>';
+$string['converttocalculated'] = 'Захаваць як новае звычайнае вылічвальнае пытаньне';
+$string['findwildcards'] = 'Знайсьці падстановы {x..}, што маюцца ў формулах слушных адказаў';
+$string['generatenewitemsset'] = 'Генераваць';
+$string['newsetwildcardvalues'] = 'новы набор значэньняў падстановаў';
+$string['pluginname'] = 'Вылічвальнае простае';
+$string['pluginname_help'] = 'Простыя вылічвальныя пытаньні дазваляюць ствараць асобныя лікавыя пытаньні з выкарыстаньнем падстановаў, што замяняюцца значэньнямі падчас выконваньня тэсту. Простыя вылічвальныя пытаньні прапаноўваюць найбольш карыстаныя функцыі вылічвальнага пытаньня з больш простым шляхам стварэньня.';
+$string['pluginname_link'] = 'question/type/calculatedsimple';
+$string['pluginnameadding'] = 'Даданьне простага вылічвальнага пытаньня';
+$string['pluginnameediting'] = 'Рэдагаваньне простага вылічвальнага пытаньня';
+$string['pluginnamesummary'] = 'Спрошчаная версія вылічвальнага пытаньня. Вылічвальнае пытаньне падобнае на лікавае, але лічбы ў ім абіраюцца выпадковым чынам з набору, падчас выкананьня тэсту.';
+$string['privacy:metadata'] = 'Плагін тыпу пытаньня Вылічвальнае простае не захоўвае аніякіх асабістых дадзеных.';
+$string['setno'] = 'Набор {$a}';
+$string['setwildcardvalues'] = 'наборы значэньняў падстановаў';
+$string['showitems'] = 'Адлюстраваць';
+$string['updatewildcardvalues'] = 'Абнавіць значэньні падстановаў';
+$string['useadvance'] = 'Выкарыстоўвайце кнопку прасунутае, каб убачыць памылкі';
+$string['wildcard'] = 'Падстанова {<strong>{$a}</strong>}';
+$string['wildcardparam'] = 'Параметры падстановаў, што выкарыстоўваюцца для генераваньня значэньняў';
+$string['wildcardrole'] = 'Падстановы <strong>{x..}</strong> будуць заменены згенераванымі лічбавымі значэньнямі';
+$string['wildcardvalues'] = 'Значэньні падстановаў';
+$string['wildcardvaluesgenerated'] = 'Згенераваныя значэньні падстановаў';
+$string['willconverttocalculated'] = 'Калі ўсталяваць <strong>Захаваць як новае пытаньне</strong>, пытаньне будзе захаванае як новае вылічвальнае пытаньне';
+$string['youmustaddatleastonevalue'] = 'Дзеля захаваньня гэтага пытаньня, трэба дадаць прынамсі адзін набор значэньняў падстановаў.';
