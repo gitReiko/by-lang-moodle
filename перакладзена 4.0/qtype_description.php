347f7da605298fa4f1de5d4e0069da3d8dccb027
Denis <denis.makouski@bseu.by>
2024-03-06 16:26:47 +0300

    ++

---
+++ перакладзена 4.0/qtype_description.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qtype_description', language 'en', version '4.0'.
+ *
+ * @package     qtype_description
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['informationtext'] = 'Інфармацыйны тэкст';
+$string['pluginname'] = 'Апісаньне';
+$string['pluginname_help'] = 'Насамрэч апісаньне - гэта не тып пытаньня. Апісаньне дазваляе адлюстроўваць тэкст без патрэбы адказваньня падобна на рэсурс-Цэтлік са старонкі курса.
+
+Тэкст пытаньня адлюстроўваецца як падчас спрабаваньня, гэтак і на старонцы агляду. Усякі агульны водгук адлюстроўваецца толькі на старонцы агляду.';
+$string['pluginnameadding'] = 'Даданьне апісаньня';
+$string['pluginnameediting'] = 'Рэдагаваньне апісаньня';
+$string['pluginnamesummary'] = 'Насамрэч гэта не пытаньне. Гэта шлях даданьня метадычных указаньняў, загалоўкаў і іншага зьмесьціва. Апісаньне падобнае на Цэтлік, які льга дадаць на старонку курса.';
+$string['privacy:metadata'] = 'Плагін тып пытаньня Апісаньне не захоўвае аніякіх асабістых дадзеных.';
