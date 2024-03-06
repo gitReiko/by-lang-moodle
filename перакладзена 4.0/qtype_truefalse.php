56b806ab22a55a9b0bf529b444a4b8c137ae0005
Denis <denis.makouski@bseu.by>
2024-03-06 16:34:02 +0300

    ++

---
+++ перакладзена 4.0/qtype_truefalse.php
@@ -0,0 +1,43 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qtype_truefalse', language 'en', version '4.0'.
+ *
+ * @package     qtype_truefalse
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['correctanswer'] = 'Слушны адказ';
+$string['correctanswerfalse'] = 'Слушны адказ - \'Няпраўда\'.';
+$string['correctanswertrue'] = 'Слушны адказ - \'Праўда\'.';
+$string['false'] = 'Няпраўда';
+$string['feedbackfalse'] = 'Водгук на водкліч \'Няпраўда\'.';
+$string['feedbacktrue'] = 'Водгук на водкліч \'Праўда\'.';
+$string['pleaseselectananswer'] = 'Калі ласка, абярыце адказ.';
+$string['pluginname'] = 'Праўда/няпраўда';
+$string['pluginname_help'] = 'Пытаньне можа мець выявы. Студэнт адказвае на пытаньне праўда альбо няпраўда.';
+$string['pluginname_link'] = 'question/type/truefalse';
+$string['pluginnameadding'] = 'Даданьне пытаньня праўда/няпраўда';
+$string['pluginnameediting'] = 'Рэдагаваньне пытаньня праўда/няпраўда';
+$string['pluginnamesummary'] = 'Простая форма пытаньня множнага выбару з двума варыянтамі \'Праўда\' альбо \'Няпраўда\'.';
+$string['privacy:metadata'] = 'Плагін тып пытаньня Праўда/няпраўда не захоўвае аніякіх асабістых дадзеных.';
+$string['selectone'] = 'Абярыце адзін варыянт:';
+$string['true'] = 'Праўда';
