e52c9ff0d3243d2d00896487ba2a37e3ce38b92d
Denis <denis.makouski@bseu.by>
2024-03-06 16:06:14 +0300

    ++

---
+++ перакладзена 4.0/qtype_ddwtos.php
@@ -0,0 +1,44 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qtype_ddwtos', language 'en', version '4.0'.
+ *
+ * @package     qtype_ddwtos
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addmorechoiceblanks'] = 'Пустэчы дзеля {no} дадатковых выбараў';
+$string['answer'] = 'Адказ';
+$string['blank'] = 'пуста';
+$string['correctansweris'] = 'Слушны адказ: {$a}';
+$string['errorlimitedchoice'] = 'Выбар [[{$a}]] быў выкарыстаны больш за адзін раз, але Неабмежаванае выкарыстаньне не дазволена. Калі ласка, спраўдзіце гэтае пытаньне.';
+$string['infinite'] = 'Неабмежаванае выкарыстаньне';
+$string['pleaseputananswerineachbox'] = 'Калі ласка, пакіньце адказ у кожную вобласьць.';
+$string['pluginname'] = 'Перасоўваньне ў тэкст';
+$string['pluginname_help'] = 'Пытаньні перасоўваньня ў тэкст патрабуюць ад студэнта перасоўваньня словаў ці выразаў у патрэбныя месцы ў тэксьце пытаньня. [[1]], [[2]], [[3]], ... выкарыстоўваюцца ў тэксьце пытаньня ў якасьці заменьнікаў; слушныя адказы вызначаюцца як выбары адказаў 1, 2, 3, ... адпаведна. Выбары адказаў льга сгрупаваць такім чынам, каб усе адказы ў пэўнай групе былі афарбаваны ў аднолькавы колер з заменьнікамі ў тэксьце пытаньня. Выбары адказаў з неабмежаваным выкарыстаньнем льга выкарыстоўваць адразу ў некалькіх заменьніках.';
+$string['pluginname_link'] = 'question/type/ddwtos';
+$string['pluginnameadding'] = 'Даданьне перасоўваньня ў тэкст';
+$string['pluginnameediting'] = 'Рэдагаваньне перасоўваньня ў тэкст';
+$string['pluginnamesummary'] = 'Прапушчаныя словы ў тэксьце пытаньня запаўняюцца шляхам перасоўваньня.';
+$string['privacy:metadata'] = 'Плагін тып пытаньня Перасоўваньне ў тэкст дазваляе аўтарам усталёўваць стандартныя опцыі ў якасьці карыстальніцкіх пераваг.';
+$string['privacy:preference:defaultmark'] = 'Стандартны набор адзнак для паданага пытаньня.';
+$string['privacy:preference:penalty'] = 'Штраф за кожную хібную спробу выкананьня пытаньня ў рэжымах паводзінаў \'Інтэрактыўны з некалькімі спробамі\' і \'Адаптыўны рэжым\'.';
+$string['privacy:preference:shuffleanswers'] = 'Ці трэба аўтаматычна перамешваць адказы.';
