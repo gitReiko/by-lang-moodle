901cc2350dbd193ac773b0b0424a71ca86a0d54a
Denis <denis.makouski@bseu.by>
2024-03-11 17:11:06 +0300

    ++

---
+++ перакладзена 4.0/qtype_essay.php
@@ -0,0 +1,86 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qtype_essay', language 'en', version '4.0'.
+ *
+ * @package     qtype_essay
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['acceptedfiletypes'] = 'Прыймальныя тыпы файлаў';
+$string['acceptedfiletypes_help'] = 'Прыймальныя тыпы файлаў льга абмежаваць сьпісам пашырэньняў файлаў. Калі пакінуць поле пустым, усе тыпы файлаў будуць прыймальнымі.';
+$string['allowattachments'] = 'Дазволіць укладаньні';
+$string['answerfiles'] = 'Файлы адказу';
+$string['answertext'] = 'Тэкст адказу';
+$string['attachedfiles'] = 'Укладаньні: {$a}';
+$string['attachmentsoptional'] = 'Неабавязковыя ўкладаньні';
+$string['attachmentsrequired'] = 'Патрабаваць укладаньні';
+$string['attachmentsrequired_help'] = 'Гэтая опцыя вызначае мінімальную колькасьць неабходных для водклічу ўкладаньняў.';
+$string['err_maxminmismatch'] = 'Максімальны ліміт словаў мусіць быць болей за мінімальны';
+$string['err_maxwordlimit'] = 'Максімальны ліміт словаў уключаны, але не ўсталяваны';
+$string['err_maxwordlimitnegative'] = 'Максімальны ліміт словаў ня можа з\'яўляцца адмоўнай лічбай';
+$string['err_minwordlimit'] = 'Мінімальны ліміт словаў уключаны, але не ўсталяваны';
+$string['err_minwordlimitnegative'] = 'Мінімальны ліміт словаў ня можа з\'яўляцца адмоўнай лічбай';
+$string['formateditor'] = 'HTML-рэдактар';
+$string['formateditorfilepicker'] = 'HTML-рэдатак без выбарніку файлаў';
+$string['formatmonospaced'] = 'Звычайны тэкст, монашырынны шрыфт';
+$string['formatnoinline'] = 'Без анлайн тэксту';
+$string['formatplain'] = 'Звычайны тэкст';
+$string['graderinfo'] = 'Інфармацыя для асобы правяральніка';
+$string['graderinfoheader'] = 'Інфармацыя для асобы правяральніка';
+$string['maxbytes'] = 'Максімальны памер файла';
+$string['maxwordlimit'] = 'Максімальны ліміт словаў';
+$string['maxwordlimit_help'] = 'Калі водкліч патрабуе ад студэнтаў уводу тэкста, гэтая налада вызначае максімальную колькасьць словаў патрэбную дзеля атрыманьня дазволу на адпраўленьне адказу для ўсіх студэнтаў.';
+$string['maxwordlimitboundary'] = 'Ліміт словаў для гэтага пытаньня {$a->limit}, вы спрабуеце адправіць {$a->count} словаў. Калі ласка, скараціце свой водкліч і паспрабуйце наноў.';
+$string['minwordlimit'] = 'Мінімальны ліміт словаў';
+$string['minwordlimit_help'] = 'Калі водкліч патрабуе ад студэнтаў уводу тэкста, гэтая налада вызначае мінімальную колькасьць словаў патрэбную дзеля атрыманьня дазволу на адпраўленьне адказу для ўсіх студэнтаў.';
+$string['minwordlimitboundary'] = 'Гэты водкліч пытаньня патрабуе прынамсі {$a->limit} словаў. Вы спрабуеце адправіць {$a->count} словаў. Калі ласка, пашырце свой водкліч і паспрабуйце наноў.';
+$string['mustattach'] = 'Калі абрана опцыя "Без анлайн тэксту" альбо водклічы неабавязковыя, вы мусіце дазволіць прынамсі адно ўкладаньне.';
+$string['mustrequire'] = 'Калі абрана опцыя "Без анлайн тэксту" альбо водклічы неабавязковыя, вы мусіце патрабаваць прынамсі адно ўкладаньне.';
+$string['mustrequirefewer'] = 'Нельга патрабаваць болей укладаньняй за дазволеную колькасьць.';
+$string['nlines'] = '{$a} радкоў';
+$string['nonexistentfiletypes'] = 'Наступныя тыпы файлаў не зразумелы: {$a}';
+$string['pluginname'] = 'Эсэ';
+$string['pluginname_help'] = 'Для водклічу на пытаньне студэнт можа запампаваць адзін ці некалькі файлаў альбо ўвесьці анлайн тэкст. Студэнтам льга падаваць шаблон адказу. Водклічы трэба ацэньваць уручную.';
+$string['pluginname_link'] = 'question/type/essay';
+$string['pluginnameadding'] = 'Даданьне пытаньня-Эсэ';
+$string['pluginnameediting'] = 'Рэдагаваньне пытаньня-Эсэ';
+$string['pluginnamesummary'] = 'Дазваляе запампоўваць файлы альбо друкаваць тэкст у якасьці водклічу на пытаньне. Трэба ацэньваць уручную.';
+$string['privacy:metadata'] = 'Плагін тып пытаньня Эсэ дазваляе аўтарам усталёўваць стандартныя опцыі ў якасьці карыстальніцкіх пераваг.';
+$string['privacy:preference:attachments'] = 'Колькасьць дазволеных укладаньняў.';
+$string['privacy:preference:attachmentsrequired'] = 'Колькасьць патрэбных укладаньняў.';
+$string['privacy:preference:defaultmark'] = 'Стандартны набор адзнак для паданага пытаньня.';
+$string['privacy:preference:maxbytes'] = 'Максімальны памер файлу.';
+$string['privacy:preference:responsefieldlines'] = 'Колькасьць радкоў, што вызначае памер поля ўводу тэкста.';
+$string['privacy:preference:responseformat'] = 'Фармат водклічу (HTML-рэдактар, звычайны тэкст, г.д.)';
+$string['privacy:preference:responserequired'] = 'Абавязковасьць уводу тэкста для студэнтаў.';
+$string['responsefieldlines'] = 'Памер поля ўводу';
+$string['responseformat'] = 'Фармат водклічу';
+$string['responseisrequired'] = 'Патрабаваць ад студэнта ўводу тэкста';
+$string['responsenotrequired'] = 'Увод тэкста неабавязковы';
+$string['responseoptions'] = 'Опцыі водклічу';
+$string['responserequired'] = 'Патрабаваць тэкст';
+$string['responsetemplate'] = 'Шаблон водклічу';
+$string['responsetemplate_help'] = 'Увесь уведзены тут тэкст будзе адлюстроўвацца ў поле ўводу напачатку новай спробы выкананьня пытаньня.';
+$string['responsetemplateheader'] = 'Шаблон водклічу';
+$string['wordcount'] = 'Колькасьць словаў: {$a}';
+$string['wordcounttoofew'] = 'Колькасьць словаў: {$a->count}, меней за запатрабаваную ({$a->limit} словаў).';
+$string['wordcounttoomuch'] = 'Колькасьць словаў: {$a->count}, болей за ліміт словаў ({$a->limit}).';
