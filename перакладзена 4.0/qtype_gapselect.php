b02a50410e3ec2722a208653d9c6c381b8c9c733
Denis <denis.makouski@bseu.by>
2024-03-12 16:08:24 +0300

    ++

---
+++ перакладзена 4.0/qtype_gapselect.php
@@ -0,0 +1,56 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qtype_gapselect', language 'en', version '4.0'.
+ *
+ * @package     qtype_gapselect
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addmorechoiceblanks'] = 'Пустэчы дзеля {no} дадатковых выбараў';
+$string['answer'] = 'Адказ';
+$string['blanknumber'] = 'Пустэча {$a}';
+$string['choices'] = 'Выбары';
+$string['choicex'] = 'Выбар [[{no}]]';
+$string['combinedcontrolnamegapselect'] = 'выпадное меню';
+$string['combinedcontrolnamegapselectplural'] = 'выпадныя меню';
+$string['correctansweris'] = 'Слушны адказ: {$a}';
+$string['errorblankchoice'] = 'Калі ласка, праверце выбары: Выбар {$a} пусты.';
+$string['errormissingchoice'] = 'Калі ласка, праверце тэкст пытаньня: {$a} ня знойдзены ў выбарах! У якасьці запаўняльнікаў могуць выкарыстоўвацца толькі лічбы з вызначанымі адказамі-выбарамі.';
+$string['errornoslots'] = 'У тэксьце пытаньня мусяць прысутнічаць запаўняльнікі кшалту [[1]] дзеля адлюстроўваньня прапушчаных словаў.';
+$string['errorquestiontextblank'] = 'Вы мусіце ўвесьці тэкст пытаньня.';
+$string['group'] = 'Група';
+$string['pleaseputananswerineachbox'] = 'Калі ласка, зрабіце адказ у кожным полі.';
+$string['pluginname'] = 'Выбар прапушчаных словаў';
+$string['pluginname_help'] = 'Пытаньні Выбар прапушчаных словаў патрабуюць ад студэнта абраньня слушных адказаў у выпадных меню. [[1]], [[2]], [[3]], ... выкарыстоўваюцца ў якасьці запаўняльнікаў у тэксьце пытаньня, а слушныя адказы вызначаюцца як выбары-адказы 1, 2, 3, ... адпаведна. Дзеля ўскладненьня пытаньня льга дадаць дадатковыя выбары-адказы. Выбары-адказы льга групаваць дзеля абмежаваньня колькасьці адказаў, даступных у выпадным меню.';
+$string['pluginname_link'] = 'question/type/gapselect';
+$string['pluginnameadding'] = 'Даданьне пытаньня выбар прапушчаных словаў';
+$string['pluginnameediting'] = 'Рэдагаваньне пытаньня выбар прапушчаных словаў';
+$string['pluginnamesummary'] = 'Прапушчаныя словы ў тэксьце пытаньня запаўняюцца з дапамогай выпаднога меню.';
+$string['privacy:metadata'] = 'Плагін тып пытаньня Выбар прапушчаных словаў дазваляе аўтарам усталёўваць стандартныя опцыі ў якасьці карыстальніцкіх пераваг.';
+$string['privacy:preference:defaultmark'] = 'Стандартная адзнака, вызначаная для паданага пытаньня.';
+$string['privacy:preference:penalty'] = 'Штраф за кожную хібную спробу выкананьня пытаньня ў рэжымах паводзінаў \'Інтэрактыўны з некалькімі спробамі\' і \'Адаптыўны рэжым\'.';
+$string['privacy:preference:shuffleanswers'] = 'Ці трэба аўтаматычна перамешваць адказы.';
+$string['regradeissuenumchoiceschanged'] = 'Колькасьць выбараў у групе {$a} зьменена.';
+$string['regradeissuenumgroupsschanged'] = 'Колькасьць групаў выбараў зьменена.';
+$string['shuffle'] = 'Перамяшаць';
+$string['tagsnotallowed'] = '{$a->tag} забаронены. (Дазволены толькі {$a->allowed}.)';
+$string['tagsnotallowedatall'] = '{$a->tag} забаронены. (HTML тут забаронены.)';
