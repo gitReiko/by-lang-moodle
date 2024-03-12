fc7ba7e3a3c1c5900da3e0ebed0c211f2a0dff31
Denis <denis.makouski@bseu.by>
2024-03-12 16:57:50 +0300

    ++

---
+++ перакладзена 4.0/qtype_missingtype.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qtype_missingtype', language 'en', version '4.0'.
+ *
+ * @package     qtype_missingtype
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['answerno'] = 'Адказ {$a}';
+$string['cannotchangeamissingqtype'] = 'Вы ня можаце зьмяняць пытаньні з адсутным тыпам.';
+$string['deletedquestion'] = 'Адсутнае пытаньне';
+$string['deletedquestiontext'] = 'Гэтае пытаньне адсутнічае, таму яго нельга адлюстраваць.';
+$string['missing'] = 'Тып пытаньня, які не ўсталяваны на гэтай сістэме';
+$string['missingqtypewarning'] = 'Гэтае пытаньне мае тып, які зараз не ўсталяваны на гэтай сістэме. Вы ня можаце нічога рабіць з гэтым пытаньнем.';
+$string['pluginname'] = 'Адсутным тып';
+$string['pluginnameadding'] = 'Даданьне тыпу пытаньня, што не ўсталяваны на гэтай сістэме';
+$string['pluginnameediting'] = 'Рэдагаваньне тыпу пытаньня, што не ўсталяваны на гэтай сістэме';
+$string['privacy:metadata'] = 'Плагін тып пытаньня Адсутным тып не захоўвае аніякіх асабістых дадзеных.';
+$string['warningmissingtype'] = '<b>Гэтае пытаньне мае тып, які дагэтуль не ўсталяваны ў вашым Moodle.<br /> Калі ласка, папярэдзьце вашага адміністратара Moodle.</b>';
---
+++ перакладзена 4.0/qtype_shortanswer.php
@@ -0,0 +1,49 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qtype_shortanswer', language 'en', version '4.0'.
+ *
+ * @package     qtype_shortanswer
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addmoreanswerblanks'] = 'Пустэчы дзеля {no} дадатковых адказаў';
+$string['answer'] = 'Адказ: {$a}';
+$string['answermustbegiven'] = 'Вы мусіце ўвесьці адказ, калі маецца адзнака альбо водгук.';
+$string['answerno'] = 'Адказ {$a}';
+$string['caseno'] = 'Не, не ўлічваць памер літар';
+$string['casesensitive'] = 'Чульлівасьць да рэгістра';
+$string['caseyes'] = 'Так, улічваць памер літар';
+$string['correctansweris'] = 'Слушны адказ: {$a}';
+$string['correctanswers'] = 'Слушныя адказы';
+$string['filloutoneanswer'] = 'Вы мусіце падаць прынамсі адзін магчымы адказ. Адказы пакінутыя пустымі ня будуць выкарыстоўвацца. \'*\' можа выкарыстоўвацца ў якасьці падстановы дзеля ўсякіх знакаў. Першы адпаведны адказ будзе выкарыстаны дзеля вызначэньня адзнакі і водгука.';
+$string['notenoughanswers'] = 'Гэты тып пытаньня патрабуе прынамсі {$a} адказаў';
+$string['pleaseenterananswer'] = 'Калі ласка, увядзіце адказ.';
+$string['pluginname'] = 'Кароткі адказ';
+$string['pluginname_help'] = 'У якасьці адказу (водклічу) на пытаньне, што можа мець выяву, студэнт мусіць увесьці слова альбо кароткую фразу. Магчыма адначасовае існаваньне слушных адказаў, кожны зь якіх будзе мець уласную адзнаку. Налада "Чульлівасьць да рэгістра" дазваляе мець розныя адзнакі для адказаў "Чырвонае" і "чырвонае".';
+$string['pluginname_link'] = 'question/type/shortanswer';
+$string['pluginnameadding'] = 'Даданьне пытаньня кароткі адказ';
+$string['pluginnameediting'] = 'Рэдагаваньне пытаньня кароткі адказ';
+$string['pluginnamesummary'] = 'Адказ на пытаньне мусіць складацца з аднога ці некалькіх словаў. Пытаньне ацэньваецца параўнаньнем з прадугледжанымі адказамі, якія могуць мець падстановы.';
+$string['privacy:metadata'] = 'Плагін тып пытаньня Кароткі адказ дазваляе аўтарам усталёўваць стандартныя опцыі ў якасьці карыстальніцкіх пераваг.';
+$string['privacy:preference:defaultmark'] = 'Стандартная адзнака, вызначаная для паданага пытаньня.';
+$string['privacy:preference:penalty'] = 'Штраф за кожную хібную спробу выкананьня пытаньня ў рэжымах паводзінаў \'Інтэрактыўны з некалькімі спробамі\' і \'Адаптыўны рэжым\'.';
+$string['privacy:preference:usecase'] = 'Патрэба ўлічваньня памеру літар.';
