e33a6f34f636b5a582320996c6734212bfdd3345
Denis <denis.makouski@bseu.by>
2024-03-13 17:09:24 +0300

    ++

---
+++ перакладзена 4.0/qtype_random.php
@@ -0,0 +1,50 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qtype_random', language 'en', version '4.0'.
+ *
+ * @package     qtype_random
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['configselectmanualquestions'] = 'Ці можа выпадковае пытаньне абраць пытаньне, што ацэньваецца ўручную, падчас абраньня пытаньня з катэгорыі?';
+$string['includingsubcategories'] = 'Уключаць падкатэгорыі';
+$string['pluginname'] = 'Выпадковае пытаньне';
+$string['pluginname_help'] = 'Выпадковае пытаньне - гэта не пытаньне, а шлях даданьня выпадковага пытаньня з пэўнай катэгорыі.';
+$string['pluginnameediting'] = 'Рэдагаваньне выпадковага пытаньня';
+$string['privacy:metadata'] = 'Плагін Выпадковае пытаньне не захоўвае аніякіх асабістых дадзеных.';
+$string['randomqname'] = 'Выпадковае ({$a})';
+$string['randomqnamefromtop'] = 'Хібнае выпадковае пытаньне! Калі ласка, выдаліце гэтае пытаньне.';
+$string['randomqnamefromtoptags'] = 'Хібнае выпадковае пытаньне! Калі ласка, выдаліце гэтае пытаньне.';
+$string['randomqnametags'] = 'Выпадковае ({$a->category}, тэгі: {$a->tags})';
+$string['randomqplusname'] = 'Выпадковае ({$a} і падкатэгорыі)';
+$string['randomqplusnamecourse'] = 'Выпадковае (Усякая катэгорыя ў гэтым курсе)';
+$string['randomqplusnamecoursecat'] = 'Выпадковае (Усякая катэгорыя ўнутры катэгорыі курса {$a})';
+$string['randomqplusnamecoursecattags'] = 'Выпадковае (Усякая катэгорыя ўнутры катэгорыі курса {$a->category}, тэгі: {$a->tags})';
+$string['randomqplusnamecoursetags'] = 'Выпадковае (Усякая катэгорыя ў гэтым курсе, тэгі: {$a->tags})';
+$string['randomqplusnamemodule'] = 'Выпадковае (Усякая катэгорыя гэтага тэста)';
+$string['randomqplusnamemoduletags'] = 'Выпадковае (Усякая катэгорыя гэтага тэста, тэгі: {$a->tags})';
+$string['randomqplusnamesystem'] = 'Выпадковае (Усякая катэгорыя роўня сайта)';
+$string['randomqplusnamesystemtags'] = 'Выпадковае (Усякая катэгорыя роўня сайта, тэгі: {$a->tags})';
+$string['randomqplusnametags'] = 'Выпадковае ({$a->category} і падкатэгорыі, тэгі: {$a->tags})';
+$string['selectedby'] = '{$a->questionname} абрана {$a->randomname}';
+$string['selectmanualquestions'] = 'Выпадковыя пытаньні могуць выкарыстоўваць пытаньні, што ацэньваюцца ўручную';
+$string['taskunusedrandomscleanup'] = 'Выдаліць некарыстаныя выпадковыя пытаньні';
---
+++ перакладзена 4.0/qtype_randomsamatch.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qtype_randomsamatch', language 'en', version '4.0'.
+ *
+ * @package     qtype_randomsamatch
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['insufficientoptions'] = 'Дзеля гэтага пытаньня бракуе варыянтаў выбару, таму ён не даступны ў гэтым тэсьце. Калі ласка, праінфармуйце вашага настаўніка.';
+$string['nosaincategory'] = 'Катэгорыя, якую вы абралі \'{$a->catname}\', ня мае пытаньняў кароткі адказ. Абярыце іншую катэгорыю альбо стварыце некалькі пытаньняў у гэтай катэгорыі.';
+$string['notenoughsaincategory'] = 'Катэгорыя, якую вы абралі \'{$a->catname}\', мае толькі {$a->nosaquestions} пытаньняў кароткіх адказаў. Абярыце іншую катэгорыю, альбо стварыце некалькі пытаньняў у гэтай катэгорыі, альбо зьменшыце колькасьць абраных вамі пытаньняў.';
+$string['pluginname'] = 'Выпадковая адпаведнасьць з кароткіх адказаў';
+$string['pluginname_help'] = 'Студэнт бачыць гэтае пытаньне як адпаведнасьць. Розьніца палягае ў тым, што сьпіс тэрмінаў і сьцьвярджэньняў (пытаньняў) дзеля адпаведнасьці абіраецца выпадковым чынам з пытаньняў кароткіх адказаў у цяперашней катэгорыі. Катэгорыя мусіць мець дастатковую колькасьць нявыкарыстаных пытаньняў кароткіх адказаў, альбо будзе адлюстравана паведамленьне пра памылку.';
+$string['pluginname_link'] = 'question/type/randomsamatch';
+$string['pluginnameadding'] = 'Даданьне пытаньня выпадковай адпаведнасьці з кароткіх адказаў';
+$string['pluginnameediting'] = 'Рэдагаваньне пытаньня выпадковай адпаведнасьці з кароткіх адказаў';
+$string['pluginnamesummary'] = 'Падобнае на пытаньне-адпаведнасьць, але ствараецца выпадковым чынам з пытаньняў кароткіх адказаў у пэўнай катэгорыі.';
+$string['privacy:metadata'] = 'Плагін Выпадковая адпаведнасьць з кароткіх адказаў не захоўвае аніякіх асабістых дадзеных.';
+$string['randomsamatch'] = 'Выпадковая адпаведнасьць з кароткіх адказаў';
+$string['randomsamatchintro'] = 'Дзеля кожнага наступнага пытаньня абярыце прыдатны адказ з меню.';
+$string['randomsamatchnumber'] = 'Колькасьць пытаньняў дзеля выбару';
+$string['subcats'] = 'Уключаць падкатэгорыі';
+$string['subcats_help'] = 'Калі ўключана, пытаньні таксама будуць абірацца з падкатэгорыяў.';
