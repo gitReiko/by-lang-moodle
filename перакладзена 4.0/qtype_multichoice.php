c7bc2b98fedc6efa7a1942fee5f68e91355ba2a0
Denis <denis.makouski@bseu.by>
2024-03-13 10:59:07 +0300

    ++

---
+++ перакладзена 4.0/qtype_multichoice.php
@@ -0,0 +1,86 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qtype_multichoice', language 'en', version '4.0'.
+ *
+ * @package     qtype_multichoice
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['answerhowmany'] = 'Адзін ці некалькі адказаў?';
+$string['answerhowmany_desc'] = 'Вызначае, ці будзе адзін адказ (радыёкнопка) альбо некалькі адказаў (сьцяжкі).';
+$string['answernumbering'] = 'Нумараваньне выбараў?';
+$string['answernumbering123'] = '1., 2., 3., ...';
+$string['answernumberingABCD'] = 'A., B., C., ...';
+$string['answernumberingIIII'] = 'I., II., III., ...';
+$string['answernumbering_desc'] = 'Стандартны стыль нумараваньня.';
+$string['answernumberingabc'] = 'a., b., c., ...';
+$string['answernumberingiii'] = 'i., ii., iii., ...';
+$string['answernumberingnone'] = 'Без нумараваньня';
+$string['answersingleno'] = 'Дазволена некалькі адказаў';
+$string['answersingleyes'] = 'Толькі адзін адказ';
+$string['choiceno'] = 'Выбар {$a}';
+$string['choices'] = 'Даступныя выбары';
+$string['clearchoice'] = 'Ачысьціць мае выбары';
+$string['clozeaid'] = 'Увядзіце прапушчанае слова';
+$string['correctansweris'] = 'Слушны адказ: {$a}';
+$string['correctanswersare'] = 'Слушныя адказы: {$a}';
+$string['correctfeedback'] = 'За ўсякі слушны водкліч';
+$string['deletedchoice'] = 'Гэты выбар быў выдалены пасьля пачынаньня спробы.';
+$string['errfractionsaddwrong'] = 'Абраныя вамі станоўчыя адзнакі не складаюцца ў 100%<br />Заміж гэтага яны складаюцца ў {$a}%';
+$string['errfractionsnomax'] = 'Адзін з выбараў мусіць быць 100% дзеля магчымасьці атрыманьня поўнай адзнакі за гэтае пытаньне.';
+$string['errgradesetanswerblank'] = 'Адзнака ўсталявана, але Адказ пусты';
+$string['feedback'] = 'Водгук';
+$string['fillouttwochoices'] = 'Вы мусіце запоўніць прынамсі два выбары. Выбары пакінутыя пустымі ня будуць выкарыстоўвацца.';
+$string['fractionsaddwrong'] = 'Абраныя вамі станоўчыя адзнакі не складаюцца ў 100%<br />Заміж гэтага яны складаюцца ў {$a}%<br />Жадаеце вярнуцца назад і выправіць гэтае пытаньне?';
+$string['fractionsnomax'] = 'Адзін з выбараў мусіць быць 100% дзеля магчымасьці атрыманьня поўнай адзнакі за гэтае пытаньне.<br />Жадаеце вярнуцца назад і выправіць гэтае пытаньне?';
+$string['incorrectfeedback'] = 'За ўсякі хібны водкліч';
+$string['notenoughanswers'] = 'Гэты тып пытаньня патрабуе прынамсі {$a} выбары';
+$string['overallcorrectfeedback'] = 'Водгук дзеля ўсякага слушнага водкліча';
+$string['overallfeedback'] = 'Выніковы водгук';
+$string['overallincorrectfeedback'] = 'Водгук дзеля ўсякага хібнага водкліча';
+$string['overallpartiallycorrectfeedback'] = 'Водгук дзеля ўсякага часткова слушнага водкліча';
+$string['partiallycorrectfeedback'] = 'За ўсякі часткова слушны водкліч';
+$string['pleaseselectananswer'] = 'Калі ласка, абярыце адказ.';
+$string['pleaseselectatleastoneanswer'] = 'Калі ласка, абярыце прынамсі адзін адказ.';
+$string['pluginname'] = 'Множны выбар';
+$string['pluginname_help'] = 'Дзеля адказу на пытаньне, якое можа мець выявы, студэнт мусіць абраць адказы з прапанаваных. Пытаньні з множным выбарам дазваляюць мець адзін ці некалькі слушных адказаў.';
+$string['pluginname_link'] = 'question/type/multichoice';
+$string['pluginnameadding'] = 'Даданьне пытаньня множны выбар';
+$string['pluginnameediting'] = 'Рэдагаваньне пытаньня множны выбар';
+$string['pluginnamesummary'] = 'Дазваляе абіраць адзін ці некалькі адказаў з прапанаванага сьпісу.';
+$string['privacy:metadata'] = 'Плагін тып пытаньня Множны выбар дазваляе аўтарам усталёўваць стандартныя опцыі ў якасьці карыстальніцкіх пераваг.';
+$string['privacy:preference:answernumbering'] = 'Выбраны стыль нумараваньня (\'1, 2, 3, ...\', \'a, b, c, ...\' г.д.).';
+$string['privacy:preference:defaultmark'] = 'Стандартная адзнака, вызначаная для паданага пытаньня.';
+$string['privacy:preference:penalty'] = 'Штраф за кожную хібную спробу выкананьня пытаньня ў рэжымах паводзінаў \'Інтэрактыўны з некалькімі спробамі\' і \'Адаптыўны рэжым\'.';
+$string['privacy:preference:showstandardinstruction'] = 'Патрэба паказваньня стандартных інструкцыяў.';
+$string['privacy:preference:shuffleanswers'] = 'Ці трэба аўтаматычна перамешваць адказы.';
+$string['privacy:preference:single'] = 'Адзін (радыёкнопка) ці некалькі адказаў (сьцяжкі).';
+$string['regradeissuenumchoiceschanged'] = 'Колькасьць выбараў у пытаньні зьмянілася.';
+$string['selectmulti'] = 'Абярыце адзін ці некалькі:';
+$string['selectone'] = 'Абярыце адзін:';
+$string['showstandardinstruction'] = 'Паказваць стандартныя інструкцыі';
+$string['showstandardinstruction_help'] = 'Вызначае неабходнасьць паказваньня інструкцыяў "Абярыце адзін:" альбо "Абярыце адзін ці некалькі:" перад адказамі з множным выбарам.';
+$string['shuffleanswers'] = 'Перамешваць выбары?';
+$string['shuffleanswers_desc'] = 'Патрэба перамешваньня выбараў выпадковым чынам перад кожным спрабаваньнем выкананьня пытаньня.';
+$string['shuffleanswers_help'] = 'Патрэба перамешваньня выбараў выпадковым чынам перад кожным спрабаваньнем выкананьня пытаньня. Дзеля працы налада "Перамешваць пытаньні" у наладах актыўнасьці мусіць быць уключанай.';
+$string['singleanswer'] = 'Абярыце адзін адказ.';
+$string['toomanyselected'] = 'Вы абралі зашмат выбараў.';
