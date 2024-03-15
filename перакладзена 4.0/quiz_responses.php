f79bf06f4b8108a60b598042f30a02f3f9705145
Denis <denis.makouski@bseu.by>
2024-03-15 16:24:56 +0300

    ++

---
+++ перакладзена 4.0/quiz_responses.php
@@ -0,0 +1,56 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'quiz_responses', language 'en', version '4.0'.
+ *
+ * @package     quiz_responses
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['cannotloadoptions'] = 'Нельга загрузіць опцыі пытаньня';
+$string['include'] = 'Уключыць';
+$string['mustselectcols'] = 'Вы мусіце ўключыць штосьці.';
+$string['pagesize'] = 'Памер старонкі';
+$string['pluginname'] = 'Водклічы';
+$string['privacy:preference:qtext'] = 'Патрэба паказваньня слупкоў з тэкстам пытаньня.';
+$string['privacy:preference:resp'] = 'Патрэба паказваньня слупкоў з водклічамі студэнтаў.';
+$string['privacy:preference:right'] = 'Патрэба паказваньня слупкоў са слушнымі водклічамі.';
+$string['privacy:preference:which_tries'] = 'Які спрабуе паказваць водклічы ад.';
+$string['privacy:preference:which_tries:all'] = 'Усе спробы выканаць пытаньне карыстальнікам.';
+$string['privacy:preference:which_tries:first'] = 'Першая спроба выканаць пытаньне карыстальнікам.';
+$string['privacy:preference:which_tries:last'] = 'Апошняя спроба выканаць пытаньне карыстальнікам.';
+$string['questiontext'] = 'тэкст пытаньня';
+$string['reportresponses'] = 'Водклічы';
+$string['response'] = 'водкліч';
+$string['responses'] = 'Водклічы';
+$string['responsesdownload'] = 'Спампаваць водклічы';
+$string['responsesfilename'] = 'водклічы';
+$string['responsesoptions'] = 'Опцыі водклічаў';
+$string['responsesreport'] = 'Справаздача па водклічам';
+$string['responsestitle'] = 'Водклічы';
+$string['responsex'] = 'Водкліч {$a}';
+$string['rightanswer'] = 'слушны адказ';
+$string['rightanswerx'] = 'Слушны адказ {$a}';
+$string['showattempts'] = 'Паказаць / спампаваць толькі спробы';
+$string['showthe'] = 'Паказаць';
+$string['summaryofquestiontext'] = 'Коратка пра пытаньне';
+$string['summaryofresponse'] = 'Коратка пра паданы водкліч';
+$string['summaryofrightanswer'] = 'Коратка пра слушны адказ';
