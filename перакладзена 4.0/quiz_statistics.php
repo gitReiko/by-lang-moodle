9279d11c9c0edf7ad9828301f6e0f699c1bbd702
Denis <denis.makouski@bseu.by>
2024-03-18 15:34:34 +0300

    ++

---
+++ перакладзена 4.0/quiz_statistics.php
@@ -0,0 +1,124 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'quiz_statistics', language 'en', version '4.0'.
+ *
+ * @package     quiz_statistics
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['actualresponse'] = 'Актуальны водкліч';
+$string['allattempts'] = 'усе спробы';
+$string['allattemptsavg'] = 'Сярэдняя адзнака ўсіх спробаў';
+$string['allattemptscount'] = 'Колькасьць выкананых ацэненых спробаў';
+$string['analysisnameonly'] = '"{$a->name}"';
+$string['analysisno'] = '({$a->number}) "{$a->name}"';
+$string['analysisnovariant'] = '({$a->number}) "{$a->name}" варыянт {$a->variant}';
+$string['analysisofresponses'] = 'Аналіз водклічаў';
+$string['analysisofresponsesfor'] = 'Аналіз водклічаў для {$a}';
+$string['analysisvariant'] = '"{$a->name}" варыянт {$a->variant}';
+$string['attempts'] = 'Спробы';
+$string['attemptsall'] = 'усе спробы';
+$string['attemptsfirst'] = 'першая спроба';
+$string['backtoquizreport'] = 'Назад на галоўную старонку статыстычнай справаздачы.';
+$string['calculatefrom'] = 'Разьлічыць статыстыку ад';
+$string['calculatingallstats'] = 'Разьлічваньне статыстыкі для тэста, пытаньняў і аналіз дадзеных водклічаў';
+$string['cic'] = 'Каэфіцыент унутранай узгодненасьці (для {$a})';
+$string['completestatsfilename'] = 'усястатыстыка';
+$string['count'] = 'Разьлічыць';
+$string['counttryno'] = 'Спроба Разьліка {$a}';
+$string['coursename'] = 'Назва курса';
+$string['detailedanalysis'] = 'Больш дэталёвы аналіз водклічаў на гэтае пытаньне';
+$string['detailedanalysisforvariant'] = 'Больш дэталёвы аналіз водклічаў на варыянт {$a} гэтага пытаньня';
+$string['discrimination_index'] = 'Індэкс дыскрымінацыі';
+$string['discriminative_efficiency'] = 'Дыскрымінацыйная эфектыўнасьць';
+$string['downloadeverything'] = 'Спампаваць поўную справаздачу як';
+$string['duration'] = 'Адкрыць для';
+$string['effective_weight'] = 'Эфектыўная вага';
+$string['errordeleting'] = 'Памылка выдаленьня {$a} старых запісаў.';
+$string['errormedian'] = 'Памылка атрыманьня медыяны';
+$string['errorpowerquestions'] = 'Памылка атрыманьня дадзеных дзеля разьліку дысьперсіі адзнак пытаньня';
+$string['errorpowers'] = 'Памылка атрыманьня дадзеных дзеля разьліку дысьперсіі адзнак тэста';
+$string['errorrandom'] = 'Памылка атрыманьня дадзеных падпрадмета';
+$string['errorratio'] = 'Каэфіцыент памылак (для {$a})';
+$string['errorstatisticsquestions'] = 'Памылка атрыманьня дадзеных дзеля разьліку статыстыкі для адзнак пытаньня';
+$string['facility'] = 'Індэкс аб\'екта';
+$string['firstattempts'] = 'першыя спробы';
+$string['firstattemptsavg'] = 'Сярэдні бал першых спробаў';
+$string['firstattemptscount'] = 'Колькасьць выкананых ацэненых першых спробаў';
+$string['frequency'] = 'Частата';
+$string['highestattempts'] = 'спроба з найвысокай адзнакай';
+$string['highestattemptsavg'] = 'Сярэдняя адзнака спробаў з найвысокай адзнакай';
+$string['intended_weight'] = 'Разьлічаная вага';
+$string['kurtosis'] = 'Куртозіс разьмеркаваньня балаў (для {$a})';
+$string['lastattempts'] = 'апошняя спроба';
+$string['lastattemptsavg'] = 'Сярэдняя адзнака за апошнія спробы';
+$string['lastcalculated'] = 'Апошні разьлік выконваўся {$a->lastcalculated} таму, з тых часоў было зроблена {$a->count} спробаў.';
+$string['maximumfacility'] = 'Максімальны аб\'ект';
+$string['median'] = 'Медыянная адзнака (для {$a})';
+$string['medianfacility'] = 'Медыянны аб\'ект';
+$string['minimumfacility'] = 'Мінімальны аб\'ект';
+$string['modelresponse'] = 'Мадэль водклічаў';
+$string['nameforvariant'] = 'Варыянт {$a->variant} з {$a->name}';
+$string['negcovar'] = 'Адмоўная каварыяцыя адзнакі з агульнай адзнакай за спробу';
+$string['negcovar_help'] = 'Адзнака гэтага пытаньня за гэты набор спробаў мяняецца процілегла выніковай адзнацы за спробу. То бок, выніковая адзнака за спробу, зазвычай, ніжэйшая за сярэднюю, калі адзнака за гэтае пытаньне вышэйшая за сярэднюю, і наадварот.
+
+Нашае раўнаньне для эфектыўнай вагі пытаньня ня можа быць разьлічана ў гэтым выпадку. Разьлікі эфектыўнай вагі пытаньня для іншых пытаньняў у гэтым тэсьце уяўляюць сабою эфектыўную вагу пытаньняў для гэтых пытаньняў, калі вылучаныя пытаньні з адмоўнай каварыяцыяй атрымліваюць максімальную адзнаку нуль.
+
+Калі вы адрэдагуеце тэст і паставіце гэтым пытаньням з адмоўнай каварыяцыяй максімальную адзнаку 0, то эфектыўная вага гэтых пытаньняў будзе роўная нулю, а рэальная эфектыўная вага іншых пытаньняў будзе такой, якой яна разьлічана зараз.';
+$string['nogradedattempts'] = 'Не было зроблена ніводнай спробы выкананьня гэтага тэсту, альбо ўсе спробы маюць пытаньні, што патрабуюць ручное ацэньваньне.';
+$string['nostudentsingroup'] = 'Гэтая група дагэтуль ня мае студэнтаў';
+$string['optiongrade'] = 'Частковы крэдыт';
+$string['partofquestion'] = 'Частка пытаньня';
+$string['pluginname'] = 'Статыстыка';
+$string['position'] = 'Пазіцыя';
+$string['positions'] = 'Пазіцыі';
+$string['privacy:metadata'] = 'Хаця плагін Статыстыка тэста мае табліцы базы дадзеных, дадзеных агрэгаваныя і не апісваюць унікальнага індывіда.';
+$string['questioninformation'] = 'Інфармацыя пытаньня';
+$string['questionname'] = 'Назва пытаньня';
+$string['questionnumber'] = 'П#';
+$string['questionstatistics'] = 'Статыстыка пытаньня';
+$string['questionstatsfilename'] = 'статыстыкапытаньня';
+$string['questiontype'] = 'Тып пытаньня';
+$string['quizinformation'] = 'Інфармацыя тэста';
+$string['quizname'] = 'Назва тэста';
+$string['quizoverallstatistics'] = 'Агульная статыстыка тэста';
+$string['quizstatisticscleanuptask'] = 'Ачыстка старых запісаў кэша статыстыкі тэста';
+$string['quizstructureanalysis'] = 'Аналіз структуры тэста';
+$string['random_guess_score'] = 'Адзнака выпадковага ўгадваньня';
+$string['rangebetween'] = '{$a->min} − {$a->max}';
+$string['rangeofvalues'] = 'Дыяпазон статыстычных дадзеных для гэтых пытаньняў';
+$string['recalculatenow'] = 'Пераразьлічыць зараз';
+$string['reportsettings'] = 'Налады разьліку статыстыкі';
+$string['response'] = 'Водкліч';
+$string['skewness'] = 'Асіметрыя разьмеркаваньня балаў (для {$a})';
+$string['slotstructureanalysis'] = 'Структурны аналіз для пытаньня нумар {$a}';
+$string['standarddeviation'] = 'Стандартнае адхіленьне (для {$a})';
+$string['standarddeviationq'] = 'Стандартнае адхіленьне';
+$string['standarderror'] = 'Стандартная памылка (для {$a})';
+$string['statistics'] = 'Статыстыка';
+$string['statistics:view'] = 'Праглядзець статыстычную справаздачу';
+$string['statisticsreport'] = 'Статыстычная справаздача';
+$string['statisticsreportgraph'] = 'Статыстыка па пазіцыях пытаньняў';
+$string['statsfor'] = 'Статыстыка тэста (для {$a})';
+$string['variant'] = 'Варыянт';
+$string['viewanalysis'] = 'Праглядзець дэталі';
+$string['whichtries'] = 'Прааналізаваць водклічы на';
