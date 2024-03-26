58e43520682a6b221c0248d6fb57e99a6ebd42c0
Denis <denis.makouski@bseu.by>
2024-03-26 17:09:41 +0300

    ++

---
+++ перакладзена 4.0/workshopallocation_manual.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'workshopallocation_manual', language 'en', version '4.0'.
+ *
+ * @package     workshopallocation_manual
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addreviewee'] = 'Дадаць карыстальніка на рэцэнзію';
+$string['addreviewer'] = 'Дадаць рэцэнзента';
+$string['allocationadded'] = 'Адпраўленьне было пасьпяхова прызначана';
+$string['allocationexists'] = 'Прызначэньне ўжо існуе';
+$string['areyousuretodeallocate'] = 'Вы ўпэўнены, што жадаеце скасаваць прызначэньне абранага ацэньваньня?';
+$string['areyousuretodeallocategraded'] = 'Вы зьбіраецеся выдаліць ацэньваньне, што ўжо было ацэнена. Вы сапраўды ўпэўнены, што жадаеце зрабіць гэтае?';
+$string['pluginname'] = 'Ручное прызначэньне';
+$string['privacy:metadata:preference:perpage'] = 'Колькасьць прызначаных ацэньваньняў, якую карыстальнік жадае бачыць на адной старонцы.';
+$string['showallparticipants'] = 'Паказаць усіх удзельнікаў';
