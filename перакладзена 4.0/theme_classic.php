49c2bebeaab856bc571188bc0c180d24458d829d
Denis <denis.makouski@bseu.by>
2024-03-28 10:19:25 +0300

    ++

---
+++ перакладзена 4.0/theme_classic.php
@@ -0,0 +1,41 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'theme_classic', language 'en', version '4.0'.
+ *
+ * @package     theme_classic
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['choosereadme'] = 'Клясічная тэма добра наладжваецца. Яна заснавана на тэме Boost, але мае адрозны макет навігацыі. Гэтая тэма прызначана для прамога выкарыстаньня альбо ў якасьці бацькавай тэмы падчас стварэньня альбо мадыфікацыі карыстальніцкіх тэм, што заснаваны на Bootstrap 4.';
+$string['configtitle'] = 'Клясічная';
+$string['navbardark'] = 'Выкарыстоўваць цёмны стыль панэлі навігацыі';
+$string['navbardarkdesc'] = 'Мяняе колеры тэкста і фону панэлі навігацыі, што знаходзіцца ўверсе старонкі, з цёмнага на сьветлы ды наадварот.';
+$string['pluginname'] = 'Клясічная';
+$string['preset'] = 'Прадусталёўка тэмы';
+$string['preset_desc'] = 'Абярыце прадусталёўку, каб цалкам зьмяніць выгляд тэмы.';
+$string['presetfiles'] = 'Дадатковыя файлы прадусталёўкі тэмы';
+$string['presetfiles_desc'] = 'Файлы прадусталёўкі льга выкарыстоўваць для кардынальнай зьмены выгляду тэмы. Праглядзіце <a href="https://docs.moodle.org/dev/Boost_Presets">Прадусталёўкі Boost</a>, каб атрымаць інфармацыю пра стварэньне і публікаваньне сваіх уласных файлаў прадусталёўкі, ды праглядзіце <a href="https://archive.moodle.net/boost">Рэпазытар прадусталёвак</a>, каб праглядзець прадусталёўкі, апублікаваныя іншымі карыстальнікамі.';
+$string['privacy:metadata'] = 'Клясічная тэма не захоўвае аніякіх асабістых дадзеных.';
+$string['region-side-post'] = 'Права';
+$string['region-side-pre'] = 'Лева';
+$string['sitetitle'] = 'Назва сайту';
+$string['totop'] = 'Уверх';
