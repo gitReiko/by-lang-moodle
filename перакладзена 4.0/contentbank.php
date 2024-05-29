01ed9e9ee4d8b35a5ff2513b91b5722bcec72fac
Denis <denis.makouski@bseu.by>
2024-05-29 16:12:11 +0300

    ++

---
+++ перакладзена 4.0/contentbank.php
@@ -0,0 +1,98 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'contentbank', language 'en', version '4.0'.
+ *
+ * @package     contentbank
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['author'] = 'Аўтар';
+$string['choosecontext'] = 'Абярыце курс ці катэгорыю...';
+$string['close'] = 'Закрыць';
+$string['contentbank'] = 'Банк кантэнту';
+$string['contentbankpreferences'] = 'Перавагі банку кантэнту';
+$string['contentdeleted'] = 'Кантэнт быў выдалены.';
+$string['contentname'] = 'Назва кантэнту';
+$string['contentnotdeleted'] = 'Падчас спробы выдаленьня кантэнту адбылася памылка.';
+$string['contentnotrenamed'] = 'Падчас спробы пераназваньня кантэнту адбылася памылка.';
+$string['contentrenamed'] = 'Кантэнт быў пераназваны.';
+$string['contentsmoved'] = 'Зьмесьціва банку кантэнту было перасунута ў {$a}.';
+$string['contenttypenoaccess'] = 'Вы ня можаце праглядзець гэты экзэмпляр {$a}.';
+$string['contenttypenoedit'] = 'Вы ня можаце рэдагаваць гэты кантэнт';
+$string['contentvisibilitychanged'] = 'Кантэнт быў зроблены {$a}.';
+$string['contentvisibilitynotset'] = 'Падчас спробы ўсталёўкі бачнасьці кантэнту адбылася памылка.';
+$string['contextnotallowed'] = 'У гэтым кантэксьце доступ да банку кантэнту забаронены.';
+$string['deletecontent'] = 'Выдаліць кантэнт';
+$string['deletecontentconfirm'] = 'Вы ўпэўнены, што жадаеце выдаліць кантэнт <em>\'{$a->name}\'</em> і ўсе асацыяваныя зь ім файлы? Гэтае дзеяньне нельга скасаваць.';
+$string['deletecontentconfirmlinked'] = 'Кантэнт будзе выдалены толькі з банку кантэнту. Усе месцы, якія ў гэты момант спасылаюцца на яго, будуць аўтаматычна абноўлены, каб выкарыстоўваць копію гэтага кантэнту заміж яго.';
+$string['displaydetails'] = 'Адлюстроўваць банк кантэнту з дэталямі файлаў';
+$string['displayicons'] = 'Адлюстроўваць банк кантэнту з выявамі';
+$string['emptynamenotallowed'] = 'Пустыя назвы забаронены';
+$string['errordeletingcontentfromcategory'] = 'Памылка выдаленьня кантэнту з катэгорыі {$a}.';
+$string['errornofile'] = 'Дзеля стварэньня кантэнту патрэбен сумяшчальны файл.';
+$string['eventcontentcreated'] = 'Кантэнт створаны';
+$string['eventcontentdeleted'] = 'Кантэнт выдалены';
+$string['eventcontentreplaced'] = 'Кантэнт заменены файлам';
+$string['eventcontentupdated'] = 'Кантэнт абноўлены';
+$string['eventcontentuploaded'] = 'Кантэнт запампаваны';
+$string['eventcontentviewed'] = 'Кантэнт прагледжаны';
+$string['exit'] = 'Выхад';
+$string['file'] = 'Запампаваць кантэнт';
+$string['file_help'] = 'Файлы могуць захоўвацца ў банке кантэнту дзеля выкарыстаньня ў курсах. Запампоўваць льга толькі файлы, што выкарыстоўваюцца ў тыпах кантэнту, уключаных на сайце.';
+$string['itemsfound'] = 'знойдзена {$a} прадметаў';
+$string['lastmodified'] = 'Апошняя зьмена';
+$string['more'] = 'Болей';
+$string['name'] = 'Кантэнт';
+$string['nocontentavailable'] = 'Кантэнт не даступны';
+$string['nocontenttypes'] = 'Няма даступных тыпаў кантэнту';
+$string['nopermissiontodelete'] = 'Вы ня маеце дазволу на выдаленьне кантэнту.';
+$string['nopermissiontomanage'] = 'Вы ня маеце дазволу на кіраваньне кантэнту.';
+$string['notavailable'] = 'Прабачце, гэты кантэнт не даступны.';
+$string['notvalidpackage'] = 'Прабачце, гэты файл хібны.';
+$string['privacy:metadata:content:contenttype'] = 'Плагін тыпу кантэнту для кантэнту банку кантэнту.';
+$string['privacy:metadata:content:name'] = 'Назва кантэнту ў банку кантэнту.';
+$string['privacy:metadata:content:timecreated'] = 'Час стварэньня кантэнту.';
+$string['privacy:metadata:content:timemodified'] = 'Час зьмены кантэнту.';
+$string['privacy:metadata:content:usercreated'] = 'Карыстальнік, які стварыў кантэнт.';
+$string['privacy:metadata:content:usermodified'] = 'Апошні карыстальнік, які зьмяніў кантэнт.';
+$string['privacy:metadata:contentbankcontent'] = 'Захоўвае кантэнт у банке кантэнту.';
+$string['privacy:metadata:userid'] = 'ID карыстальніка, што стварыў ці зьмяніў кантэнт у банке кантэнту.';
+$string['privacy:request:preference:set'] = 'Значэньне налады \'{$a->name}\' была \'{$a->value}\'';
+$string['public'] = 'публічны';
+$string['rename'] = 'Пераназваць';
+$string['renamecontent'] = 'Пераназваць кантэнт';
+$string['replacecontent'] = 'Замяніць файл';
+$string['searchcontentbankbyname'] = 'Пошук кантэнту па назьве';
+$string['size'] = 'Памер';
+$string['timecreated'] = 'Час стварэньня';
+$string['type'] = 'Тып';
+$string['unlisted'] = 'не ў сьпісе';
+$string['unsupported'] = 'Гэты тып кантэнту не падтрымліваецца.';
+$string['upload'] = 'Запампаваць';
+$string['uses'] = 'Зьвязаныя месцы';
+$string['visibilitychoicepublic'] = 'Публічны';
+$string['visibilitychoiceunlisted'] = 'Не ў сьпісе';
+$string['visibilitypref'] = 'Стандартная бачнасьць кантэнту';
+$string['visibilitypref_help'] = 'Кантэнт, які вы ствараеце ў банке кантэнту, будзе выкарыстоўваць гэтую стандартную наладу бачнасьці.';
+$string['visibilitysetpublic'] = 'Зрабіць публічным';
+$string['visibilitysetunlisted'] = 'Зьмясьціць у сьпіс';
+$string['visibilitytitleunlisted'] = '{$a} (Не ў сьпісе)';
