e36a38d0070a8590c15ed2f179bbbd3abcc4607f
Denis <denis.makouski@bseu.by>
2024-06-13 16:59:19 +0300

    ++

---
+++ праца/scormreport_interactions.php
@@ -0,0 +1,41 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'scormreport_interactions', language 'en', version '4.0'.
+ *
+ * @package     scormreport_interactions
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Interactions report';
+$string['privacy:metadata:preference:scorm_report_interactions_qtext'] = 'Whether to display the summary of questions in the SCORM interactions report';
+$string['privacy:metadata:preference:scorm_report_interactions_resp'] = 'Whether to display the summary of responses in the SCORM interactions report';
+$string['privacy:metadata:preference:scorm_report_interactions_result'] = 'Whether to display the summary of results in the SCORM interactions report';
+$string['privacy:metadata:preference:scorm_report_interactions_right'] = 'Whether to display the summary of right anwers in the SCORM interactions report';
+$string['privacy:metadata:preference:scorm_report_pagesize'] = 'Number of users to display in the SCORM reports';
+$string['questionx'] = 'Question {$a}';
+$string['responsex'] = 'Response {$a}';
+$string['resultx'] = 'Result {$a}';
+$string['rightanswerx'] = 'Right answer {$a}';
+$string['summaryofquestiontext'] = 'Summary of question';
+$string['summaryofresponse'] = 'Summary of responses';
+$string['summaryofresult'] = 'Summary of result';
+$string['summaryofrightanswer'] = 'Summary of right answer';
