9a81eecf26048c0e2c6742f881112eecaeb72cf9
Denis <denis.makouski@bseu.by>
2024-06-17 15:58:15 +0300

    ++

---
+++ перакладзена 4.0/editor_atto.php
@@ -0,0 +1,57 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'editor_atto', language 'en', version '4.0'.
+ *
+ * @package     editor_atto
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['autosavefailed'] = 'Нельга далучыцца да сэрверу. Калі вы адправіце гэтую старонку зараз, вашыя зьмены могуць быць страчаны.';
+$string['autosavefrequency'] = 'Частата аўтазахаваньня';
+$string['autosavefrequency_desc'] = 'Гэта колькасьць сякундаў між спробамі аўтазахаваньня. Atto будзе аўтаматычна захоўваць тэкст у рэдактары ў адпаведнасьці з гэтай наладай, дзякуючы таму тэкст можа быць аўтаматычна адноўлены, калі той жа карыстальнік вярнецца да той жа формы.';
+$string['autosaves'] = 'Інфармацыя пра аўтазахаваньне рэдактару';
+$string['autosavesucceeded'] = 'Чарнавік захаваны.';
+$string['editor_command_keycode'] = 'Cmd + {$a}';
+$string['editor_control_keycode'] = 'Ctrl + {$a}';
+$string['errorcannotparseline'] = 'Радок \'{$a}\' мае хібны фармат.';
+$string['errorgroupisusedtwice'] = 'Група \'{$a}\' вызначана двойчы; назвы групаў мусяць быць унікальнымі.';
+$string['errornopluginsorgroupsfound'] = 'Ня знойдзена ніводнага плагіна ці групы; калі ласка, дадайце некалькі групаў альбо плагінаў.';
+$string['errorpluginisusedtwice'] = 'Плагін \'{$a}\' выкарыстоўваецца двойчы; плагіны могуць быць вызначаны толькі аднойчы.';
+$string['errorpluginnotfound'] = 'Плагін \'{$a}\' ня можа быць выкарастаны; здаецца, ён не ўсталяваны.';
+$string['errortextrecovery'] = 'На жаль, не ўдалося аднавіць чарнавы варыянт.';
+$string['infostatus'] = 'Інфармацыя';
+$string['plugin_title_shortcut'] = '{$a->title} [{$a->shortcut}]';
+$string['pluginname'] = 'HTML-рэдактар Atto';
+$string['privacy:metadata:database:atto_autosave'] = 'Аўтаматычнае захаваньне чарнавікоў тэкставага рэдактару.';
+$string['privacy:metadata:database:atto_autosave:drafttext'] = 'Тэкст, які быў захаваны.';
+$string['privacy:metadata:database:atto_autosave:timemodified'] = 'Час зьмены зьмесьціва.';
+$string['privacy:metadata:database:atto_autosave:userid'] = 'ID карыстальніка, чые дадзеныя былі захаваны.';
+$string['recover'] = 'Аднавіць';
+$string['richtexteditor'] = 'Прасунуты рэдактар тэксту';
+$string['settings'] = 'Налады панэлі прылад Atto';
+$string['subplugintype_atto'] = 'Плагін Atto';
+$string['subplugintype_atto_plural'] = 'Плагіны Atto';
+$string['taskautosavecleanup'] = 'Выдаліць пратэрмінаваныя чарнавікі аўтазахаваньня';
+$string['textrecovered'] = 'Чарнавая версія гэтага тэксту была аўтаматычна адноўлена.';
+$string['toolbarconfig'] = 'Налады панэлі прылад';
+$string['toolbarconfig_desc'] = 'Тут льга сканфігураваць сьпіс плагінаў і парадак іх адлюстраваньня. Канфігурацыя складаецца з групаў (па адной у радку), за якімі ідзе спарадкаваны сьпіс плагінаў для гэтай групы. Група адзьдзяляецца ад плагінаў знакам роўнасьці, а плагіны - коскамі. Назвы групаў мусяць быць унікальнымі і паказваць на тое, што яднае кнопкі. Назвы кнопак і групаў не павінны паўтарацца і могуць утрымліваць толькі літарна-лічбавыя знакі.';
+$string['warningstatus'] = 'Увага';
